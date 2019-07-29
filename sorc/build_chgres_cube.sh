7dabe8c20b0c454e1dc46a640ff1d8b4d6390cb8
jeff beck <jeff.beck@noaa.gov>
2019-07-29 22:24:55 +0000

    Changes to build script/module file and name change for output file for FV3 data form tile1 to tile7

--- sorc/build_chgres_cube.sh
+++ sorc/build_chgres_cube.sh
@@ -4,31 +4,33 @@ set -eux
 source ./machine-setup.sh > /dev/null 2>&1
 cwd=`pwd`
 
-USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
+USE_PREINST_LIBS=${USE_PREINST_LIBS:-"false"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH=/scratch3/NCEPDEV/nwprod/lib/modulefiles
-  source ../modulefiles/chgres_cube.$target             > /dev/null 2>&1
+  source ../modulefiles/chgres_cube.${target}           > /dev/null 2>&1
 else
   export MOD_PATH=${cwd}/lib/modulefiles
   if [ $target = wcoss_cray ]; then
     source ../modulefiles/chgres_cube.${target}_userlib > /dev/null 2>&1
   else
-    source ../modulefiles/chgres_cube.$target           > /dev/null 2>&1
+    source ../modulefiles/chgres_cube.${target}         > /dev/null 2>&1
   fi
 fi
-
-# Check final exec folder exists
+#
+# Check whether exec folder into which the executable will be copied ex-
+# ists.  If not, create it.
+#
 if [ ! -d "../exec" ]; then
   mkdir ../exec
 fi
-
 #
-# --- Chgres part
+# Change location to the directory of the chgres_cube code.  Then clean
+# any existing build, rebuild, and install (i.e. move the executable 
+# from the source code directory to the exec directory where all other
+# executables are located).
 #
 cd chgres_cube.fd
+make -f Makefile clean
+make -f Makefile
+make -f Makefile install
 
-make clean
-make
-make install
-
-exit
