86d92b080d37b28769be0000d2a2b3077b85a6fc
Bin.Liu <Bin.Liu@noaa.gov>
2019-09-20 16:38:26 +0000

    feature/HAFS: More for Hera porting. Use newer versions of libraries on Hera. Modify the method to detect the Hera machine.

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -28,7 +28,7 @@ if [[ -d /lfs3 ]] ; then
     target=jet
     module purge
     module use /mnt/lfs3/projects/hfv3gfs/nwprod/lib/modulefiles
-elif [[ `hostname -a` == *"hera.fairmont.rdhpcs.noaa.gov"* ]] ; then
+elif [[ -e /scratch1/NCEPDEV ]] ; then
     # We are on NOAA Hera
     if ( ! eval module help > /dev/null 2>&1 ) ; then
 	echo load the module command 1>&2
