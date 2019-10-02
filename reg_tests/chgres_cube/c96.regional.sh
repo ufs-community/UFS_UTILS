adf493debae792147859ab2e314d7bd7276857a6
GeorgeGayno-NOAA <52789452+GeorgeGayno-NOAA@users.noreply.github.com>
2019-10-02 09:39:25 -0400

    Feature/chgres cube reg tests (#21)
    develop:  Create a set of regression tests for chgres_cube program.
    
    This commit references issue #5 .

---
+++ reg_tests/chgres_cube/c96.regional.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+# Invoke chgres to create C96 regional coldstart files, including a lateral
+# boundary file, using FV3 gaussian nemsio files as input.  The 
+# coldstart files are then compared to baseline files using the 'nccmp' 
+# utility.  
+#
+# This script is run by the machine specific driver script.
+#-----------------------------------------------------------------------------
+
+set -x
+
+export DATA=$OUTDIR/c96_regional
+rm -fr $DATA
+
+export FIXfv3=${HOMEreg}/fix/C96.regional
+export OROG_FILES_TARGET_GRID="C96_oro_data.tile7.nc"
+export COMIN=${HOMEreg}/input_data/fv3.nemsio
+export ATM_FILES_INPUT=gfs.t12z.atmf000.nemsio
+export SFC_FILES_INPUT=gfs.t12z.sfcf000.nemsio
+export VCOORD_FILE=${HOMEufs}/fix/fix_am/global_hyblev.l64.txt
+export REGIONAL=1
+export HALO_BLEND=0
+export HALO_BNDY=4
+
+export CDATE=2019070412
+
+export OMP_NUM_THREADS_CY=1
+
+#-----------------------------------------------------------------------------
+# Invoke chgres program.
+#-----------------------------------------------------------------------------
+
+echo "Starting at: " `date`
+
+${HOMEufs}/ush/chgres_cube.sh
+
+iret=$?
+if [ $iret -ne 0 ]; then
+  echo "<<< C96 REGIONAL TEST FAILED. <<<"
+  exit $iret
+fi
+
+echo "Ending at: " `date`
+
+#-----------------------------------------------------------------------------
+# Compare output from chgres to baseline set of data.
+#-----------------------------------------------------------------------------
+
+cd $DATA
+
+test_failed=0
+for files in *.nc
+do
+  if [ -f $files ]; then
+    echo CHECK $files
+    $NCCMP -dmfqS $files $HOMEreg/baseline_data/c96_regional/$files
+    iret=$?
+    if [ $iret -ne 0 ]; then
+      test_failed=1
+    fi
+  fi
+done
+
+set +x
+if [ $test_failed -ne 0 ]; then
+  echo "<<< C96 REGIONAL TEST FAILED. >>>"
+else
+  echo "<<< C96 REGIONAL TEST PASSED. >>>"
+fi
+
+exit 0
---
+++ reg_tests/chgres_cube/driver.jet.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+#
+# Run the chgres_cube regression tests on JET.
+#
+# Set OUTDIR to your working directory.  Set the PROJECT_CODE and QUEUE
+# as appropriate.  To see which projects you are authorized to use,
+# type "account_params".
+#
+# Invoke the script with no arguments.  A series of daily-
+# chained jobs will be submitted.  To check the queue, type:
+# "squeue -u USERNAME".
+#
+# The run output will be stored in OUTDIR.  Log output from the suite
+# will be in LOG_FILE.  Once the suite has completed, a summary is
+# placed in SUM_FILE.
+#
+# A test fails when its output does not match the baseline files as
+# determined by the "nccmp" utility.  The baseline files are stored in
+# HOMEreg.
+#
+#-----------------------------------------------------------------------------
+
+set -x
+
+source /apps/lmod/lmod/init/sh
+module purge
+module load intel/15.0.3.187
+module load impi/2018.4.274
+module load szip
+module load hdf5
+module load netcdf/4.2.1.1
+module list
+
+export OUTDIR=/mnt/lfs3/projects/emcda/$LOGNAME/stmp/chgres_reg_tests
+PROJECT_CODE="emcda"
+QUEUE="windfall"
+
+#-----------------------------------------------------------------------------
+# Should not have to change anything below here.  HOMEufs is the root
+# directory of your UFS_UTILS clone.  HOMEreg contains the input data
+# and baseline data for each test.
+#-----------------------------------------------------------------------------
+
+export HOMEufs=$PWD/../..
+
+export HOMEreg=/mnt/lfs3/projects/emcda/George.Gayno/reg_tests/chgres_cube
+
+export NCCMP=/apps/nccmp/1.8.2.1/intel/18.0.3.222/bin/nccmp
+
+LOG_FILE=regression.log
+SUM_FILE=summary.log
+rm -f $LOG_FILE $SUM_FILE
+
+export OMP_STACKSIZE=1024M
+
+export APRUN=srun
+
+rm -fr $OUTDIR
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using FV3 warm restart files.
+#-----------------------------------------------------------------------------
+
+TEST1=$(sbatch --parsable --partition=xjet --nodes=1 --ntasks-per-node=6 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J c96.fv3.restart \
+      -o $LOG_FILE -e $LOG_FILE ./c96.fv3.restart.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C192 using FV3 tiled history files.
+#-----------------------------------------------------------------------------
+
+TEST2=$(sbatch --parsable --partition=xjet --nodes=1 --ntasks-per-node=6 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J c192.fv3.history \
+      -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST1 ./c192.fv3.history.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using FV3 gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST3=$(sbatch --parsable --partition=xjet --nodes=1 --ntasks-per-node=6 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J c96.fv3.nemsio \
+      -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST2 ./c96.fv3.nemsio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using spectral GFS sigio/sfcio files.
+#-----------------------------------------------------------------------------
+
+TEST4=$(sbatch --parsable --partition=xjet --nodes=2 --ntasks-per-node=3 --cpus-per-task=6 -t 0:15:00 \
+      -A $PROJECT_CODE -q $QUEUE -J c96.gfs.sigio -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST3 ./c96.gfs.sigio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using spectral GFS gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST5=$(sbatch --parsable --partition=xjet --nodes=1 --ntasks-per-node=6 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J c96.gfs.nemsio \
+      -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST4 ./c96.gfs.nemsio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize regional C96 using FV3 gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST6=$(sbatch --parsable --partition=xjet --nodes=1 --ntasks-per-node=6 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J c96.regional \
+      -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST5 ./c96.regional.sh)
+
+#-----------------------------------------------------------------------------
+# Create summary log.
+#-----------------------------------------------------------------------------
+
+sbatch --partition=xjet --nodes=1  -t 0:01:00 -A $PROJECT_CODE -J chgres_summary -o $LOG_FILE -e $LOG_FILE \
+       --open-mode=append -q $QUEUE -d afterok:$TEST6 << EOF
+#!/bin/sh
+grep -a '<<<' $LOG_FILE  > $SUM_FILE
+EOF
+
+exit 0
