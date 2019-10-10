445bb677ec2693bc788895e4b1bb8bb77db8890e
GeorgeGayno-NOAA <52789452+GeorgeGayno-NOAA@users.noreply.github.com>
2019-10-10 08:33:47 -0400

    Feature/hera port (#25)
    develop:  This commit references #1 
    
    Update repository to run on Hera, the replacement for Theia.
    Remove all references to Theia.

--- sorc/link_fixdirs.sh
+++ sorc/link_fixdirs.sh
@@ -9,20 +9,20 @@ machine=${2}
 if [ $# -lt 2 ]; then
     set +x
     echo '***ERROR*** must specify two arguements: (1) RUN_ENVIR, (2) machine'
-    echo ' Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | theia | jet )'
+    echo ' Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | hera  | jet )'
     exit 1
 fi
 
 if [ $RUN_ENVIR != emc -a $RUN_ENVIR != nco ]; then
     set +x
     echo '***ERROR*** unsupported run environment'
-    echo 'Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | theia | jet )'
+    echo 'Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | hera | jet )'
     exit 1
 fi
-if [ $machine != cray -a $machine != theia -a $machine != dell -a $machine != jet ]; then
+if [ $machine != cray -a $machine != hera -a $machine != dell -a $machine != jet ]; then
     set +x
     echo '***ERROR*** unsupported machine'
-    echo 'Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | theia | jet )'
+    echo 'Syntax: link_fv3gfs.sh ( nco | emc ) ( cray | dell | hera | jet )'
     exit 1
 fi
 
@@ -39,23 +39,15 @@ if [ $machine == "cray" ]; then
     FIX_DIR="/gpfs/hps3/emc/global/noscrub/emc.glopara/git/fv3gfs/fix"
 elif [ $machine = "dell" ]; then
     FIX_DIR="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/fv3gfs/fix"
-elif [ $machine = "theia" ]; then
-    FIX_DIR="/scratch4/NCEPDEV/global/save/glopara/git/fv3gfs/fix"
+elif [ $machine = "hera" ]; then
+    FIX_DIR="/scratch1/NCEPDEV/global/glopara/fix"
 elif [ $machine = "jet" ]; then
     FIX_DIR="/lfs3/projects/hfv3gfs/glopara/git/fv3gfs/fix"
 fi
 cd ${pwd}/../fix                ||exit 8
-for dir in fix_am fix_fv3 fix_orog fix_fv3_gmted2010 ; do
+for dir in fix_am fix_fv3 fix_orog fix_fv3_gmted2010 fix_sfc_climo; do
     [[ -d $dir ]] && rm -rf $dir
     $LINK $FIX_DIR/$dir  .
 done
-
-if [ $machine == "cray" ] || [ $machine = "dell" ]; then
-    $LINK /gpfs/dell2/emc/modeling/noscrub/George.Gayno/landutil.git/climo_fields_netcdf ./fix_sfc_climo
-elif [ $machine = "theia" ]; then
-    $LINK /scratch4/NCEPDEV/da/noscrub/George.Gayno/climo_fields_netcdf ./fix_sfc_climo
-elif [ $machine = "jet" ]; then
-    $LINK /mnt/lfs3/projects/emcda/George.Gayno/climo_fields_netcdf ./fix_sfc_climo
-fi
 
 exit 0
