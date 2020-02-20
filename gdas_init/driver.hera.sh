64f94b325c3bbe654e32afe035d307ac873b3d32
George Gayno <George.Gayno@noaa.gov>
2020-02-20 19:15:12 +0000

    feature/gdas_init:  This commit references #33.
    Update 'driver.hera.sh' to work with pre-v14 gfs data.

--- gdas_init/driver.hera.sh
+++ gdas_init/driver.hera.sh
@@ -33,6 +33,13 @@ if [ $EXTRACT_DATA == yes ]; then
   WALLT="2:00:00"
 
   case $gfs_ver in
+    v12 | v13)
+      DATAH=$(sbatch --parsable --partition=service --ntasks=1 --mem=$MEM -t $WALLT -A $PROJECT_CODE -q $QUEUE -J get_hires \
+       -o log.data.hires -e log.data.hires ./get_pre-v14.data.sh hires)
+      DATA1=$(sbatch --parsable --partition=service --ntasks=1 --mem=$MEM -t $WALLT -A $PROJECT_CODE -q $QUEUE -J get_enkf \
+       -o log.data.enkf -e log.data.enkf ./get_pre-v14.data.sh enkf)
+      DEPEND="-d afterok:$DATAH:$DATA1"
+      ;;
     v14)
       DATAH=$(sbatch --parsable --partition=service --ntasks=1 --mem=$MEM -t $WALLT -A $PROJECT_CODE -q $QUEUE -J get_hires \
        -o log.data.hires -e log.data.hires ./get_v14.data.sh hires)
@@ -74,6 +81,7 @@ if [ $RUN_CHGRES == yes ]; then
   MEMBER=hires
   NODES=3
   WALLT="0:15:00"
+  export OMP_NUM_THREADS=1
   if [ $CRES_HIRES == 'C768' ] ; then
     NODES=5
   elif [ $CRES_HIRES == 'C1152' ] ; then
@@ -81,6 +89,13 @@ if [ $RUN_CHGRES == yes ]; then
     WALLT="0:20:00"
   fi
   case $gfs_ver in
+    v12 | v13)
+      export OMP_NUM_THREADS=4
+      export OMP_STACKSIZE=1024M
+      sbatch --parsable --ntasks-per-node=6 --nodes=${NODES} --cpus-per-task=$OMP_NUM_THREADS \
+        -t $WALLT -A $PROJECT_CODE -q $QUEUE -J chgres_${MEMBER} \
+        -o log.${MEMBER} -e log.${MEMBER} ${DEPEND} run_pre-v14.chgres.sh ${MEMBER}
+      ;;
     v14)
       sbatch --parsable --ntasks-per-node=6 --nodes=${NODES} -t $WALLT -A $PROJECT_CODE -q $QUEUE -J chgres_${MEMBER} \
       -o log.${MEMBER} -e log.${MEMBER} ${DEPEND} run_v14.chgres.sh ${MEMBER}
@@ -98,6 +113,13 @@ if [ $RUN_CHGRES == yes ]; then
       MEMBER_CH="0${MEMBER}"
     fi
     case $gfs_ver in
+      v12 | v13)
+        export OMP_NUM_THREADS=2
+        export OMP_STACKSIZE=1024M
+        sbatch --parsable --ntasks-per-node=12 --nodes=1 --cpus-per-task=$OMP_NUM_THREADS \
+         -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J chgres_${MEMBER_CH} \
+         -o log.${MEMBER_CH} -e log.${MEMBER_CH} ${DEPEND} run_pre-v14.chgres.sh ${MEMBER_CH}
+        ;;
       v14)
         sbatch --parsable --ntasks-per-node=12 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J chgres_${MEMBER_CH} \
         -o log.${MEMBER_CH} -e log.${MEMBER_CH} ${DEPEND} run_v14.chgres.sh ${MEMBER_CH}
