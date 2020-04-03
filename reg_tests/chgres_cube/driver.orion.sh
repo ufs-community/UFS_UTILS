6d1c6f80e4f1e12c0f7eacd7de8ca02c9c0cdb3e
George Gayno <George.Gayno@noaa.gov>
2020-04-03 16:37:57 -0500

    feature/orion  This commit references #96.
    Add Orion driver script for chgres_cube regression test suite.

---
+++ reg_tests/chgres_cube/driver.orion.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+#
+# Run the chgres_cube regression tests on Orion.
+#
+# Set OUTDIR to your working directory.  Set the PROJECT_CODE and QUEUE
+# as appropriate.  To see which projects you are authorized to use,
+# type "account_params".
+#
+# Invoke the script with no arguments.  A series of daily-chained
+# regression tests will be submitted.  To check the queue, type:
+# "squeue -u USERNAME".
+#
+# The run output will be stored in OUTDIR.  Log output from the suite
+# will be in LOG_FILE.  Once the suite has completed, a summary is
+# placed in SUM_FILE.
+#
+# A test fails when its output does not match the baseline files as
+# determined by the "nccmp" utility.  The baseline files are stored in
+# HOMEreg.
+#
+#-----------------------------------------------------------------------------
+
+set -x
+
+source /apps/lmod/init/sh
+module purge
+module load intel/2020
+module load impi/2020
+module load netcdf/4.7.2
+module list
+
+export OUTDIR=/work/noaa/stmp/$LOGNAME/chgres_reg_tests
+PROJECT_CODE="fv3-cpu"
+QUEUE="debug"
+
+#-----------------------------------------------------------------------------
+# Should not have to change anything below here.  HOMEufs is the root
+# directory of your UFS_UTILS clone.  HOMEreg contains the input data
+# and baseline data for each test.
+#-----------------------------------------------------------------------------
+
+export HOMEufs=$PWD/../..
+
+export HOMEreg=/work/noaa/da/ggayno/save/reg_tests/chgres_cube
+
+export NCCMP=/apps/nccmp-1.8.5/bin/nccmp
+
+LOG_FILE=regression.log
+SUM_FILE=summary.log
+rm -f $LOG_FILE $SUM_FILE
+
+export OMP_STACKSIZE=1024M
+
+export APRUN=srun
+
+rm -fr $OUTDIR
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using FV3 warm restart files.
+#-----------------------------------------------------------------------------
+
+TEST1=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.fv3.restart \
+      -o $LOG_FILE -e $LOG_FILE ./c96.fv3.restart.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C192 using FV3 tiled history files.
+#-----------------------------------------------------------------------------
+
+TEST2=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c192.fv3.history \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST1 ./c192.fv3.history.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using FV3 gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST3=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.fv3.nemsio \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST2 ./c96.fv3.nemsio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using spectral GFS sigio/sfcio files.
+#-----------------------------------------------------------------------------
+
+TEST4=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.gfs.sigio \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST3 ./c96.gfs.sigio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize C96 using spectral GFS gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST5=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.gfs.nemsio \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST4 ./c96.gfs.nemsio.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize regional C96 using FV3 gaussian nemsio files.
+#-----------------------------------------------------------------------------
+
+TEST6=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.regional \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST5 ./c96.regional.sh)
+
+#-----------------------------------------------------------------------------
+# Initialize global C192 using GFS GRIB2 files.
+#-----------------------------------------------------------------------------
+
+TEST7=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:05:00 -A $PROJECT_CODE -q $QUEUE -J c192.gfs.grib2 \
+      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST6 ./c192.gfs.grib2.sh)
+
+#-----------------------------------------------------------------------------
+# Create summary log.
+#-----------------------------------------------------------------------------
+
+sbatch --nodes=1 -t 0:01:00 -A $PROJECT_CODE -J chgres_summary -o $LOG_FILE -e $LOG_FILE \
+       --open-mode=append -q $QUEUE -d afterok:$TEST7 << EOF
+#!/bin/sh
+grep -a '<<<' $LOG_FILE  > $SUM_FILE
+EOF
+
+exit 0
