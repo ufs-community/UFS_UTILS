41a3d36dd72ac3c38449dc21c80c6c76074a50f6
George Gayno <George.Gayno@noaa.gov>
2020-04-13 19:54:25 +0000

    feature/cmake2   This commit references #91.
    Open new branch.  Add cmake build for sfc_climo_gen.

--- sorc/build_sfc_climo_gen.sh
+++ sorc/build_sfc_climo_gen.sh
@@ -2,6 +2,7 @@
 set -eux
 
 source ./machine-setup.sh > /dev/null 2>&1
+
 cwd=`pwd`
 
 # Check final exec folder exists
@@ -9,7 +10,6 @@ if [ ! -d "../exec" ]; then
   mkdir ../exec
 fi
 
-module purge
 USE_PREINST_LIBS=${USE_PREINST_LIBS:-"true"}
 if [ $USE_PREINST_LIBS = true ]; then
   export MOD_PATH
@@ -27,8 +27,15 @@ module list
 
 cd ./sfc_climo_gen.fd
 
+rm -fr build
+mkdir build
+cd build
+
+cmake .. -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_INSTALL_PREFIX=../../..
+
 make clean
 make
 make install
 
+set +x
 echo; echo DONE BUILDING sfc_climo_gen
