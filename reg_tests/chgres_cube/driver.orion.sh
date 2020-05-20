9e0e46ab03afbdb4f41df48ae999e31892399113
George Gayno <George.Gayno@noaa.gov>
2020-05-20 09:32:25 -0500

    feature/orion  This commit references #96.
    New regression test driver scripts for 'global_cycle' and
    'grid_gen'.

--- reg_tests/chgres_cube/driver.orion.sh
+++ reg_tests/chgres_cube/driver.orion.sh
@@ -6,11 +6,11 @@
 #
 # Set OUTDIR to your working directory.  Set the PROJECT_CODE and QUEUE
 # as appropriate.  To see which projects you are authorized to use,
-# type "account_params".
+# type "saccount_params".
 #
 # Invoke the script with no arguments.  A series of daily-chained
 # regression tests will be submitted.  To check the queue, type:
-# "squeue -u USERNAME".
+# "squeue -u $LOGNAME".
 #
 # The run output will be stored in OUTDIR.  Log output from the suite
 # will be in LOG_FILE.  Once the suite has completed, a summary is
