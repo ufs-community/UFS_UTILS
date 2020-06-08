93f438cc23e9749b16e4441e13f7a74bb225929c
Shan.Sun <shan.sun@noaa.gov>
2020-06-08 05:07:19 +0000

    Modifying file names.

--- ush/fv3gfs_add_lake.sh
+++ ush/fv3gfs_add_lake.sh
@@ -9,7 +9,7 @@ orodata_dir=/scratch1/NCEPDEV/nems/emc.nemspara/RT/NEMSfv3gfs/develop-20200603/I
 
 if [ $# -ne 2 ]; then 
 echo "Usage $0 [res] [outdir]"
-echo "res: 48, 96, ... , 1152, or all to create all resolutions."
+echo "res: 96, ... , 768, or all to create all resolutions."
 echo "outdir: output directory."
 exit 1 
 fi
@@ -29,15 +29,31 @@ cp ${executable}  ${outdir}
 cd $outdir
 
 if [ $res -eq "all" ]; then 
-  for res in 48 96 192 384 768 1152 
+  for res in 96 192 384 768
   do
-    cp ${orodata_dir}/FV3_input_data_c${res}/INPUT/oro_data.tile?.nc .
+    if [ $res -eq 96 ]; then 
+      cp ${orodata_dir}/FV3_input_data/INPUT/oro_data.tile?.nc .
+    else
+      cp ${orodata_dir}/FV3_input_data_c${res}/INPUT/oro_data.tile?.nc .
+    fi
     lakefrac 0 ${res}
+    for tile in 1 2 3 4 5 6
+    do
+      mv oro_data.tile${tile}.nc C${res}_oro_data.tile${tile}.nc
+    done 
   done
 elif [ $res -eq 96 ]; then
   cp ${orodata_dir}/FV3_input_data/INPUT/oro_data.tile?.nc .
   lakefrac 0 ${res}
+  for tile in 1 2 3 4 5 6
+  do
+    mv oro_data.tile${tile}.nc C${res}_oro_data.tile${tile}.nc
+  done 
 else
   cp ${orodata_dir}/FV3_input_data_c${res}/INPUT/oro_data.tile?.nc .
   lakefrac 0 ${res}
+  for tile in 1 2 3 4 5 6
+  do
+    mv oro_data.tile${tile}.nc C${res}_oro_data.tile${tile}.nc
+  done 
 fi
