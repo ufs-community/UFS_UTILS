bcc5f2196507e23bd6e4b76add362f9f4e84d315
George Gayno <George.Gayno@noaa.gov>
2020-06-10 20:59:40 +0000

    feature/chgres_thom_mp  This commit references #106.
    Time interpolate thompson fields (linearly) using logic borrowed
    from sfcsub.F

--- sorc/chgres_cube.fd/thompson_mp.F90
+++ sorc/chgres_cube.fd/thompson_mp.F90
@@ -2,6 +2,7 @@
 
  use esmf
  use netcdf
+ use program_setup, only      : cycle_mon, cycle_day, cycle_hour
 
  implicit none
 
@@ -27,24 +28,30 @@
  implicit none
 
  character(len=150) :: thomp_mp_climo_file
- character(len=3)   :: month(12)
+ character(len=3)   :: month(13)
  character(len=14)  :: record
  character(len=2)   :: level
 
  integer            :: error, ncid, rc, clb(2), cub(2)
  integer            :: i, j, k, localpet, npets, id_var
+ integer            :: jda(8), jdow, jdoy, jday
+ integer            :: mm, mmm, mmp, mon1, mon2
 
- real(esmf_kind_r8), allocatable :: dummy3d(:,:,:)
+ real(esmf_kind_r8), allocatable :: dummy3d(:,:,:), dummy4d(:,:,:,:)
 
  real(esmf_kind_r8), pointer :: lat_ptr(:,:), lon_ptr(:,:)
  real(esmf_kind_r8) :: lon, lat
+ real               :: rjday, dayhf(13), wei1m, wei2m
 
  type(esmf_vm)                :: vm
 
  type(esmf_polekind_flag)         :: polekindflag(2)
 
  data month /'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', &
-             'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC' /
+             'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN' /
+
+ data dayhf/ 15.5, 45.0, 74.5,105.0,135.5,166.0,   &
+             196.5,227.5,258.0,288.5,319.0,349.5,380.5/
 
 ! First create esmf grid object for the climo grid.
 
@@ -145,34 +152,74 @@
 
  thomp_mp_climo_file = "/scratch1/NCEPDEV/da/George.Gayno/ufs_utils.git/chgres_thomp_mp/QNWFA_QNIFA_SIGMA_MONTHLY.dat.nc"
 
-
  print*,"- READ THOMP_MP_CLIMO_FILE: ", trim(thomp_mp_climo_file)
  error=nf90_open(trim(thomp_mp_climo_file),nf90_nowrite,ncid)
  call netcdf_err(error, 'opening: '//trim(thomp_mp_climo_file) )
  
+
+ jda=0
+ jda(1) = 2007
+ if (cycle_mon == 2 .and. cycle_day == 29) then  ! leap year
+   jda(2) = 3
+   jda(3) = 1
+ else
+   jda(2) = cycle_mon
+   jda(3) = cycle_day
+ endif
+
+ jda(5) = cycle_hour
+ jdow = 0
+ jdoy = 0
+ jday = 0
+ call w3doxdat(jda,jdow,jdoy,jday)
+ rjday = float(jdoy) + float(jda(5)) / 24.
+ if(rjday < dayhf(1)) rjday = rjday + 365.
  
+ print*,'rjday ', jda, rjday
+
+ do mm=1,12
+   mmm = mm
+   mmp = mm + 1
+   if(rjday >= dayhf(mmm) .and. rjday < dayhf(mmp)) then
+     mon1 = mmm
+     mon2 = mmp
+     exit
+   endif
+ enddo
+
+ wei1m = (dayhf(mon2)-rjday)/(dayhf(mon2)-dayhf(mon1))
+ wei2m = 1.0 - wei1m
+
+ print*,'mon1/2 ',mon1,mon2,wei1m,wei2m
+
  if (localpet == 0) then
    allocate(dummy3d(i_thomp_mp_climo, j_thomp_mp_climo, lev_thomp_mp_climo))
    dummy3d = 0.0
+   allocate(dummy4d(i_thomp_mp_climo, j_thomp_mp_climo, lev_thomp_mp_climo, mon1:mon2))
+   dummy4d = 0.0
  else
    allocate(dummy3d(0,0,0))
+   allocate(dummy4d(0,0,0,0))
  endif
 
  if (localpet == 0) then
+   do mm = mon1, mon2
    do k = 1, lev_thomp_mp_climo
      write(level, "(I2.2)") k
      if (k < 10) then
-     record = "QNIFA_" // month(1) // "__" // level
+       record = "QNIFA_" // month(mm) // "__" // level
      else
-     record = "QNIFA_" // month(1) // "__0" // level
+       record = "QNIFA_" // month(mm) // "__0" // level
      endif
-     print*,'record is ',record
+     print*,'reading record ',record
      error=nf90_inq_varid(ncid, trim(record), id_var)
      call netcdf_err(error, 'reading  field id' )
-     error=nf90_get_var(ncid, id_var, dummy3d(:,:,k))
+     error=nf90_get_var(ncid, id_var, dummy4d(:,:,k,mm))
      call netcdf_err(error, 'reading field' )
-     print*,'maxval ', record,  maxval(dummy3d(:,:,k)),minval(dummy3d(:,:,k))
+     print*,'maxval ', record,  maxval(dummy4d(:,:,k,mm)),minval(dummy4d(:,:,k,mm))
    enddo
+   enddo
+   dummy3d(:,:,:) = wei1m * dummy4d(:,:,:,mon1) + wei2m * dummy4d(:,:,:,mon2)
  endif
 
  print*,"- CALL FieldScatter FOR qnifa input climo."
@@ -181,20 +228,23 @@
     call error_handler("IN FieldScatter", rc)
 
  if (localpet == 0) then
+   do mm = mon1, mon2
    do k = 1, lev_thomp_mp_climo
      write(level, "(I2.2)") k
      if (k < 10) then
-     record = "QNWFA_" // month(1) // "__" // level
+       record = "QNWFA_" // month(mm) // "__" // level
      else
-     record = "QNWFA_" // month(1) // "__0" // level
+       record = "QNWFA_" // month(mm) // "__0" // level
      endif
-     print*,'record is ',record
+     print*,'reading record ',record
      error=nf90_inq_varid(ncid, trim(record), id_var)
      call netcdf_err(error, 'reading  field id' )
-     error=nf90_get_var(ncid, id_var, dummy3d(:,:,k))
+     error=nf90_get_var(ncid, id_var, dummy4d(:,:,k,mm))
      call netcdf_err(error, 'reading field' )
-     print*,'maxval ', record,  maxval(dummy3d(:,:,k)),minval(dummy3d(:,:,k))
+     print*,'maxval ', record,  maxval(dummy4d(:,:,k,mm)),minval(dummy4d(:,:,k,mm))
    enddo
+   enddo
+   dummy3d(:,:,:) = wei1m * dummy4d(:,:,:,mon1) + wei2m * dummy4d(:,:,:,mon2)
  endif
 
  print*,"- CALL FieldScatter FOR qnwfa input climo."
@@ -203,20 +253,23 @@
     call error_handler("IN FieldScatter", rc)
 
  if (localpet == 0) then
+   do mm = mon1, mon2
    do k = 1, lev_thomp_mp_climo
      write(level, "(I2.2)") k
      if (k < 10) then
-     record = "P_WIF_" // month(1) // "__" // level
+       record = "P_WIF_" // month(mm) // "__" // level
      else
-     record = "P_WIF_" // month(1) // "__0" // level
+       record = "P_WIF_" // month(mm) // "__0" // level
      endif
-     print*,'record is ',record
+     print*,'reading record ',record
      error=nf90_inq_varid(ncid, trim(record), id_var)
      call netcdf_err(error, 'reading  field id' )
-     error=nf90_get_var(ncid, id_var, dummy3d(:,:,k))
+     error=nf90_get_var(ncid, id_var, dummy4d(:,:,k,mm))
      call netcdf_err(error, 'reading field' )
-     print*,'maxval ', record,  maxval(dummy3d(:,:,k)),minval(dummy3d(:,:,k))
+     print*,'maxval ', record,  maxval(dummy4d(:,:,k,mm)),minval(dummy4d(:,:,k,mm))
    enddo
+   enddo
+   dummy3d(:,:,:) = wei1m * dummy4d(:,:,:,mon1) + wei2m * dummy4d(:,:,:,mon2)
  endif
 
  print*,"- CALL FieldScatter FOR thomp press."
@@ -225,7 +278,7 @@
     call error_handler("IN FieldScatter", rc)
  error=nf90_close(ncid)
 
- deallocate(dummy3d)
+ deallocate(dummy3d, dummy4d)
 
  end subroutine read_thomp_mp_data
 
