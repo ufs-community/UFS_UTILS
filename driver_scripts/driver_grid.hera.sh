7371edaf3b7c94b5557c254296e1d17a68f7c5b1
GeorgeGayno-NOAA <52789452+GeorgeGayno-NOAA@users.noreply.github.com>
2020-08-13 12:11:51 -0400

    Add extended Schmidt gnomonic grid capability
    Add capability to create regional extended Schmidt gnomonic 
    grids (from Jim Purser).  
    
    Includes new program to compute the global equivalent resolution
    for a regional grid (from Gerard).
    
    Move programs filter_topo, global_equiv_resol,
    regional_esg_grid and shave from fre-nctools.fd to its
    own directory.  fre-nctools.fd should only contain the GFDL
    tools.
    
    For details, see #4 and #125

--- driver_scripts/driver_grid.hera.sh
+++ driver_scripts/driver_grid.hera.sh
@@ -33,19 +33,24 @@
 #         "uniform"  - global uniform grid
 #         "stretch"  - global stretched grid
 #         "nest"     - global stretched grid with nest
-#         "regional" - stand-alone regional grid
+#         "regional_gfdl" - stand-alone gfdl regional grid
+#         "regional_esg"  - stand-alone extended Schmidt gnomonic
+#                           (esg) regional grid
 #   3) For "stretch" and "nest" grids, set the stretching factor -
 #       "stretch_fac", and center lat/lon of highest resolution
 #      tile - "target_lat" and "target_lon".
 #   4) For "nest" grids, set the refinement ratio - "refine_ratio",
 #      the starting/ending i/j index location within the parent
 #      tile - "istart_nest", "jstart_nest", "iend_nest", "jend_nest"
-#   5) For "regional" grids, set the "halo".  Default is three
+#   5) For "regional_gfdl" grids, set the "halo".  Default is three
 #      rows/columns.
-#   6) Set working directory - TMPDIR - and path to the repository
+#   6) For "regional_esg" grids, set center lat/lon of grid,
+#      - "target_lat/lon" - the i/j dimensions - "i/jdim", the
+#      x/y grid spacing - "delx/y", and halo.
+#   7) Set working directory - TMPDIR - and path to the repository
 #      clone - home_dir.
-#   7) Submit script: "sbatch $script".
-#   8) All files will be placed in "out_dir".
+#   8) Submit script: "sbatch $script".
+#   9) All files will be placed in "out_dir".
 #
 #-----------------------------------------------------------------------
 
@@ -59,14 +64,18 @@ module list
 # Set grid specs here.
 #-----------------------------------------------------------------------
 
-export res=96
-export gtype=uniform  # 'uniform', 'stretch', 'nest', or 'regional'
+export gtype=uniform       # 'uniform', 'stretch', 'nest'
+                           # 'regional_gfdl', 'regional_esg'
 
-if [ $gtype = stretch ]; then
+if [ $gtype = uniform ]; then
+  export res=96
+elif [ $gtype = stretch ]; then
+  export res=96
   export stretch_fac=1.5       # Stretching factor for the grid
   export target_lon=-97.5      # Center longitude of the highest resolution tile
   export target_lat=35.5       # Center latitude of the highest resolution tile
-elif [ $gtype = nest ] || [ $gtype = regional ]; then
+elif [ $gtype = nest ] || [ $gtype = regional_gfdl ]; then
+  export res=96
   export stretch_fac=1.5       # Stretching factor for the grid
   export target_lon=-97.5      # Center longitude of the highest resolution tile
   export target_lat=35.5       # Center latitude of the highest resolution tile
@@ -76,6 +85,19 @@ elif [ $gtype = nest ] || [ $gtype = regional ]; then
   export iend_nest=166         # Ending i-direction index of nest grid in parent tile supergrid
   export jend_nest=164         # Ending j-direction index of nest grid in parent tile supergrid
   export halo=3                # Lateral boundary halo
+elif [ $gtype = regional_esg ] ; then
+  export res=-999              # equivalent res is computed.
+  export target_lon=-97.5      # Center longitude of grid
+  export target_lat=35.5       # Center latitude of grid
+  export idim=301              # Dimension of grid in 'i' direction
+  export jdim=200              # Dimension of grid in 'j' direction
+  export delx=0.0585           # Grid spacing (in degrees) in the 'i' direction
+                               # on the SUPERGRID (which has twice the resolution of
+                               # the model grid).  The physical grid spacing in the 'i'
+                               # direction is related to delx as follows:
+                               #    distance = 2*delx*(circumf_Earth/360 deg)
+  export dely=0.0585           # Grid spacing (in degrees) in the 'j' direction.
+  export halo=3                # number of row/cols for halo
 fi
 
 #-----------------------------------------------------------------------
@@ -87,7 +109,7 @@ fi
 
 export home_dir=$SLURM_SUBMIT_DIR/..
 export TMPDIR=/scratch2/NCEPDEV/stmp1/$LOGNAME/fv3_grid.$gtype
-export out_dir=/scratch2/NCEPDEV/stmp1/$LOGNAME/C${res}
+export out_dir=/scratch2/NCEPDEV/stmp1/$LOGNAME/my_grids
 
 #-----------------------------------------------------------------------
 # Should not need to change anything below here.
