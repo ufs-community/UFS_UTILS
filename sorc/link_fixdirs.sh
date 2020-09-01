2c5e7ee4629f2b20f311f7a41cf9e7d88a4413dd
George Gayno <George.Gayno@noaa.gov>
2020-09-01 13:04:39 +0000

    release/ops-gfsv16  Update link_fixdirs.sh per Fanglin's request.
    Issue #26.

--- sorc/link_fixdirs.sh
+++ sorc/link_fixdirs.sh
@@ -48,7 +48,10 @@ elif [ $machine = "orion" ]; then
 fi
 cd ${pwd}/../fix                ||exit 8
 for dir in fix_am fix_fv3 fix_orog fix_fv3_gmted2010 fix_sfc_climo; do
-    [[ -d $dir ]] && rm -rf $dir
+    if [ -d $dir ]; then
+      [[ $RUN_ENVIR = nco ]] && chmod -R 755 $dir
+      rm -rf $dir
+    fi
     $LINK $FIX_DIR/$dir  .
 done
 
