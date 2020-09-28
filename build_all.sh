65cc1c4a705676747527eea3698a0a36824e8c3f
Larissa Reames <larissa.reames@noaa.gov>
2020-09-28 19:36:24 +0000

    Syntax fix. Also fixing a change that got overwritten in a previous merge that allowed cmake build to work on Odin.

--- build_all.sh
+++ build_all.sh
@@ -27,7 +27,7 @@ cd ./build
 
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON"
 
-if [[ "$target" != "wcoss_cray" ]]; then
+if [[ "$target" != "wcoss_cray" && "$target" != "odin" ]]; then
   CMAKE_FLAGS+=" -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc"
 fi
 
