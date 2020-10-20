633d536a900900194f06aee552a8fe90f85d9e36
George Gayno <George.Gayno@noaa.gov>
2020-10-20 16:00:42 +0000

    feature/gnu  Update grid driver scripts and gdas init utility scripts for new build module names.
    Issue #151.

--- driver_scripts/driver_grid.orion.sh
+++ driver_scripts/driver_grid.orion.sh
@@ -60,7 +60,7 @@ set -x
 
 source ../sorc/machine-setup.sh > /dev/null 2>&1
 module use ../modulefiles
-module load build.$target
+module load build.$target.intel
 module list
 
 #-----------------------------------------------------------------------
