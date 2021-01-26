58c6f14095ed9df70cc66344cab125a0e7dd2544
George Gayno <George.Gayno@noaa.gov>
2021-01-26 21:20:57 +0000

    feature/gdas_init.v16
    Some script clean up.
    
    Issue #102

--- util/gdas_init/get_v16.data.sh
+++ util/gdas_init/get_v16.data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #----------------------------------------------------------------------
-# Retrieve gfs v16 data from Eric's tarballs.
+# Retrieve gfs v16 data from Eric's tarballs, which should match
+# the official OPS v16 tarballs.
 #----------------------------------------------------------------------
 
 bundle=$1
@@ -19,8 +20,8 @@ dd_m6=$(echo $date10_m6 | cut -c7-8)
 hh_m6=$(echo $date10_m6 | cut -c9-10)
 
 #----------------------------------------------------------------------
-# Get the hires tiled restart files.  Need to use the 6-hour forecast files from
-# the previous cycle.  
+# Get the atm and sfc 'anl' netcdf files from the gfs or gdas
+# tarball.
 #----------------------------------------------------------------------
 
 if [ "$bundle" = "gdas" ] || [ "$bundle" = "gfs" ]; then
@@ -49,7 +50,7 @@ if [ "$bundle" = "gdas" ] || [ "$bundle" = "gfs" ]; then
   rm -f ./list.hires*
 
 #----------------------------------------------------------------------
-# Get the 'abias' files from current cycle
+# Get the 'abias' and radstat files when processing 'gdas'.
 #----------------------------------------------------------------------
 
   if [ "$bundle" = "gdas" ] ; then
@@ -76,30 +77,29 @@ if [ "$bundle" = "gdas" ] || [ "$bundle" = "gfs" ]; then
   fi
 
 #----------------------------------------------------------------------
-# Get the enkf tiled restart files for all members.
+# Get the enkf netcdf history files.  They are not saved for the
+# current cycle.  So get the 6-hr forecast files from the
+# previous cycle.
 #----------------------------------------------------------------------
 
 else
 
-  for group in $bundle
-  do
+  group=$bundle
 
-    directory=/NCEPDEV/emc-meso/2year/Eric.Rogers/5year/rh${yy_m6}/${yy_m6}${mm_m6}/${yy_m6}${mm_m6}${dd_m6}
-    file=com_gfs_para_enkfgdas.${yy_m6}${mm_m6}${dd_m6}_${hh_m6}.enkfgdas_${group}.tar
+  directory=/NCEPDEV/emc-meso/2year/Eric.Rogers/5year/rh${yy_m6}/${yy_m6}${mm_m6}/${yy_m6}${mm_m6}${dd_m6}
+  file=com_gfs_para_enkfgdas.${yy_m6}${mm_m6}${dd_m6}_${hh_m6}.enkfgdas_${group}.tar
 
-    rm -f ./list*.${group}
-    htar -tvf  $directory/$file > ./list1.${group}
-    grep "006.nc" ./list1.${group} > ./list2.${group}
-    while read -r line
-    do 
-      echo ${line##*' '} >> ./list3.${group}
-    done < "./list2.${group}"
-    htar -xvf $directory/$file  -L ./list3.${group}
-    rc=$?
-    [ $rc != 0 ] && exit $rc
-    rm -f ./list*.${group}
-
-  done
+  rm -f ./list*.${group}
+  htar -tvf  $directory/$file > ./list1.${group}
+  grep "006.nc" ./list1.${group} > ./list2.${group}
+  while read -r line
+  do 
+    echo ${line##*' '} >> ./list3.${group}
+  done < "./list2.${group}"
+  htar -xvf $directory/$file  -L ./list3.${group}
+  rc=$?
+  [ $rc != 0 ] && exit $rc
+  rm -f ./list*.${group}
 
 fi
 
