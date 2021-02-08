38c5dc89d33d7bb8a22ab9328a87e599734676bc
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2021-02-08 08:35:03 -0700

    adding test code

---
+++ tests/ftst_input_data.F90
@@ -0,0 +1,13 @@
+  ! This is a test for UFS_UTILS.
+  !
+  ! This tests the input_data module.
+  !
+  ! Ed Hartnett, 2/8/21
+program ftst_input_data
+
+  print *,'Testing input_data module...'
+
+  
+  print *,'SUCCESS!'
+
+end program ftst_input_data
