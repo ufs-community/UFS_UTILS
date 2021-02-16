be80f7731b5f3672608f43bb6bafeefa403d0fe0
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-02-16 09:35:26 -0700

    adding test

---
+++ tests/chres_cube/ftst_input_data.F90
@@ -0,0 +1,14 @@
+! Unit test for chres_cube utility, input_data.F90.
+!
+! Ed Hartnett 2/16/21
+ 
+program ftst_input_data
+ implicit none
+
+ print*, "Starting test of input_data."
+ print*, "testing ..."
+
+ print*, "OK"
+ 
+ print*, "SUCCESS!"
+end program ftst_input_data
