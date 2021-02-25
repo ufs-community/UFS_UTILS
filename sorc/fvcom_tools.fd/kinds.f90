18d7fb72039dcd0ac5a97aaebbf3983a6545b1fb
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-02-25 07:44:01 -0700

    updated docs

--- sorc/fvcom_tools.fd/kinds.f90
+++ sorc/fvcom_tools.fd/kinds.f90
@@ -17,15 +17,15 @@ module kinds
 
 ! Integer types
   integer, parameter, public  :: i_kind = 4 !< generic specification kind for default integer.
-  integer, parameter, public  :: i_short = 2
-  integer, parameter, public  :: i_byte  = 1
+  integer, parameter, public  :: i_short = 2 !< generic specification kind for default short.
+  integer, parameter, public  :: i_byte  = 1 !< generic specification kind for default byte.
 ! Real types
   integer, parameter, public  :: r_single = 4  !< specification kind for single precision (4-byte) real variable.
   integer, parameter, public  :: r_kind = 8 !< generic specification kind for default floating point
 
 !
-  real(r_single),parameter,public :: rmissing=-99999.0
-  real(i_kind),parameter,public :: imissing=-99999
-  real(r_kind),parameter,public :: drmissing=-99999.0
+  real(r_single),parameter,public :: rmissing=-99999.0 !< Fill value for single real missing data.
+  real(i_kind),parameter,public :: imissing=-99999 !< Fill value for integer missing data.
+  real(r_kind),parameter,public :: drmissing=-99999.0 !< Fill value for double real missing data.
 
 end module kinds
