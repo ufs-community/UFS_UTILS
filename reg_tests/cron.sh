7ab423267c7dfac7cc4d337e3eed9371f397e9d4
kgerheiser <kgerheiser@icloud.com>
2021-03-18 14:31:44 -0400

    Use correct path

--- reg_tests/cron.sh
+++ reg_tests/cron.sh
@@ -46,7 +46,7 @@ for dir in chgres_cube grid_gen; do
     ./driver.$target.sh
 
     # Wait for each reg-test to finish before submitting more jobs
-    while [ ! -f "${dir}/summary.log" ]; do
+    while [ ! -f "summary.log" ]; do
         sleep 10
     done
     
