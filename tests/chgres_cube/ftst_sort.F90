02e3cdda922cad783a5e4795268f98567c0d79f1
George Gayno <George.Gayno@noaa.gov>
2021-04-30 15:49:57 +0000

    Updates to test.
    Part of #256

--- tests/chgres_cube/ftst_sort.F90
+++ tests/chgres_cube/ftst_sort.F90
@@ -1,20 +1,31 @@
- program sort
+ program quicksort
+
+! Test the quicksort function of the chgres_cube program.
+! The routine sorts numbers in ascending order. Sort
+! an array of pressure levels in millibars.
 
  use input_data
 
  implicit none
 
- real*8 :: a(5)
+ real*8 :: pressure_in_mb(5), sorted_pressure_in_mb(5)
  integer :: first, last
 
- data a /300.0, 600.0, 200.0, 50.0, 1000.0/
+ data pressure_in_mb /300.0, 600.0, 200.0, 50.0, 1000.0/
+ data sorted_pressure_in_mb /50.0, 200.0, 300.0, 600.0, 1000.0/
 
- print*,'hello world'
+ print*,'Starting test of quicksort function.'
 
  first = 1
  last = 5
- call quicksort(a,first,last)
+ call quicksort(pressure_in_mb,first,last)
 
- print*,'after sort 1 ',a,first,last
+ if (pressure_in_mb(1) /= sorted_pressure_in_mb(1)) stop 2
+ if (pressure_in_mb(2) /= sorted_pressure_in_mb(2)) stop 3
+ if (pressure_in_mb(3) /= sorted_pressure_in_mb(3)) stop 4
+ if (pressure_in_mb(4) /= sorted_pressure_in_mb(4)) stop 5
+ if (pressure_in_mb(5) /= sorted_pressure_in_mb(5)) stop 6
 
- end program sort
+ print*,"SUCCESS!"
+
+ end program quicksort
