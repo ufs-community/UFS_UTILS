e2f2e86a7ceeb4b63da072005903a0f04f03a884
George Gayno <George.Gayno@noaa.gov>
2021-05-05 20:52:11 +0000

    Updates to test.
    Part of #256

--- sorc/chgres_cube.fd/input_data.F90
+++ sorc/chgres_cube.fd/input_data.F90
@@ -56,8 +56,8 @@
  type(esmf_field), public              :: ps_input_grid         !< surface pressure
  type(esmf_field), public              :: terrain_input_grid    !< terrain height
  type(esmf_field), public              :: temp_input_grid       !< temperature
- type(esmf_field)                      :: u_input_grid          !< u/v wind at grid
- type(esmf_field)                      :: v_input_grid          !< box center
+ type(esmf_field), public              :: u_input_grid          !< u/v wind at grid
+ type(esmf_field), public              :: v_input_grid          !< box center
  type(esmf_field), public              :: wind_input_grid       !< 3-component wind
  type(esmf_field), allocatable, public :: tracers_input_grid(:) !< tracers
 
@@ -134,6 +134,7 @@
  public :: cleanup_input_nst_data
  public :: check_soilt
  public :: quicksort
+ public :: convert_winds
  
  contains
 
--- tests/chgres_cube/ftst_convert_winds.F90
+++ tests/chgres_cube/ftst_convert_winds.F90
@@ -3,20 +3,28 @@
  use esmf
 
  use model_grid, only : i_input, j_input, &
-                        input_grid
+                        input_grid, &
+                        latitude_input_grid, &
+                        longitude_input_grid
+
+ use input_data, only : lev_input, convert_winds, &
+                        wind_input_grid, &
+                        u_input_grid, &
+                        v_input_grid
 
  implicit none
 
  integer                      :: ierr, localpet, npets, rc
+ integer                      :: i, j, k
 
  real(esmf_kind_r8), allocatable  :: latitude(:,:)
  real(esmf_kind_r8), allocatable  :: longitude(:,:)
+ real(esmf_kind_r8), allocatable  :: u_wind(:,:,:)
+ real(esmf_kind_r8), allocatable  :: v_wind(:,:,:)
+ real(esmf_kind_r8), pointer      :: windptr(:,:,:,:)
 
  type(esmf_vm)                :: vm
  type(esmf_polekind_flag)     :: polekindflag(2)
- type(esmf_field) :: latitude_input_grid
- type(esmf_field) :: longitude_input_grid
- type(esmf_field) :: wind_input_grid
 
  call mpi_init(ierr)
 
@@ -26,8 +34,9 @@
 
  call ESMF_VMGet(vm, localPet=localpet, petCount=npets, rc=ierr)
 
- i_input = 3
- j_input = 3
+ i_input = 4
+ j_input = 2
+ lev_input = 1
  
  print*,'wind check ',i_input, j_input
 
@@ -42,6 +51,7 @@
                                    coordSys=ESMF_COORDSYS_SPH_DEG, &
                                    regDecomp=(/1,npets/),  &
                                    indexflag=ESMF_INDEX_GLOBAL, rc=rc)
+ print*,'rc ',rc
 
  print*,"- CALL FieldCreate FOR INPUT GRID LATITUDE."
  latitude_input_grid = ESMF_FieldCreate(input_grid, &
@@ -49,6 +59,7 @@
                                    staggerloc=ESMF_STAGGERLOC_CENTER, &
                                    name="input_grid_latitude", &
                                    rc=rc)
+ print*,'rc ',rc
 
  print*,"- CALL FieldCreate FOR INPUT GRID LONGITUDE."
  longitude_input_grid = ESMF_FieldCreate(input_grid, &
@@ -56,22 +67,102 @@
                                    staggerloc=ESMF_STAGGERLOC_CENTER, &
                                    name="input_grid_longitude", &
                                    rc=rc)
+ print*,'rc ',rc
 
- allocate(latitude(i_input,j_input))
- allocate(longitude(i_input,j_input))
-
- latitude = 55.
- longitude = 180.
-
- call ESMF_FieldScatter(longitude_input_grid, longitude, rootpet=0, rc=rc)
- call ESMF_FieldScatter(latitude_input_grid, latitude, rootpet=0, rc=rc)
 
  print*,"- CALL FieldCreate FOR INPUT wind."
  wind_input_grid = ESMF_FieldCreate(input_grid, &
                                    typekind=ESMF_TYPEKIND_R8, &
                                    staggerloc=ESMF_STAGGERLOC_CENTER, &
                                    ungriddedLBound=(/1,1/), &
-                                   ungriddedUBound=(/1,3/), rc=rc)
+                                   ungriddedUBound=(/lev_input,3/), rc=rc)
+ print*,'rc ',rc
+
+ u_input_grid = ESMF_FieldCreate(input_grid, &
+                                 typekind=ESMF_TYPEKIND_R8, &
+                                 staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                 ungriddedLBound=(/1/), &
+                                 ungriddedUBound=(/lev_input/), rc=rc)
+ print*,'rc ',rc
+
+ v_input_grid = ESMF_FieldCreate(input_grid, &
+                                 typekind=ESMF_TYPEKIND_R8, &
+                                 staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                 ungriddedLBound=(/1/), &
+                                 ungriddedUBound=(/lev_input/), rc=rc)
+ print*,'rc ',rc
+
+ allocate(latitude(i_input,j_input))
+ allocate(longitude(i_input,j_input))
+
+ allocate(u_wind(i_input,j_input,lev_input))
+ allocate(v_wind(i_input,j_input,lev_input))
+
+ latitude(1,1) = 0.
+ longitude(1,1) = 0.
+ u_wind(1,1,1) = 1.0
+ v_wind(1,1,1) = 0.0
+
+ latitude(2,1) = 0.
+ longitude(2,1) = 90.
+ u_wind(2,1,1) = 1.0
+ v_wind(2,1,1) = 0.0
+
+ latitude(3,1) = 0.
+ longitude(3,1) = 180.
+ u_wind(3,1,1) = 1.0
+ v_wind(3,1,1) = 0.0
+
+ latitude(4,1) = 0.
+ longitude(4,1) = 270.
+ u_wind(4,1,1) = 1.0
+ v_wind(4,1,1) = 0.0
+
+ latitude(1,2) = 0.
+ longitude(1,2) = 0.
+ u_wind(1,2,1) = 0.0
+ v_wind(1,2,1) = 1.0
+
+ latitude(2,2) = 0.
+ longitude(2,2) = 90.
+ u_wind(2,2,1) = 0.0
+ v_wind(2,2,1) = 1.0
+
+ latitude(3,2) = 0.
+ longitude(3,2) = 180.
+ u_wind(3,2,1) = 0.0
+ v_wind(3,2,1) = 1.0
+
+ latitude(4,2) = 0.
+ longitude(4,2) = 270.
+ u_wind(4,2,1) = 0.0
+ v_wind(4,2,1) = 1.0
+
+ call ESMF_FieldScatter(u_input_grid, u_wind, rootpet=0, rc=rc)
+ print*,'rc ',rc
+ call ESMF_FieldScatter(v_input_grid, v_wind, rootpet=0, rc=rc)
+ print*,'rc ',rc
+ call ESMF_FieldScatter(longitude_input_grid, longitude, rootpet=0, rc=rc)
+ print*,'rc ',rc
+ call ESMF_FieldScatter(latitude_input_grid, latitude, rootpet=0, rc=rc)
+ print*,'rc ',rc
+
+ call convert_winds
+
+ print*,"- CALL FieldGet FOR 3-D WIND."
+ call ESMF_FieldGet(wind_input_grid, &
+                    farrayPtr=windptr, rc=rc)
+ print*,'rc ',rc
+
+ do j = 1, j_input
+ do i = 1, i_input
+ do k = 1, lev_input
+   print*,'wind ', i,j,k,windptr(i,j,k,:)
+ enddo
+ enddo
+ enddo
+
+ deallocate(u_wind, v_wind, latitude, longitude)
 
  print*,"SUCCESS!"
 
