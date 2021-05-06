3d76cbd842927a3b927b45ca46d3fa2fbb434485
George Gayno <George.Gayno@noaa.gov>
2021-05-06 13:58:28 +0000

    Add test points.
    Part of #256

--- tests/chgres_cube/ftst_convert_winds.F90
+++ tests/chgres_cube/ftst_convert_winds.F90
@@ -35,8 +35,8 @@
  call ESMF_VMGet(vm, localPet=localpet, petCount=npets, rc=ierr)
 
  i_input = 4
- j_input = 2
- lev_input = 1
+ j_input = 3
+ lev_input = 1 ! number of vertical levels
  
  print*,'wind check ',i_input, j_input
 
@@ -98,45 +98,89 @@
  allocate(u_wind(i_input,j_input,lev_input))
  allocate(v_wind(i_input,j_input,lev_input))
 
+! Test point 1 - A pure west wind at the Equator/Greenwich.
+
  latitude(1,1) = 0.
  longitude(1,1) = 0.
  u_wind(1,1,1) = 1.0
  v_wind(1,1,1) = 0.0
+
+! Test point 2 - A pure west wind at the Equator/90 degrees E longitude.
 
  latitude(2,1) = 0.
  longitude(2,1) = 90.
  u_wind(2,1,1) = 1.0
  v_wind(2,1,1) = 0.0
 
+! Test point 3 - A pure west wind at the Equator/dateline.
+
  latitude(3,1) = 0.
  longitude(3,1) = 180.
  u_wind(3,1,1) = 1.0
  v_wind(3,1,1) = 0.0
+
+! Test point 4 - A pure west wind at the Equator/90 degrees W longitude.
 
  latitude(4,1) = 0.
  longitude(4,1) = 270.
  u_wind(4,1,1) = 1.0
  v_wind(4,1,1) = 0.0
 
+! Test point 5 - A pure south wind at the Equator/Greenwich.
+
  latitude(1,2) = 0.
  longitude(1,2) = 0.
  u_wind(1,2,1) = 0.0
  v_wind(1,2,1) = 1.0
+
+! Test point 6 - A pure south wind at the Equator/90 degrees E longitude.
 
  latitude(2,2) = 0.
  longitude(2,2) = 90.
  u_wind(2,2,1) = 0.0
  v_wind(2,2,1) = 1.0
 
+! Test point 7 - A pure south wind at the Equator/dateline.
+
  latitude(3,2) = 0.
  longitude(3,2) = 180.
  u_wind(3,2,1) = 0.0
  v_wind(3,2,1) = 1.0
 
+! Test point 8 - A pure south wind at the Equator/90 degrees W longitude.
+
  latitude(4,2) = 0.
  longitude(4,2) = 270.
  u_wind(4,2,1) = 0.0
  v_wind(4,2,1) = 1.0
+
+! Test point 9 - A pure south wind at the North pole/Greenwich.
+
+ latitude(1,3) = 90.
+ longitude(1,3) = 0.
+ u_wind(1,3,1) = 0.0
+ v_wind(1,3,1) = 1.0
+
+! Test point 10 - A pure south wind at the South pole/Greenwich.
+
+ latitude(2,3) = -(90.0)
+ longitude(2,3) = 0.
+ u_wind(2,3,1) = 0.0
+ v_wind(2,3,1) = 1.0
+
+! Test point 11 - A west wind at the 45 degrees N/Greenwich.
+
+ latitude(3,3) = 45.
+ longitude(3,3) = 0.
+ u_wind(3,3,1) = 1.0
+ v_wind(3,3,1) = 0.0
+
+! Test point 12 - A south wind at 45 degrees N/Dateline.
+
+ latitude(4,3) = 45.
+ longitude(4,3) = 180.
+ u_wind(4,3,1) = 0.0
+ v_wind(4,3,1) = 1.0
 
  call ESMF_FieldScatter(u_input_grid, u_wind, rootpet=0, rc=rc)
  print*,'rc ',rc
@@ -163,6 +207,9 @@
  enddo
 
  deallocate(u_wind, v_wind, latitude, longitude)
+
+ call ESMF_finalize(endflag=ESMF_END_KEEPMPI)
+ call mpi_finalize(rc)
 
  print*,"SUCCESS!"
 
