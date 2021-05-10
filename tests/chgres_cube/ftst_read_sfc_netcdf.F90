7326cd0437b9fd2917772b8b840f659a6c6f8032
George Gayno <George.Gayno@noaa.gov>
2021-05-10 21:00:22 +0000

    Update to test.
    Fixes #510
    Part of #256

--- tests/chgres_cube/ftst_read_sfc_netcdf.F90
+++ tests/chgres_cube/ftst_read_sfc_netcdf.F90
@@ -17,8 +17,27 @@
                         num_tiles_input_grid
 
  use input_data, only : read_input_sfc_data, &
+                        lsoil_input, &
                         landsea_mask_input_grid, &
-                        terrain_input_grid
+                        terrain_input_grid, &
+                        soilm_liq_input_grid, &
+                        soilm_tot_input_grid, &
+                        soil_temp_input_grid, &
+                        seaice_fract_input_grid, &
+                        seaice_depth_input_grid, &
+                        seaice_skin_temp_input_grid, &
+                        snow_liq_equiv_input_grid, &
+                        snow_depth_input_grid, &
+                        veg_type_input_grid, &
+                        soil_type_input_grid, &
+                        t2m_input_grid, &
+                        q2m_input_grid, &
+                        tprcp_input_grid, &
+                        f10m_input_grid, &
+                        ffmm_input_grid, &
+                        ustar_input_grid, &
+                        srflag_input_grid, &
+                        skin_temp_input_grid
 
  use program_setup, only : input_type, &
                            data_dir_input_grid, &
@@ -36,6 +55,7 @@
  integer :: rc, localpet, npets
 
  real(esmf_kind_r8), allocatable    :: data_one_tile(:,:)
+ real(esmf_kind_r8), allocatable    :: data_one_tile_3d(:,:,:)
 
 ! The expected values were determined by the checking
 ! the input NetCDF history file using 'ncdump'.
@@ -43,9 +63,17 @@
 
  real :: landsea_mask_expected_values(NUM_VALUES)
  real :: terrain_expected_values(NUM_VALUES)
+ real :: soilm_liq1_expected_values(NUM_VALUES)
+ real :: soilm_liq2_expected_values(NUM_VALUES)
+ real :: soilm_liq3_expected_values(NUM_VALUES)
+ real :: soilm_liq4_expected_values(NUM_VALUES)
 
  data landsea_mask_expected_values /1.0, 1.0/
  data terrain_expected_values /319.5036, 405.1155/
+ data soilm_liq1_expected_values / 0.33374, 0.33583/
+ data soilm_liq2_expected_values / 0.33563, 0.33659/
+ data soilm_liq3_expected_values / 0.33697, 0.33554/
+ data soilm_liq4_expected_values / 0.33997, 0.33849/
 
  print*,"Starting test of read_input_sfc_netcdf_file."
 
@@ -80,6 +108,7 @@
  call read_input_sfc_data(localpet)
  
  allocate(data_one_tile(i_input,j_input))
+ allocate(data_one_tile_3d(i_input,j_input,lsoil_input))
 
  call ESMF_FieldGather(landsea_mask_input_grid, data_one_tile, rootPet=0, rc=rc)
  if (localpet == 0) then
@@ -95,9 +124,110 @@
    if (abs(data_one_tile(i_input,j_input) - terrain_expected_values(2)) > EPSILON) stop 8
  endif
 
+ call ESMF_FieldGather(soilm_liq_input_grid, data_one_tile_3d, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'slc 1 ',data_one_tile_3d(1,1,:)
+   print*,'slc last ',data_one_tile_3d(i_input,j_input,:)
+   if (abs(data_one_tile_3d(1,1,1) - soilm_liq1_expected_values(1)) > EPSILON) stop 10
+   if (abs(data_one_tile_3d(i_input,j_input,1) - soilm_liq1_expected_values(2)) > EPSILON) stop 11
+   if (abs(data_one_tile_3d(1,1,2) - soilm_liq2_expected_values(1)) > EPSILON) stop 6
+   if (abs(data_one_tile_3d(i_input,j_input,2) - soilm_liq2_expected_values(2)) > EPSILON) stop 12
+   if (abs(data_one_tile_3d(1,1,3) - soilm_liq3_expected_values(1)) > EPSILON) stop 6
+   if (abs(data_one_tile_3d(i_input,j_input,3) - soilm_liq3_expected_values(2)) > EPSILON) stop 13
+   if (abs(data_one_tile_3d(1,1,4) - soilm_liq4_expected_values(1)) > EPSILON) stop 6
+   if (abs(data_one_tile_3d(i_input,j_input,4) - soilm_liq4_expected_values(2)) > EPSILON) stop 14
+ endif
+
+ call ESMF_FieldGather(soilm_tot_input_grid, data_one_tile_3d, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'slm 1 ',data_one_tile_3d(1,1,:)
+   print*,'slm last ',data_one_tile_3d(i_input,j_input,:)
+ endif
+
+ call ESMF_FieldGather(soil_temp_input_grid, data_one_tile_3d, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'stc 1 ',data_one_tile_3d(1,1,:)
+   print*,'stc last ',data_one_tile_3d(i_input,j_input,:)
+ endif
+
+ call ESMF_FieldGather(seaice_fract_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'icef ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(seaice_depth_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'iced ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(seaice_skin_temp_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'icet ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(snow_liq_equiv_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'weasd ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(snow_depth_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'snod ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(veg_type_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'vegt ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(soil_type_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'soilt ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(t2m_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'t2m ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(q2m_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'q2m ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(tprcp_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'tprcp ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(f10m_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'f10m ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(ffmm_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'ffmm ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(ustar_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'ustar ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(srflag_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'srflag ',data_one_tile
+ endif
+
+ call ESMF_FieldGather(skin_temp_input_grid, data_one_tile, rootPet=0, rc=rc)
+ if (localpet == 0) then
+   print*,'skin t ',data_one_tile
+ endif
+
  print*,"OK"
 
- deallocate(data_one_tile)
+ deallocate(data_one_tile, data_one_tile_3d)
 
  call ESMF_finalize(endflag=ESMF_END_KEEPMPI)
 
