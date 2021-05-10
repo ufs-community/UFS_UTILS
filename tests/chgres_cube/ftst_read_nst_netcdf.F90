63edf4d7d0a552d286788285f7efc24b82b0569f
George Gayno <George.Gayno@noaa.gov>
2021-05-10 15:09:18 +0000

    Add check of both corner points.
    Fixes #506
    Part of #256

--- tests/chgres_cube/ftst_read_nst_netcdf.F90
+++ tests/chgres_cube/ftst_read_nst_netcdf.F90
@@ -5,7 +5,7 @@
 ! Reads a 9x9 version of the GFS v16 NetCDF surface history 
 ! file. This smaller version of the full file was created
 ! using the 'ncks' utility. The full file is too big for
-! Github. The data read from the file is compared to the
+! Github. The data read from the file is compared to
 ! expected values as determined from the 'ncdump' utility.
 !
 ! Author George Gayno
@@ -42,7 +42,7 @@
 
  implicit none
 
- integer, parameter :: NUM_VALUES=1
+ integer, parameter :: NUM_VALUES=2
 
  real, parameter :: EPSILON=0.00001
 
@@ -55,6 +55,7 @@
 
 ! The expected values were determined by the checking
 ! the input NetCDF history file using 'ncdump'.
+! The two values are the first and last corner points.
 
  real :: c_d_expected_values(NUM_VALUES)
  real :: c_0_expected_values(NUM_VALUES)
@@ -75,24 +76,24 @@
  real :: zc_expected_values(NUM_VALUES)
  real :: zm_expected_values(NUM_VALUES)
 
- data c_d_expected_values / -76.08778 /
- data c_0_expected_values / 0.0638366 /
- data dt_cool_expected_values / 0.412696 /
- data d_conv_expected_values / 0.0 /
- data ifd_expected_values / 1.0 /
- data qrain_expected_values / 0.00321 /
- data tref_expected_values / 302.315 /
- data wd_expected_values / 0.001898 /
- data w0_expected_values / -0.000331 /
- data xs_expected_values / 0.02394 /
- data xt_expected_values / 0.466102 /
- data xtts_expected_values / 0.05393 /
- data xu_expected_values / -0.22587 /
- data xv_expected_values / -0.094777 /
- data xz_expected_values / 7.71445 /
- data xzts_expected_values / 0.913822 /
- data zc_expected_values / 0.0008382 /
- data zm_expected_values / 0.0 /
+ data c_d_expected_values / -76.08778, -83.7614/
+ data c_0_expected_values / 0.0638366, 0.0646863 /
+ data dt_cool_expected_values / 0.412696, 0.3639118 /
+ data d_conv_expected_values / 0.0, 0.0/
+ data ifd_expected_values / 1.0, 1.0 /
+ data qrain_expected_values / 0.0, 0.003217 /
+ data tref_expected_values / 302.315, 302.54385 /
+ data wd_expected_values / 0.001898, 0.0 /
+ data w0_expected_values / -0.000331, 0.0 /
+ data xs_expected_values / 0.02394, 0.0 /
+ data xt_expected_values / 0.466102, 0.0 /
+ data xtts_expected_values / 0.05393, 0.0 /
+ data xu_expected_values / -0.22587, 0.0 /
+ data xv_expected_values / -0.094777, 0.0 /
+ data xz_expected_values / 7.71445, 30.0 /
+ data xzts_expected_values / 0.913822, 0.0 /
+ data zc_expected_values / 0.0008382, 0.0007719 /
+ data zm_expected_values / 0.0, 0.0 /
 
  print*,"Starting test of read_input_nst_netcdf_file."
 
@@ -109,7 +110,7 @@
 
  input_type = "gaussian_netcdf"
  num_tiles_input_grid = 1
- data_dir_input_grid = "/lfs4/HFIP/emcda/George.Gayno/ufs_utils.git/UFS_UTILS/tests/chgres_cube/data/"
+ data_dir_input_grid = "/scratch1/NCEPDEV/da/George.Gayno/ufs_utils.git/UFS_UTILS/tests/chgres_cube/data/"
  sfc_files_input_grid(1) = "sfc.nc"
 
  polekindflag(1:2) = ESMF_POLEKIND_MONOPOLE
@@ -121,7 +122,6 @@
                                    coordSys=ESMF_COORDSYS_SPH_DEG, &
                                    regDecomp=(/1,npets/),  &
                                    indexflag=ESMF_INDEX_GLOBAL, rc=rc)
- print*,'rc ',rc
 
 ! Call the chgres_cube read routine.
 
@@ -130,166 +130,112 @@
  allocate(data_one_tile(i_input,j_input))
 
  call ESMF_FieldGather(c_d_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'c_d ',data_one_tile
-   if (abs(data_one_tile(1,1) - c_d_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), c_d_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - c_d_expected_values(1)) > EPSILON) stop 2
+   if (abs(data_one_tile(i_input,j_input) - c_d_expected_values(2)) > EPSILON) stop 4
  endif
  call ESMF_FieldGather(c_0_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'c_0 ',data_one_tile
-   if (abs(data_one_tile(1,1) - c_0_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), c_0_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - c_0_expected_values(1)) > EPSILON) stop 6
+   if (abs(data_one_tile(i_input,j_input) - c_0_expected_values(2)) > EPSILON) stop 8
  endif
  call ESMF_FieldGather(d_conv_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'d_conv ',data_one_tile
-   if (abs(data_one_tile(1,1) - d_conv_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), d_conv_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - d_conv_expected_values(1)) > EPSILON) stop 10
+   if (abs(data_one_tile(i_input,j_input) - d_conv_expected_values(2)) > EPSILON) stop 12
  endif
  call ESMF_FieldGather(dt_cool_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'dt_cool ',data_one_tile
-   if (abs(data_one_tile(1,1) - dt_cool_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), dt_cool_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - dt_cool_expected_values(1)) > EPSILON) stop 14
+   if (abs(data_one_tile(i_input,j_input) - dt_cool_expected_values(2)) > EPSILON) stop 16
  endif
  call ESMF_FieldGather(ifd_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'ifd ',data_one_tile
-   if (abs(data_one_tile(1,1) - ifd_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), ifd_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - ifd_expected_values(1)) > EPSILON) stop 18
+   if (abs(data_one_tile(i_input,j_input) - ifd_expected_values(2)) > EPSILON) stop 20
  endif
  call ESMF_FieldGather(qrain_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'qrain ',data_one_tile
-   if (abs(data_one_tile(i_input,j_input) - qrain_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(i_input,j_input),qrain_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - qrain_expected_values(1)) > EPSILON) stop 22
+   if (abs(data_one_tile(i_input,j_input) - qrain_expected_values(2)) > EPSILON) stop 24
  endif
  call ESMF_FieldGather(tref_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'tref ',data_one_tile
-   if (abs(data_one_tile(1,1) - tref_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),tref_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - tref_expected_values(1)) > EPSILON) stop 26
+   if (abs(data_one_tile(i_input,j_input) - tref_expected_values(2)) > EPSILON) stop 28
  endif
  call ESMF_FieldGather(w_d_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'w_d ',data_one_tile
-   if (abs(data_one_tile(1,1) - wd_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),wd_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - wd_expected_values(1)) > EPSILON) stop 30
+   if (abs(data_one_tile(i_input,j_input) - wd_expected_values(2)) > EPSILON) stop 32
  endif
  call ESMF_FieldGather(w_0_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'w_0 ',data_one_tile
-   if (abs(data_one_tile(1,1) - w0_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),w0_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - w0_expected_values(1)) > EPSILON) stop 34
+   if (abs(data_one_tile(i_input,j_input) - w0_expected_values(2)) > EPSILON) stop 36
  endif
  call ESMF_FieldGather(xs_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xs ',data_one_tile
-   if (abs(data_one_tile(1,1) - xs_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xs_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xs_expected_values(1)) > EPSILON) stop 38
+   if (abs(data_one_tile(i_input,j_input) - xs_expected_values(2)) > EPSILON) stop 40
  endif
  call ESMF_FieldGather(xt_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xt ',data_one_tile
-   if (abs(data_one_tile(1,1) - xt_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xt_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xt_expected_values(1)) > EPSILON) stop 42
+   if (abs(data_one_tile(i_input,j_input) - xt_expected_values(2)) > EPSILON) stop 44
  endif
  call ESMF_FieldGather(xu_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xu ',data_one_tile
-   if (abs(data_one_tile(1,1) - xu_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xu_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xu_expected_values(1)) > EPSILON) stop 46
+   if (abs(data_one_tile(i_input,j_input) - xu_expected_values(2)) > EPSILON) stop 48
  endif
  call ESMF_FieldGather(xv_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xv ',data_one_tile
-   if (abs(data_one_tile(1,1) - xv_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xv_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xv_expected_values(1)) > EPSILON) stop 50
+   if (abs(data_one_tile(i_input,j_input) - xv_expected_values(2)) > EPSILON) stop 52
  endif
  call ESMF_FieldGather(xz_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xz ',data_one_tile
-   if (abs(data_one_tile(1,1) - xz_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xz_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xz_expected_values(1)) > EPSILON) stop 54
+   if (abs(data_one_tile(i_input,j_input) - xz_expected_values(2)) > EPSILON) stop 56
  endif
  call ESMF_FieldGather(xtts_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xtts ',data_one_tile
-   if (abs(data_one_tile(1,1) - xtts_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xtts_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xtts_expected_values(1)) > EPSILON) stop 58
+   if (abs(data_one_tile(i_input,j_input) - xtts_expected_values(2)) > EPSILON) stop 60
  endif
  call ESMF_FieldGather(xzts_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'xzts ',data_one_tile
-   if (abs(data_one_tile(1,1) - xzts_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),xzts_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - xzts_expected_values(1)) > EPSILON) stop 62
+   if (abs(data_one_tile(i_input,j_input) - xzts_expected_values(2)) > EPSILON) stop 64
  endif
  call ESMF_FieldGather(z_c_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'z_c ',data_one_tile
-   if (abs(data_one_tile(1,1) - zc_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1),zc_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - zc_expected_values(1)) > EPSILON) stop 66
+   if (abs(data_one_tile(i_input,j_input) - zc_expected_values(2)) > EPSILON) stop 68
  endif
  call ESMF_FieldGather(zm_input_grid, data_one_tile, rootPet=0, rc=rc)
- print*,'rc ',rc
  if (localpet == 0) then
    print*,'zm ',data_one_tile
-   if (abs(data_one_tile(1,1) - zm_expected_values(1)) > EPSILON) then
-     print*,'bad value ',data_one_tile(1,1), zm_expected_values(1)
-     stop
-   endif
+   if (abs(data_one_tile(1,1) - zm_expected_values(1)) > EPSILON) stop 70
+   if (abs(data_one_tile(i_input,j_input) - zm_expected_values(2)) > EPSILON) stop 72
  endif
 
  print*,"OK"
