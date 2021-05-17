b9f13e1db0097739f7e01519eb50075f82d6821e
Larissa Reames <larissa.reames@noaa.gov>
2021-05-17 15:53:31 +0000

    Removed duplicate deallocate statement that somehow made its way in to unrelated test script.

--- tests/chgres_cube/ftst_sfc_input_data.F90
+++ tests/chgres_cube/ftst_sfc_input_data.F90
@@ -135,12 +135,6 @@
  endif
  deallocate(cnwat_bad,cnwat_updated,cnwat_correct)
 
- deallocate(mask)
- deallocate(skint)
- deallocate(soilt_bad)
- deallocate(soilt_updated)
- deallocate(soilt_correct)
-
  call mpi_finalize(ierr)
 
  print*,"SUCCESS!"
