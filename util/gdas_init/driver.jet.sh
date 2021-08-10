17bbfded924cb76f4c6bb55be228efed6695edda
George Gayno <George.Gayno@noaa.gov>
2021-08-10 19:36:02 +0000

    Update to run chgres for v16 data.
    Fixes #544.

--- util/gdas_init/driver.jet.sh
+++ util/gdas_init/driver.jet.sh
@@ -157,7 +157,7 @@ if [ $RUN_CHGRES == yes ]; then
       ;;
     v16)
       sbatch --parsable --ntasks-per-node=6 --nodes=${NODES} -t $WALLT -A $PROJECT_CODE -q $QUEUE -J chgres_${CDUMP} \
-      -o log.${CDUMP} -e log.${CDUMP} ${DEPEND} run_v16.chgres.sh ${CDUMP}
+      -o log.${CDUMP} -e log.${CDUMP} --partition=${PARTITION} ${DEPEND} run_v16.chgres.sh ${CDUMP}
       ;;
   esac
 
@@ -197,7 +197,7 @@ if [ $RUN_CHGRES == yes ]; then
             ;;
           v16)
               sbatch --parsable --ntasks-per-node=12 --nodes=1 -t $WALLT -A $PROJECT_CODE -q $QUEUE -J chgres_${MEMBER_CH} \
-              -o log.${MEMBER_CH} -e log.${MEMBER_CH} ${DEPEND} run_v16.chgres.sh ${MEMBER_CH}
+              -o log.${MEMBER_CH} -e log.${MEMBER_CH} --partition=${PARTITION} ${DEPEND} run_v16.chgres.sh ${MEMBER_CH}
             ;;
         esac
         MEMBER=$(( $MEMBER + 1 ))
