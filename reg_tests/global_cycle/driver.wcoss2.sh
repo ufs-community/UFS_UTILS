ee51e7d14db71ac68768cd9b6ae82adcbf0dd5dc
George.Gayno <george.gayno@noaa.gov>
2021-09-16 15:00:21 +0000

    Minor updates to global_cycle reg test script.
    Fixes #559

--- reg_tests/global_cycle/driver.wcoss2.sh
+++ reg_tests/global_cycle/driver.wcoss2.sh
@@ -41,6 +41,7 @@ DATA_DIR="${WORK_DIR}/reg-tests/global-cycle"
 export HOMEreg=/lfs/h2/emc/eib/noscrub/George.Gayno/ufs_utils.git/reg_tests/global_cycle
 
 export OMP_NUM_THREADS_CY=2
+export OMP_PLACES=cores
 
 export APRUNCY="mpiexec -n 6 -ppn 6 --cpu-bind core --depth 2"
 
@@ -54,17 +55,17 @@ rm -f ${LOG_FILE}*
 export DATA="${DATA_DIR}/test1"
 export COMOUT=$DATA
 TEST1=$(qsub -V -o ${LOG_FILE}01 -e ${LOG_FILE}01 -q $QUEUE -A $PROJECT_CODE -l walltime=00:05:00 \
-        -N c768.fv3gfs -l select=1:ncpus=12:mem=8GB $PWD/C768.fv3gfs.sh)
+        -N c768.fv3gfs -l select=1:ncpus=24:mem=8GB $PWD/C768.fv3gfs.sh)
 
 export DATA="${DATA_DIR}/test2"
 export COMOUT=$DATA
 TEST2=$(qsub -V -o ${LOG_FILE}02 -e ${LOG_FILE}02 -q $QUEUE -A $PROJECT_CODE -l walltime=00:05:00 \
-        -N c768.lndincsoil -l select=1:ncpus=12:mem=1GB $PWD/C768.lndincsoil.sh)
+        -N c768.lndincsoil -l select=1:ncpus=24:mem=1GB $PWD/C768.lndincsoil.sh)
 
 export DATA="${DATA_DIR}/test3"
 export COMOUT=$DATA
 TEST3=$(qsub -V -o ${LOG_FILE}03 -e ${LOG_FILE}03 -q $QUEUE -A $PROJECT_CODE -l walltime=00:05:00 \
-        -N c768.lndincsnow -l select=1:ncpus=12:mem=1GB $PWD/C768.lndincsnow.sh)
+        -N c768.lndincsnow -l select=1:ncpus=24:mem=1GB $PWD/C768.lndincsnow.sh)
 
 qsub -V -o ${LOG_FILE} -e ${LOG_FILE} -q $QUEUE -A $PROJECT_CODE -l walltime=00:01:00 \
         -N cycle_summary -l select=1:ncpus=2:mem=100MB -W depend=afterok:$TEST1:$TEST2:$TEST3 << EOF
