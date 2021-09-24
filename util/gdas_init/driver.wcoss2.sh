49675fd2c3df339b125c006a643263f63fb130a0
George.Gayno <george.gayno@noaa.gov>
2021-09-24 21:16:24 +0000

    Update gdas_init driver script for new hardware configuration.
    Fixes #559.

--- util/gdas_init/driver.wcoss2.sh
+++ util/gdas_init/driver.wcoss2.sh
@@ -35,77 +35,77 @@ if [ $EXTRACT_DATA == yes ]; then
   case $gfs_ver in
     v12 | v13)
       DATAH=$(qsub -V -o log.data.${CDUMP} -e log.data.${CDUMP} -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-              -N get_${CDUMP} -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_pre-v14.data.sh ${CDUMP})
+              -N get_${CDUMP} -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_pre-v14.data.sh ${CDUMP})
       DEPEND="-W depend=afterok:$DATAH"
       if [ "$CDUMP" = "gdas" ] ; then
         DATA1=$(qsub -V -o log.data.enkf -e log.data.enkf -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-              -N get_enkf -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_pre-v14.data.sh enkf)
+              -N get_enkf -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_pre-v14.data.sh enkf)
         DEPEND="-W depend=afterok:$DATAH:$DATA1"
       fi
       ;;
     v14)
         DATAH=$(qsub -V -o log.data.${CDUMP} -e log.data.${CDUMP} -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_${CDUMP} -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v14.data.sh ${CDUMP})
+                -N get_${CDUMP} -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v14.data.sh ${CDUMP})
         DEPEND="-W depend=afterok:$DATAH"
       if [ "$CDUMP" = "gdas" ] ; then
         DATA1=$(qsub -V -o log.data.enkf -e log.data.enkf -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_enkf -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v14.data.sh enkf)
+                -N get_enkf -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v14.data.sh enkf)
         DEPEND="-W depend=afterok:$DATAH:$DATA1"
       fi
       ;;
     v15)
       if [ "$CDUMP" = "gfs" ] ; then
         DATAH=$(qsub -V -o log.data.${CDUMP} -e log.data.${CDUMP} -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_${CDUMP} -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh ${CDUMP})
+                -N get_${CDUMP} -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh ${CDUMP})
         DEPEND="-W depend=afterok:$DATAH"
       else
         DATAH=$(qsub -V -o log.data.${CDUMP} -e log.data.${CDUMP} -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_${CDUMP} -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh ${CDUMP})
+                -N get_${CDUMP} -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh ${CDUMP})
         DATA1=$(qsub -V -o log.data.grp1 -e log.data.grp1 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp1 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp1)
+                -N get_grp1 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp1)
         DATA2=$(qsub -V -o log.data.grp2 -e log.data.grp2 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp2 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp2)
+                -N get_grp2 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp2)
         DATA3=$(qsub -V -o log.data.grp3 -e log.data.grp3 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp3 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp3)
+                -N get_grp3 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp3)
         DATA4=$(qsub -V -o log.data.grp4 -e log.data.grp4 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp4 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp4)
+                -N get_grp4 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp4)
         DATA5=$(qsub -V -o log.data.grp5 -e log.data.grp5 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp5 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp5)
+                -N get_grp5 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp5)
         DATA6=$(qsub -V -o log.data.grp6 -e log.data.grp6 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp6 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp6)
+                -N get_grp6 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp6)
         DATA7=$(qsub -V -o log.data.grp7 -e log.data.grp7 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp7 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp7)
+                -N get_grp7 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp7)
         DATA8=$(qsub -V -o log.data.grp8 -e log.data.grp8 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp8 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v15.data.sh grp8)
+                -N get_grp8 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v15.data.sh grp8)
         DEPEND="-W depend=afterok:$DATAH:$DATA1:$DATA2:$DATA3:$DATA4:$DATA5:$DATA6:$DATA7:$DATA8"
       fi
       ;;
     v16retro)
       DATAH=$(qsub -V -o log.data.v16retro -e log.data.v16retro -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-              -N get_v16retro -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16retro.data.sh ${CDUMP})
+              -N get_v16retro -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16retro.data.sh ${CDUMP})
       DEPEND="-W depend=afterok:$DATAH"
       ;;
     v16)
       DATAH=$(qsub -V -o log.data.${CDUMP} -e log.data.${CDUMP} -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-              -N get_${CDUMP} -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh ${CDUMP})
+              -N get_${CDUMP} -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh ${CDUMP})
       DEPEND="-W depend=afterok:$DATAH"
       if [ "$CDUMP" = "gdas" ] ; then
         DATA1=$(qsub -V -o log.data.grp1 -e log.data.grp1 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp1 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp1)
+                -N get_grp1 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp1)
         DATA2=$(qsub -V -o log.data.grp2 -e log.data.grp2 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp2 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp2)
+                -N get_grp2 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp2)
         DATA3=$(qsub -V -o log.data.grp3 -e log.data.grp3 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp3 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp3)
+                -N get_grp3 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp3)
         DATA4=$(qsub -V -o log.data.grp4 -e log.data.grp4 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp4 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp4)
+                -N get_grp4 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp4)
         DATA5=$(qsub -V -o log.data.grp5 -e log.data.grp5 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp5 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp5)
+                -N get_grp5 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp5)
         DATA6=$(qsub -V -o log.data.grp6 -e log.data.grp6 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp5 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp6)
+                -N get_grp5 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp6)
         DATA7=$(qsub -V -o log.data.grp7 -e log.data.grp7 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp7 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp7)
+                -N get_grp7 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp7)
         DATA8=$(qsub -V -o log.data.grp8 -e log.data.grp8 -q $QUEUE -A $PROJECT_CODE -l walltime=$WALLT \
-                -N get_grp8 -l select=1:ncpus=2:mem=$MEM -- ${this_dir}/get_v16.data.sh grp8)
+                -N get_grp8 -l select=1:ncpus=1:mem=$MEM -- ${this_dir}/get_v16.data.sh grp8)
         DEPEND="-W depend=afterok:$DATAH:$DATA1:$DATA2:$DATA3:$DATA4:$DATA5:$DATA6:$DATA7:$DATA8"
       fi
       ;;
@@ -132,7 +132,7 @@ if [ $RUN_CHGRES == yes ]; then
     TASKS_PER_NODE=48
     WALLT="0:20:00"
   fi
-  (( NCPUS = TASKS_PER_NODE * 2 ))
+  NCPUS=${TASKS_PER_NODE}
   (( TASKS = NODES * TASKS_PER_NODE ))
   export APRUN="mpiexec -n $TASKS -ppn $TASKS_PER_NODE --cpu-bind core"
   case $gfs_ver in
@@ -140,7 +140,7 @@ if [ $RUN_CHGRES == yes ]; then
       export OMP_NUM_THREADS=4
       export OMP_STACKSIZE=1024M
       export OMP_PLACES=cores
-      export APRUN="$APRUN --depth 2"
+      export APRUN="$APRUN --depth ${OMP_NUM_THREADS}"
       (( NCPUS = NCPUS * OMP_NUM_THREADS ))
       qsub -V -l select=${NODES}:ncpus=${NCPUS}:ompthreads=${OMP_NUM_THREADS}:mem=${MEM} -l walltime=$WALLT -A $PROJECT_CODE -q $QUEUE \
       -N chgres_${CDUMP} -o log.${CDUMP} -e log.${CDUMP} ${DEPEND} -- ${this_dir}/run_pre-v14.chgres.sh ${CDUMP}
@@ -179,7 +179,7 @@ if [ $RUN_CHGRES == yes ]; then
     MEM=75GB
     NODES=1
     TASKS_PER_NODE=12
-    (( NCPUS = TASKS_PER_NODE * 2 ))
+    NCPUS=${TASKS_PER_NODE}
     (( TASKS = NODES * TASKS_PER_NODE))
     export APRUN="mpiexec -n $TASKS -ppn $TASKS_PER_NODE --cpu-bind core"
 
@@ -195,7 +195,7 @@ if [ $RUN_CHGRES == yes ]; then
          export OMP_NUM_THREADS=2
          export OMP_STACKSIZE=1024M
          export OMP_PLACES=cores
-         export APRUN="$APRUN --depth 2"
+         export APRUN="$APRUN --depth ${OMP_NUM_THREADS}"
          (( NCPUS = NCPUS * OMP_NUM_THREADS ))
         ;;
       esac
