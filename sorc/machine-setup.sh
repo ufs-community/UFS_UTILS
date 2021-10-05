a698ca57802e42b2bb4e535e292ac9205600724c
George.Gayno <george.gayno@noaa.gov>
2021-10-05 14:47:14 +0000

    Updates to build global_cycle on wcoss2.
    Fixes #580

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -28,6 +28,9 @@ if [[ -d /lfs3 ]] ; then
     target=jet
     module purge
     module use /lfs4/HFIP/hfv3gfs/nwprod/NCEPLIBS/modulefiles
+elif [[ -d /lfs/h1 ]]; then
+    target=wcoss2_cray
+    module purge
 elif [[ -d /scratch1 ]] ; then
     # We are on NOAA Hera
     if ( ! eval module help > /dev/null 2>&1 ) ; then
