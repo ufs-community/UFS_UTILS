a868ad0bab2a33fdb543027eca633e8c7cfd8342
dmwright526 <dmwright526@gmail.com>
2021-10-08 17:31:51 +0000

    Add ability to read restart files and choose specific time in FVCOM files

--- sorc/fvcom_tools.fd/process_FVCOM.f90
+++ sorc/fvcom_tools.fd/process_FVCOM.f90
@@ -54,7 +54,7 @@ program process_FVCOM
    integer :: lbclon, lbclat, lbctimes
    integer :: i, j, t1, t2
    integer :: num_args, ix
-   integer :: inputFVCOMsel
+   integer :: indexFVCOMsel
 
    real :: rad2deg = 180.0/3.1415926
    real :: userDX, userDY, CEN_LAT, CEN_LON
@@ -65,12 +65,13 @@ program process_FVCOM
 
    character(len=180) :: fv3file
    character(len=180) :: fvcomfile
+   character(len=180) :: wcstart
    character(len=180) :: inputFVCOMselStr
    character(len=180), dimension(:), allocatable :: args
 
    real(r_kind), allocatable :: fv3ice(:,:), fv3sst(:,:)
    real(r_kind), allocatable :: fv3sfcT(:,:), fv3mask(:,:)
-   real(r_kind), allocatable :: fv3iceT(:,:)
+   real(r_kind), allocatable :: fv3iceT(:,:), fv3sfcTl(:,:)
    real(r_kind), allocatable :: lbcice(:,:), lbcsst(:,:)
    real(r_kind), allocatable :: lbcsfcT(:,:), lbcmask(:,:)
    real(r_kind), allocatable :: lbciceT(:,:)   
@@ -101,15 +102,17 @@ if(mype==0) then
    end do
 ! fv3file: location of UFS grid
 ! fvcomfile: location of FVCOM data file
-! inputFVCOMtimes: length of time dim in FVCOM file
-! inputFVCOMsel: time index to use
+! wcstart: warm (restart) or cold start
+! inputFVCOMtimes: string of time to use
    fv3file=trim(args(1))
    write(*,*) trim(fv3file)
    fvcomfile=trim(args(2))    
    write(*,*) trim(fvcomfile)
-   inputFVCOMselStr=trim(args(3))
-   read(inputFVCOMselStr,*) inputFVCOMsel
-   write(*,*) 'select index = ', inputFVCOMsel
+   wcstart=trim(args(3))
+   write(*,*) 'warm or cold start = ', wcstart
+   inputFVCOMselStr=trim(args(4))
+!   read(inputFVCOMselStr,*) inputFVCOMsel
+   write(*,*) 'select time = ', inputFVCOMselStr
 
 !  Obtain grid parameters
 
@@ -132,6 +135,7 @@ if(mype==0) then
    allocate(fv3sst(nlon,nlat))
    allocate(fv3mask(nlon,nlat))
    allocate(fv3iceT(nlon,nlat))
+   allocate(fv3sfcTl(nlon,nlat))
 
    allocate(lbcice(nlon,nlat))
    allocate(lbcsfcT(nlon,nlat))
@@ -147,10 +151,10 @@ if(mype==0) then
    fv3times=1  
    t1=1
 
-   call fcst%initial('FV3LAM')
+   call fcst%initial('FV3LAM',wcstart)
    call fcst%list_initial
-   call fcst%read_n(trim(fv3file),'FV3LAM',fv3lon,fv3lat,fv3times,t1,fv3mask,fv3sst,fv3ice,fv3sfcT,fv3iceT)
-   call fcst%finish
+   call fcst%read_n(trim(fv3file),'FV3LAM',wcstart,fv3lon,fv3lat,fv3times,t1,fv3mask,fv3sst,fv3ice,fv3sfcT,fv3iceT,fv3sfcTl)
+   call fcst%finish('FV3LAM',wcstart)
 
 
    write(*,*) 'fv3times: ', fv3times
@@ -160,13 +164,16 @@ if(mype==0) then
 
 !   fvcomfile='fvcom.nc'
 !   lbctimes: length of time dimension of FVCOM input data (command line input)
-!   t2: index of time dimension to pull from FVCOM (command line input) 
-   t2=inputFVCOMsel
 ! Space infront of ' FVCOM' below is important!!
-   call fcst%initial(' FVCOM')
+   call fcst%initial(' FVCOM',wcstart)
    call fcst%list_initial
-   call fcst%read_n(trim(fvcomfile),' FVCOM',lbclon,lbclat,lbctimes,t2,lbcmask,lbcsst,lbcice,lbcsfcT,lbciceT)
-   call fcst%finish
+   call fcst%get_time_ind(trim(fvcomfile),inputFVCOMselStr,indexFVCOMsel)
+!   t2: index of time dimension to pull from FVCOM
+   t2=indexFVCOMsel
+   write(*,*) 'time asked for =', trim(inputFVCOMselStr)
+   write(*,*) 'time index selected = ', t2
+   call fcst%read_n(trim(fvcomfile),' FVCOM',wcstart,lbclon,lbclat,lbctimes,t2,lbcmask,lbcsst,lbcice,lbcsfcT,lbciceT,fv3sfcTl)
+   call fcst%finish(' FVCOM',wcstart)
 
 !  Check that the dimensions match
 
@@ -193,33 +200,64 @@ if(mype==0) then
 !   ice cover data. Ice fraction is set
 !   to a minimum of 15% due to FV3-LAM
 !   raising any value below 15% to 15%.
-
-   do j=1,nlat
-      do i=1,nlon
-         if (lbcmask(i,j) > 0. .and. lbcsst(i,j) .ge. -90.0) then
-            !If ice fraction below 15%, set to 0
-            if (lbcice(i,j) < 0.15) then
-              lbcice(i,j) = 0.0
-            endif
-            fv3ice(i,j) = lbcice(i,j)
-            !If ice in FVCOM, but not in FV3-LAM, change to ice
-            if (lbcice(i,j) > 0. .and. fv3mask(i,j) == 0.) then
-              fv3mask(i,j) = 2.
-            endif
-            !If ice in FV3-LAM and not FVCOM, remove it from FV3-LAM
-            if (fv3mask(i,j) == 2. .and. lbcice(i,j) == 0.) then
-              fv3mask(i,j) = 0.
-            endif
-            fv3sst(i,j) = lbcsst(i,j) + 273.15
-            fv3sfcT(i,j) = lbcsst(i,j) + 273.15
-            fv3iceT(i,j) = lbcsst(i,j) + 273.15
-             !If ice exists in FVCOM, change ice surface temp
-            if (lbcice(i,j) > 0.) then
-              fv3iceT(i,j) = lbciceT(i,j) + 273.15
-            end if
-         endif
-      enddo
-   enddo
+   if (wcstart == 'warm') then
+     do j=1,nlat
+        do i=1,nlon
+           if (lbcmask(i,j) > 0. .and. lbcsst(i,j) .ge. -90.0) then
+              !If ice fraction below 15%, set to 0
+              if (lbcice(i,j) < 0.15) then
+                lbcice(i,j) = 0.0
+              endif
+              fv3ice(i,j) = lbcice(i,j)
+              !If ice in FVCOM, but not in FV3-LAM, change to ice
+              if (lbcice(i,j) > 0. .and. fv3mask(i,j) == 0.) then
+                fv3mask(i,j) = 2.
+              endif
+              !If ice in FV3-LAM and not FVCOM, remove it from FV3-LAM
+              if (fv3mask(i,j) == 2. .and. lbcice(i,j) == 0.) then
+                fv3mask(i,j) = 0.
+              endif
+              fv3sst(i,j) = lbcsst(i,j) + 273.15
+              fv3sfcT(i,j) = lbcsst(i,j) + 273.15
+              fv3iceT(i,j) = lbcsst(i,j) + 273.15
+              fv3sfcTl(i,j)= lbcsst(i,j) + 273.15
+               !If ice exists in FVCOM, change ice surface temp
+              if (lbcice(i,j) > 0.) then
+                fv3iceT(i,j) = lbciceT(i,j) + 273.15
+              end if
+           end if
+        enddo
+     enddo
+   else if (wcstart == 'cold') then
+     do j=1,nlat
+        do i=1,nlon
+           if (lbcmask(i,j) > 0. .and. lbcsst(i,j) .ge. -90.0) then
+              !If ice fraction below 15%, set to 0
+              if (lbcice(i,j) < 0.15) then
+                lbcice(i,j) = 0.0
+              endif
+              fv3ice(i,j) = lbcice(i,j)
+              !If ice in FVCOM, but not in FV3-LAM, change to ice
+              if (lbcice(i,j) > 0. .and. fv3mask(i,j) == 0.) then
+                fv3mask(i,j) = 2.
+              endif
+              !If ice in FV3-LAM and not FVCOM, remove it from FV3-LAM
+              if (fv3mask(i,j) == 2. .and. lbcice(i,j) == 0.) then
+                fv3mask(i,j) = 0.
+              endif
+              fv3sst(i,j) = lbcsst(i,j) + 273.15
+              fv3sfcT(i,j) = lbcsst(i,j) + 273.15
+              fv3iceT(i,j) = lbcsst(i,j) + 273.15
+               !If ice exists in FVCOM, change ice surface temp
+              if (lbcice(i,j) > 0.) then
+                fv3iceT(i,j) = lbciceT(i,j) + 273.15
+              end if
+           end if
+        enddo
+     enddo
+   else
+     write(*,*) 'Variable wcstart is not set to either warm or cold'
+   end if
 
 ! Write out sfc file again
 
@@ -228,9 +266,18 @@ if(mype==0) then
    call geo%replace_var("fice",NLON,NLAT,fv3ice)
    call geo%replace_var("slmsk",NLON,NLAT,fv3mask)
    call geo%replace_var("tisfc",NLON,NLAT,fv3iceT)
+
+   if (wcstart == 'cold') then
 ! Add_New_Var takes names of (Variable,Dim1,Dim2,Dim3,Long_Name,Units)
-   call geo%add_new_var('glmsk','xaxis_1','yaxis_1','Time','glmsk','none')
-   call geo%replace_var('glmsk',NLON,NLAT,lbcmask)
+      call geo%add_new_var('glmsk','xaxis_1','yaxis_1','Time','glmsk','none')
+      call geo%replace_var('glmsk',NLON,NLAT,lbcmask)
+   end if
+   if (wcstart == 'warm') then
+      call geo%replace_var("tsfc",NLON,NLAT,fv3sfcT)
+      call geo%replace_var("tsfcl",NLON,NLAT,fv3sfcTl)
+      call geo%add_new_var('glmsk','xaxis_1','yaxis_1','glmsk','none')
+      call geo%replace_var('glmsk',NLON,NLAT,lbcmask)
+   end if
    call geo%close
 
    write(6,*) "=== LOWBC UPDATE SUCCESS ==="
