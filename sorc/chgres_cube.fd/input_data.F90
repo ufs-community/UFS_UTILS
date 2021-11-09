8037cf0ed4b86f45293bdd1f5989a7a10142a11e
George Gayno <George.Gayno@noaa.gov>
2021-11-09 14:46:16 +0000

    Replace wgrib2 read of ustar with g2 read.
    Fixes #591.

--- sorc/chgres_cube.fd/input_data.F90
+++ sorc/chgres_cube.fd/input_data.F90
@@ -5683,6 +5683,7 @@ if (localpet == 0) then
     endif
     print*,'ffmm ',maxval(dummy2d_8),minval(dummy2d_8)
 
+
  endif
 
  print*,"- CALL FieldScatter FOR INPUT GRID FFMM"
@@ -5691,24 +5692,68 @@ if (localpet == 0) then
     call error_handler("IN FieldScatter", rc)
     
  if (localpet == 0) then
+!  print*,"- READ USTAR."
+!  vname="fricv"
+!  slev=":surface:" 
+!  call get_var_cond(vname,this_miss_var_method=method,this_miss_var_value=value, &
+!                        loc=varnum)  
+!   vname=":FRICV:"              
+!   rc= grb2_inq(the_file, inv_file, vname,slev, data2=dummy2d)
+!   if (rc <= 0) then
+!     call handle_grib_error(vname, slev ,method,value,varnum,rc, var= dummy2d)
+!     if (rc==1) then ! missing_var_method == skip or no entry in varmap table
+!       print*, "WARNING: "//trim(vname)//" NOT AVAILABLE IN FILE. THIS FIELD WILL "//&
+!                  "REPLACED WITH CLIMO. SET A FILL "// &
+!                     "VALUE IN THE VARMAP TABLE IF THIS IS NOT DESIRABLE."
+!       dummy2d(:,:) = 0.0_esmf_kind_r4
+!     endif
+!   endif
+!  dummy2d_8= real(dummy2d,esmf_kind_r8)
+!  print*,'wgrib2 fricv ',maxval(dummy2d),minval(dummy2d)
+
    print*,"- READ USTAR."
    vname="fricv"
    slev=":surface:" 
    call get_var_cond(vname,this_miss_var_method=method,this_miss_var_value=value, &
                          loc=varnum)  
-    vname=":FRICV:"              
-    rc= grb2_inq(the_file, inv_file, vname,slev, data2=dummy2d)
-    if (rc <= 0) then
-      call handle_grib_error(vname, slev ,method,value,varnum,rc, var= dummy2d)
-      if (rc==1) then ! missing_var_method == skip or no entry in varmap table
-        print*, "WARNING: "//trim(vname)//" NOT AVAILABLE IN FILE. THIS FIELD WILL "//&
+
+     jdisc   = 0     ! search for discipline - meteo products
+     j = 0
+     jpdt    = -9999  ! array of values in product definition template 4.n
+     jpdtn   = 0  ! search for product def template number 0 - anl or fcst.
+     jpdt(1) = 2  ! oct 10 - param cat - momentum
+     jpdt(2) = 30  ! oct 11 - param number - friction vel.
+     jpdt(10) = 1 ! oct 23 - type of level - ground surface
+     unpack=.true.
+     call getgb2(lugb, lugi, j, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt, &
+             unpack, k, gfld, rc)
+     if (rc /= 0) then
+       jpdt(2) = 197  ! oct 11 - param number - friction vel.
+       call getgb2(lugb, lugi, j, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt, &
+             unpack, k, gfld, rc)
+     endif
+
+     if (rc == 0) then
+       print*,'getgb2 fricv ', maxval(gfld%fld),minval(gfld%fld)
+       dummy2d_8 = reshape(gfld%fld , (/i_input,j_input/))
+     else
+       call handle_grib_error(vname, slev ,method,value,varnum,rc, var8= dummy2d_8)
+       if (rc==1) then ! missing_var_method == skip or no entry in varmap table
+         print*, "WARNING: "//trim(vname)//" NOT AVAILABLE IN FILE. THIS FIELD WILL "//&
                    "REPLACED WITH CLIMO. SET A FILL "// &
                       "VALUE IN THE VARMAP TABLE IF THIS IS NOT DESIRABLE."
-        dummy2d(:,:) = 0.0_esmf_kind_r4
-      endif
-    endif
-   dummy2d_8= real(dummy2d,esmf_kind_r8)
-   print*,'fricv ',maxval(dummy2d),minval(dummy2d)
+         dummy2d_8(:,:) = 0.0
+       endif
+     endif
+
+! temporary code. wgrib flips the pole of gfs data.
+    if (trim(external_model) == "GFS") then
+      dummy2d_82 = dummy2d_8
+      do j = 1, j_input
+        dummy2d_8(:,j) = dummy2d_82(:,j_input-j+1)
+      enddo
+    endif 
+
  endif
 
  print*,"- CALL FieldScatter FOR INPUT GRID USTAR"
