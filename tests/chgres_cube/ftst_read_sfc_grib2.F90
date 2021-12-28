f1956de8500681e4ea9b1ca7140e927d29a008e8
George Gayno <George.Gayno@noaa.gov>
2021-12-28 21:56:37 +0000

    Add new unit test for grib2 surface read.
    Fixes #591.

---
+++ tests/chgres_cube/ftst_read_sfc_grib2.F90
@@ -0,0 +1,96 @@
+ program read_sfc_grib2
+
+ use esmf
+
+ use input_data, only      : read_input_sfc_data
+
+ use program_setup, only   : external_model, data_dir_input_grid, &
+                             grib2_file_input_grid, varmap_file, &
+                             read_varmap, input_type
+
+ use model_grid, only      : input_grid, &
+                             i_input, j_input, &
+                             latitude_input_grid, &
+                             longitude_input_grid
+
+ implicit none
+
+ integer                      :: localpet, npets, rc
+
+ real(esmf_kind_r8), allocatable  :: latitude(:,:)
+ real(esmf_kind_r8), allocatable  :: longitude(:,:)
+
+ type(esmf_vm)                :: vm
+
+ type(esmf_polekind_flag)     :: polekindflag(2)
+
+ print*,"Starting test of read_atm_grib2_file."
+
+ call mpi_init(rc)
+
+ call ESMF_Initialize(rc=rc)
+
+ call ESMF_VMGetGlobal(vm, rc=rc)
+
+ call ESMF_VMGet(vm, localPet=localpet, petCount=npets, rc=rc)
+
+ external_model="GFS"
+ input_type="grib2"
+ data_dir_input_grid = "/scratch1/NCEPDEV/da/George.Gayno/noscrub/reg_tests/chgres_cube/input_data/gfs.grib2"
+!data_dir_input_grid = "/data"
+ grib2_file_input_grid = "gfs.t00z.pgrb2.0p50.f000"
+
+ i_input = 720
+ j_input = 361
+
+ polekindflag(1:2) = ESMF_POLEKIND_MONOPOLE
+ input_grid = ESMF_GridCreate1PeriDim(minIndex=(/1,1/), &
+                                   maxIndex=(/i_input,j_input/), &
+                                   polekindflag=polekindflag, &
+                                   periodicDim=1, &
+                                   poleDim=2,  &
+                                   coordSys=ESMF_COORDSYS_SPH_DEG, &
+                                   regDecomp=(/1,npets/),  &
+                                   indexflag=ESMF_INDEX_GLOBAL, rc=rc)
+
+
+ varmap_file ="./data/GFSphys_varmap.txt"
+ call read_varmap
+
+ latitude_input_grid = ESMF_FieldCreate(input_grid, &
+                                   typekind=ESMF_TYPEKIND_R8, &
+                                   staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                   name="input_grid_latitude", &
+                                   rc=rc)
+
+ longitude_input_grid = ESMF_FieldCreate(input_grid, &
+                                   typekind=ESMF_TYPEKIND_R8, &
+                                   staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                   name="input_grid_longitude", &
+                                   rc=rc)
+
+! Lat/lon used in surface read. Use flag values.
+
+ allocate(latitude(i_input,j_input))
+ allocate(longitude(i_input,j_input))
+ latitude=90.0
+ longitude=0.0
+
+ call ESMF_FieldScatter(longitude_input_grid, longitude, rootpet=0, rc=rc)
+ call ESMF_FieldScatter(latitude_input_grid, latitude, rootpet=0, rc=rc)
+
+! Call the chgres_cube read routine.
+
+ call read_input_sfc_data(localpet)
+
+ call ESMF_finalize(endflag=ESMF_END_KEEPMPI)
+
+ call mpi_finalize(rc)
+
+ print*,"SUCCESS!"
+
+
+
+
+
+ end program read_sfc_grib2
