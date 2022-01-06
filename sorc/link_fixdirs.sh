876b8a136854c5868ff48b8ccc360fb765816595
George.Gayno <george.gayno@noaa.gov>
2022-01-06 13:55:29 +0000

    Update "link_fixdirs.sh" to point to latest location of the fixed directories on WCOSS2.
    Fixes #580.

--- sorc/link_fixdirs.sh
+++ sorc/link_fixdirs.sh
@@ -44,7 +44,7 @@ elif [ $machine = "hera" ]; then
 elif [ $machine = "jet" ]; then
     FIX_DIR="/lfs4/HFIP/hfv3gfs/glopara/git/fv3gfs/fix_nco_gfsv16"
 elif [ $machine = "wcoss2" ]; then
-    FIX_DIR="/lfs/h2/emc/global/noscrub/Kate.Friedman/glopara/FIX/fix"
+    FIX_DIR="/lfs/h2/emc/global/save/emc.global/FIX/fix_nco_gfsv16"
 elif [ $machine = "orion" ]; then
     FIX_DIR="/work/noaa/global/glopara/fix_nco_gfsv16"
 fi
