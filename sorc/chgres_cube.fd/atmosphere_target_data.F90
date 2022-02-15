c9af07ffdc1080e5c9a584076678b34c3c3c07d4
George Gayno <George.Gayno@noaa.gov>
2022-02-15 15:31:14 +0000

    Add Doxygen to atmosphere_target_data.F90.
    Fixes #459.

--- sorc/chgres_cube.fd/atmosphere_target_data.F90
+++ sorc/chgres_cube.fd/atmosphere_target_data.F90
@@ -1,3 +1,11 @@
+!> @file
+!! @brief Define atmospheric target data variables.
+!! @author George Gayno NCEP/EMC
+
+!> Module to hold variables and ESMF fields associated 
+!! with the target grid atmospheric data.
+!!
+!! @author George Gayno NCEP/EMC
  module atmosphere_target_data
 
  use esmf
@@ -6,33 +14,34 @@
 
  private
 
- integer, public                    :: lev_target       !< num vertical levels
- integer, public                    :: levp1_target     !< num levels plus 1
- integer, public                    :: nvcoord_target   !< num vertical coordinate variables
+ integer, public                    :: lev_target       !< Number of vertical levels.
+ integer, public                    :: levp1_target     !< Number of vertical levels plus 1.
+ integer, public                    :: nvcoord_target   !< Number of vertical coordinate variables.
 
- real(esmf_kind_r8), allocatable, public :: vcoord_target(:,:)  !< vertical coordinate
+ real(esmf_kind_r8), allocatable, public :: vcoord_target(:,:)  !< Vertical coordinate.
 
- type(esmf_field), public               :: delp_target_grid !< pressure thickness
- type(esmf_field), public               :: dzdt_target_grid !< vertical velocity
- type(esmf_field), public               :: ps_target_grid !< surface pressure
- type(esmf_field), public               :: temp_target_grid !< temperautre
- type(esmf_field), allocatable, public  :: tracers_target_grid(:) !< tracers
- type(esmf_field), public               :: u_s_target_grid !< u-wind, 'south' edge
- type(esmf_field), public               :: v_s_target_grid !< v-wind, 'south' edge
- type(esmf_field), public               :: u_w_target_grid !< u-wind, 'west' edge
- type(esmf_field), public               :: v_w_target_grid !< v-wind, 'west' edge
- type(esmf_field), public               :: zh_target_grid !< 3-d height
- type(esmf_field), public               :: qnifa_climo_target_grid !< number concentration of ice
-                                           !! friendly aerosols on target
-                                           !! horiz/vert grid.
- type(esmf_field), public               :: qnwfa_climo_target_grid !< number concentration of water
-                                           !! friendly aerosols on target
-                                           !! horiz/vert grid.
+ type(esmf_field), public               :: delp_target_grid !< Pressure thickness.
+ type(esmf_field), public               :: dzdt_target_grid !< Vertical velocity.
+ type(esmf_field), public               :: ps_target_grid !< Surface pressure.
+ type(esmf_field), public               :: temp_target_grid !< Temperautre.
+ type(esmf_field), allocatable, public  :: tracers_target_grid(:) !< Tracers.
+ type(esmf_field), public               :: u_s_target_grid !< U-wind, 'south' edge of grid cell.
+ type(esmf_field), public               :: v_s_target_grid !< V-wind, 'south' edge of grid cell.
+ type(esmf_field), public               :: u_w_target_grid !< U-wind, 'west' edge of grid cell.
+ type(esmf_field), public               :: v_w_target_grid !< V-wind, 'west' edge of grid cell.
+ type(esmf_field), public               :: zh_target_grid !< 3-d height.
+ type(esmf_field), public               :: qnifa_climo_target_grid !< Number concentration of ice
+                                           !! friendly aerosols.
+ type(esmf_field), public               :: qnwfa_climo_target_grid !< Number concentration of water
+                                           !! friendly aerosols.
 
  public :: cleanup_atmosphere_target_data
 
  contains
 
+!> Free up memory for fields and variables in this module.
+!!
+!! @author George.Gayno NOAA/EMC
  subroutine cleanup_atmosphere_target_data
 
  use program_setup, only      : num_tracers
