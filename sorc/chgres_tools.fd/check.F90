31bceb53643ce71b6c583454104f46ace3fd2222
George Gayno <George.Gayno@noaa.gov>
2022-04-26 14:14:13 +0000

    Update ./chgres_tools.fd/check.F90 to loop over multiple tiles.
    Fixes #123.

--- sorc/chgres_tools.fd/check.F90
+++ sorc/chgres_tools.fd/check.F90
@@ -20,9 +20,12 @@
 
  character(len=150) :: file_ceiling, file_frac, file_orog_frac
 
- character(len=15) :: varname(num_var), varname3d(num_var3d)
+ character(len=15) :: varname_frac(num_var), varname3d(num_var3d)
+ character(len=15) :: varname_ceil(num_var)
+ character(len=21) :: oro_files(6)
+ character(len=16) :: the_files(6)
 
- integer :: error, id_dim, idim, jdim
+ integer :: error, id_dim, idim, jdim, tiles
  integer :: varid, varid_ceil, varid_frac, var
  integer :: i, j, n, ncid_ceil, ncid_orog_frac, ncid_frac
 
@@ -30,22 +33,38 @@
  real*8, allocatable :: dummy_ceil(:,:)
  real*8, allocatable :: dummy_frac3d(:,:,:), dummy_ceil3d(:,:,:)
 
- data varname /'alvsf', 'alvwf', 'alnsf', 'alnwf', 'canopy', &
+ data varname_frac /'alvsf', 'alvwf', 'alnsf', 'alnwf', 'canopy', &
                'f10m' , 'facsf', 'facwf', 'ffhh' , 'ffmm',   &
                'q2m'  , 'shdmax','shdmin', 'sheleg', 'slope', &
                'snoalb', 'snwdph', 'srflag','stype', 't2m', &
                'tg3',  'tprcp', 'tsfcl',   'uustar', 'vfrac', &
                'vtype', 'zorll'  /
 
+ data varname_ceil /'alvsf', 'alvwf', 'alnsf', 'alnwf', 'canopy', &
+               'f10m' , 'facsf', 'facwf', 'ffhh' , 'ffmm',   &
+               'q2m'  , 'shdmax','shdmin', 'sheleg', 'slope', &
+               'snoalb', 'snwdph', 'srflag','stype', 't2m', &
+               'tg3',  'tprcp', 'tsea',   'uustar', 'vfrac', &
+               'vtype', 'zorl'  /
+
+ data the_files /'out.sfc.tile1.nc', 'out.sfc.tile2.nc', 'out.sfc.tile3.nc', &
+                 'out.sfc.tile4.nc', 'out.sfc.tile5.nc', 'out.sfc.tile6.nc'/
+
+ data oro_files /'C96_oro_data.tile1.nc', 'C96_oro_data.tile2.nc', 'C96_oro_data.tile3.nc', &
+                 'C96_oro_data.tile4.nc', 'C96_oro_data.tile5.nc', 'C96_oro_data.tile6.nc' /
+
  data varname3d /'stc', 'slc', 'smc'/
 
- file_frac="/gpfs/dell1/stmp/George.Gayno/chgres_fractional/out.sfc.tile1.nc"
- file_orog_frac="/gpfs/dell2/emc/modeling/noscrub/George.Gayno/ufs_utils.git/chgres_cube.fractional/my_grids_fract/C96/C96_oro_data.tile1.nc"
- file_ceiling="/gpfs/dell1/stmp/George.Gayno/chgres_ceiling/out.sfc.tile1.nc"
+ TILE : do tiles = 1, 6
+
+ file_frac="/gpfs/dell1/stmp/George.Gayno/chgres_fractional/" // the_files(tiles)
+ file_orog_frac="/gpfs/dell2/emc/modeling/noscrub/George.Gayno/ufs_utils.git/chgres_cube.fractional/my_grids_fract/C96/" // oro_files(tiles)
+ file_ceiling="/gpfs/dell1/stmp/George.Gayno/chgres_ceiling/" // the_files(tiles)
 
 ! Open the file created using the non-fractional logic, but with
 ! slmsk modified to be ceiling of the land fraction.
 
+ print*,"- OPEN FILE ", trim(file_ceiling)
  error=nf90_open(trim(file_ceiling),nf90_nowrite,ncid_ceil)
  call netcdf_err(error, 'opening file_ceiling' )
 
@@ -59,7 +78,7 @@
  error=nf90_inquire_dimension(ncid_ceil,id_dim,len=jdim)
  call netcdf_err(error, 'reading yaxis' )
 
- allocate(slmsk(idim,jdim))
+ if(.not.allocated(slmsk)) allocate(slmsk(idim,jdim))
 
  error=nf90_inq_varid(ncid_ceil,"slmsk",varid)
  call netcdf_err(error, 'reading slmsk id' )
@@ -71,10 +90,11 @@
 ! Open the fractional grid orography file. Read the
 ! land fraction.
 
+ print*,"- OPEN FILE ", trim(file_orog_frac)
  error=nf90_open(trim(file_orog_frac),nf90_nowrite,ncid_orog_frac)
  call netcdf_err(error, 'opening file_orog_frac' )
 
- allocate(land_frac(idim,jdim))
+ if(.not.allocated(land_frac)) allocate(land_frac(idim,jdim))
 
  error=nf90_inq_varid(ncid_orog_frac,"land_frac",varid_frac)
  call netcdf_err(error, 'reading land_frac id' )
@@ -91,13 +111,13 @@
  do i = 1, idim
    if (nint(slmsk(i,j)) == 1) then
      if(ceiling(land_frac(i,j)) /= 1.0_8) then
-       print*,'bad point 1', i,j,slmsk(i,j),ceiling(land_frac(i,j))
+       print*,'bad mask point 1', i,j,slmsk(i,j),ceiling(land_frac(i,j))
        stop
      endif
    endif
    if (ceiling(land_frac(i,j)) == 1.0_8) then
      if (nint(slmsk(i,j)) /= 1) then
-       print*,'bad point 2', i,j,slmsk(i,j),ceiling(land_frac(i,j))
+       print*,'bad mask point 2', i,j,slmsk(i,j),ceiling(land_frac(i,j))
        stop
      endif
    endif
@@ -106,26 +126,26 @@
 
 ! Check data.
 
- allocate(dummy_frac(idim,jdim))
- allocate(dummy_ceil(idim,jdim))
+ if(.not.allocated(dummy_frac)) allocate(dummy_frac(idim,jdim))
+ if(.not.allocated(dummy_ceil)) allocate(dummy_ceil(idim,jdim))
 
+ print*,"- OPEN FILE ", trim(file_frac)
  error=nf90_open(trim(file_frac),nf90_nowrite,ncid_frac)
  call netcdf_err(error, 'opening file_frac' )
 
  do var = 1, num_var
    
-   print*,'CHECK FIELD ', trim(varname(var))
+   print*,'CHECK FIELD ', trim(varname_frac(var))
 
-   error=nf90_inq_varid(ncid_frac, varname(var), varid_frac)
+   error=nf90_inq_varid(ncid_frac, varname_frac(var), varid_frac)
    call netcdf_err(error, 'reading frac id' )
    error=nf90_get_var(ncid_frac,varid_frac,dummy_frac)
    call netcdf_err(error, 'reading frac field' )
 
    print*,'frac field ',maxval(dummy_frac),minval(dummy_frac)
 
-   if (trim(varname(var)) == 'tsfcl') varname(var) = 'tsea'
-   if (trim(varname(var)) == 'zorll') varname(var) = 'zorl'
-   error=nf90_inq_varid(ncid_ceil, varname(var), varid_ceil)
+   print*,'read variable ',varname_ceil(var)
+   error=nf90_inq_varid(ncid_ceil, varname_ceil(var), varid_ceil)
    call netcdf_err(error, 'reading ceil id' )
    error=nf90_get_var(ncid_ceil,varid_ceil,dummy_ceil)
    call netcdf_err(error, 'reading ceil field' )
@@ -147,8 +167,8 @@
 
  enddo
 
- allocate(dummy_frac3d(idim,jdim,4))
- allocate(dummy_ceil3d(idim,jdim,4))
+ if(.not.allocated(dummy_frac3d)) allocate(dummy_frac3d(idim,jdim,4))
+ if(.not.allocated(dummy_ceil3d)) allocate(dummy_ceil3d(idim,jdim,4))
 
  do var = 1, num_var3d
    
@@ -174,7 +194,8 @@
 
        if (nint(slmsk(i,j)) == 1) then
          if (dummy_ceil3d(i,j,n) /= dummy_frac3d(i,j,n)) then
-           print*,'bad pt ',i,j,n,dummy_ceil3d(i,j,n),dummy_frac3d(i,j,n)
+           print*,'bad 3d pt ',i,j,n,dummy_ceil3d(i,j,n),dummy_frac3d(i,j,n)
+           stop
          endif
        endif
 
@@ -184,6 +205,8 @@
    enddo
 
  enddo
+
+ enddo TILE
 
  print*,'DONE'
 
