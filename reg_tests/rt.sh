9a13a6b2a03ae1766d35616fad5c6d08d60af18c
George.Gayno <george.gayno@noaa.gov>
2022-06-07 15:37:49 +0000

    Update ./reg_tests/rt.sh to run cpld_gridgen on wcoss2.
    Fixes #559.

--- reg_tests/rt.sh
+++ reg_tests/rt.sh
@@ -74,7 +74,7 @@ cd fix
 
 cd ../reg_tests
 
-if [[ $target == "orion" ]] || [[ $target == "jet" ]] || [[ $target == "hera" ]] ; then
+if [[ $target == "orion" ]] || [[ $target == "jet" ]] || [[ $target == "hera" ]] || [[ $target == "wcoss2" ]] ; then
 
   cd cpld_gridgen
   export ACCOUNT=$PROJECT_CODE
