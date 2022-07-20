365c1b92caaa3a19fb5a49ce91ddaf8a08da6e08
George Gayno <George.Gayno@noaa.gov>
2022-07-20 19:27:52 +0000

    Update to use latest version of sfcsub.F
    Fixes #670.

--- sorc/global_cycle.fd/machine.f90
+++ sorc/global_cycle.fd/machine.f90
@@ -8,8 +8,12 @@ MODULE MACHINE
 
  IMPLICIT NONE
  SAVE
- integer, parameter :: kind_io4  = 4 !< Kind type for 4-byte float point
-                                     !! variables.
- integer, parameter :: kind_io8  = 8 !< Kind type for 8-byte float point
-                                     !! variables.
+ integer, parameter :: kind_sngl_prec  = 4 !< Kind type for 4-byte float point
+                                           !! variables.
+ integer, parameter :: kind_io4  = kind_sngl_prec !< Kind type for 4-byte float point
+                                                  !! variables.
+ integer, parameter :: kind_dbl_prec  = 8 !< Kind type for 8-byte float point
+                                          !! variables.
+ integer, parameter :: kind_io8  = kind_dbl_prec !< Kind type for 8-byte float point
+                                          !! variables.
  END MODULE MACHINE
