9303cf9e416ac9622b64d7de4838138d8ec39971
George Gayno <George.Gayno@noaa.gov>
2022-08-09 08:53:26 -0500

    Add check of successful clone to rt.sh
    Fixes #680.

--- reg_tests/rt.sh
+++ reg_tests/rt.sh
@@ -17,6 +17,18 @@ rm -f reg_test_results.txt
 rm -rf UFS_UTILS
 
 git clone --recursive https://github.com/ufs-community/UFS_UTILS.git
+rc=$?
+
+# Check to see if the clone was successful. Previously, it has
+# failed due to lack of disk space.
+
+if [[ $rc == 0 ]] && [[ -d UFS_UTILS ]];then
+  echo "Clone Successful"
+else
+  target=`hostname -s`
+  echo "Clone Failed" | mail -s "UFS_UTILS Consistency Tests failed on ${target}" ${MAILTO}
+fi
+
 cd UFS_UTILS
 
 source sorc/machine-setup.sh
