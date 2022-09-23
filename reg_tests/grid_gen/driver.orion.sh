0c039fc32595181e2263aad0f0e106307d39fa28
George Gayno <George.Gayno@noaa.gov>
2022-09-23 09:37:01 -0500

    Update orion script to run grid tests in parallel.
    Fixes #696.

--- reg_tests/grid_gen/driver.orion.sh
+++ reg_tests/grid_gen/driver.orion.sh
@@ -6,14 +6,14 @@
 #
 # Set WORK_DIR to your working directory. Set the PROJECT_CODE and QUEUE
 # as appropriate.  To see which projects you are authorized to use,
-# type "saccount_params".
+# type "saccount_params" (after a 'module load contrib noaatools').
 #
-# Invoke the script with no arguments.  A series of daily-
-# chained jobs will be submitted.  To check the queue, type:
+# Invoke the script with no arguments.  A set of tests will
+# be submitted to run in parallel. To check the queue, type:
 # "squeue -u $LOGNAME".
 #
-# Log output from the suite will be in LOG_FILE.  Once the suite
-# has completed, a summary is placed in SUM_FILE.
+# Log output from each test will be placed in its own LOG_FILE.  
+# Once the suite has completed, a summary is placed in SUM_FILE.
 #
 # A test fails when its output does not match the baseline files as
 # determined by the "nccmp" utility.  The baseline files are stored in
@@ -61,43 +61,48 @@ rm -fr $WORK_DIR
 # C96 uniform grid
 #-----------------------------------------------------------------------------
 
+LOG_FILE1=${LOG_FILE}01
 TEST1=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.uniform \
-      -o $LOG_FILE -e $LOG_FILE ./c96.uniform.sh)
+      -o $LOG_FILE1 -e $LOG_FILE1 ./c96.uniform.sh)
 
 #-----------------------------------------------------------------------------
 # C96 uniform grid using viirs vegetation type data.
 #-----------------------------------------------------------------------------
 
+LOG_FILE2=${LOG_FILE}02
 TEST2=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.viirs.vegt \
-      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST1 ./c96.viirs.vegt.sh)
+      -o $LOG_FILE2 -e $LOG_FILE2 ./c96.viirs.vegt.sh)
 
 #-----------------------------------------------------------------------------
 # GFDL regional grid
 #-----------------------------------------------------------------------------
 
+LOG_FILE3=${LOG_FILE}03
 TEST3=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J gfdl.regional \
-      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST2 ./gfdl.regional.sh)
+      -o $LOG_FILE3 -e $LOG_FILE3 ./gfdl.regional.sh)
 
 #-----------------------------------------------------------------------------
 # ESG regional grid
 #-----------------------------------------------------------------------------
 
+LOG_FILE4=${LOG_FILE}04
 TEST4=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J esg.regional \
-      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST3 ./esg.regional.sh)
+      -o $LOG_FILE4 -e $LOG_FILE4 ./esg.regional.sh)
 
 #-----------------------------------------------------------------------------
 # Regional grid with GSL gravity wave drag fields.
 #-----------------------------------------------------------------------------
 
+LOG_FILE5=${LOG_FILE}05
 TEST5=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:10:00 -A $PROJECT_CODE -q $QUEUE -J reg.gsl.gwd \
-      --open-mode=append -o $LOG_FILE -e $LOG_FILE -d afterok:$TEST4 ./regional.gsl.gwd.sh)
+      -o $LOG_FILE5 -e $LOG_FILE5 ./regional.gsl.gwd.sh)
 
 #-----------------------------------------------------------------------------
 # Create summary log.
 #-----------------------------------------------------------------------------
 
 sbatch --nodes=1 -t 0:01:00 -A $PROJECT_CODE -J grid_summary -o $LOG_FILE -e $LOG_FILE \
-       --open-mode=append -q $QUEUE -d afterok:$TEST5 << EOF
+       -q $QUEUE -d afterok:$TEST1:$TEST2:$TEST3:$TEST4:$TEST5 << EOF
 #!/bin/bash
-grep -a '<<<' $LOG_FILE  > $SUM_FILE
+grep -a '<<<' ${LOG_FILE}*  > $SUM_FILE
 EOF
