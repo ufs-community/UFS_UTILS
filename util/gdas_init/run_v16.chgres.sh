4960ba597b8b97fe2b6a4547115c68c226f3bdb6
Chunxi.Zhang-NOAA <Chunxi.Zhang@noaa.gov>
2023-01-31 13:52:25 -0600

    Revert adding util/gdas_init/run_v17.chgres.sh and modify util/gdas_init/run_v16.chgres.sh to make all new UFS applications use fracoro data

--- util/gdas_init/run_v16.chgres.sh
+++ util/gdas_init/run_v16.chgres.sh
@@ -59,13 +59,30 @@ rm -fr $WORKDIR
 mkdir -p $WORKDIR
 cd $WORKDIR
 
+if [ "${FRAC_ORO:-"NO"}" = "YES" ]; then
+  if  [ ${CTAR} == 'C48' ] ; then
+    OCNRES='400'
+  elif [ ${CTAR} == 'C96' ] ; then
+    OCNRES='100'
+  elif [ ${CTAR} == 'C192' ] ; then
+    OCNRES='050'
+  elif [ ${CTAR} == 'C384' ] || [ ${CTAR} == 'C768' ] || [ ${CTAR} == 'C1152' ]; then
+    OCNRES='025'
+  fi
+  ORO_DIR="${CTAR}.mx${OCNRES}_frac"
+  ORO_NAME="oro_${CTAR}.mx${OCNRES}"
+else
+  ORO_DIR="${CTAR}"
+  ORO_NAME="${CTAR}_oro_data"
+fi
+
 cat << EOF > fort.41
 
 &config
- fix_dir_target_grid="${FIX_ORO}/${CTAR}/fix_sfc"
- mosaic_file_target_grid="${FIX_ORO}/${CTAR}/${CTAR}_mosaic.nc"
- orog_dir_target_grid="${FIX_ORO}/${CTAR}"
- orog_files_target_grid="${CTAR}_oro_data.tile1.nc","${CTAR}_oro_data.tile2.nc","${CTAR}_oro_data.tile3.nc","${CTAR}_oro_data.tile4.nc","${CTAR}_oro_data.tile5.nc","${CTAR}_oro_data.tile6.nc"
+ fix_dir_target_grid="${FIX_ORO}/${ORO_DIR}/fix_sfc"
+ mosaic_file_target_grid="${FIX_ORO}/${ORO_DIR}/${CTAR}_mosaic.nc"
+ orog_dir_target_grid="${FIX_ORO}/${ORO_DIR}"
+ orog_files_target_grid="${ORO_NAME}.tile1.nc","${ORO_NAME}.tile2.nc","${ORO_NAME}.tile3.nc","${ORO_NAME}.tile4.nc","${ORO_NAME}.tile5.nc","${ORO_NAME}.tile6.nc"
  data_dir_input_grid="${INPUT_DATA_DIR}"
  atm_files_input_grid="${ATMFILE}"
  sfc_files_input_grid="${SFCFILE}"
