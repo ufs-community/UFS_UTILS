8b852e4b16011703721ad5091e7667571c534a45
akubaryk <Adam.Kubaryk@noaa.gov>
2023-02-02 11:22:23 -0700

    fixing typo

--- sorc/chgres_cube.fd/wam_climo_data.f90
+++ sorc/chgres_cube.fd/wam_climo_data.f90
@@ -1,5 +1,5 @@
 ! This software incorporates the MSIS empirical atmospheric model software
-! designed and provided by NRL. Use is governbed by the Open Source Academic
+! designed and provided by NRL. Use is governed by the Open Source Academic
 ! research License Agreement contained in the file msis2.1/nrlmsis2.1_license..txt
   subroutine gettemp(iday,nday,xlat,nlat,pr,np,pf,temp,n_o,n_o2,n_n2)
     use msis_init,      only: msisinit
