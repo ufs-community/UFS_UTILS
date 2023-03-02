ff87f3cb06d6bbcfa7624412b54764a748969e0d
George Gayno <George.Gayno@noaa.gov>
2023-03-02 13:19:21 +0000

    Some fixes from Adam.
    Fixes #759

--- sorc/chgres_cube.fd/wam_climo_data.f90
+++ sorc/chgres_cube.fd/wam_climo_data.f90
@@ -1,27 +1,27 @@
 ! This software incorporates the MSIS empirical atmospheric model software
 ! designed and provided by NRL. Use is governed by the Open Source Academic
 ! research License Agreement contained in the file msis2.1/nrlmsis2.1_license..txt
-  subroutine gettemp(iday,nday,xlat,nlat,pr,np,pf,temp,n_o,n_o2,n_n2)
+  subroutine gettemp(iday,xlat,pr,np,pf,temp,n_o,n_o2,n_n2)
     use msis_init,      only: msisinit
     use msis_constants, only: rp
+    use esmf,           only: esmf_kind_r8
 
     implicit none
 
-    integer(kind=rp), intent(in) :: iday(nday)  ! calender day
-    integer, intent(in) :: nday                 ! number of days
-    real(kind=rp),    intent(in) :: xlat(nlat)  ! latitude in degree
-    integer, intent(in) :: nlat                 ! number of latitudes
-    real(kind=rp),    intent(in) :: pr(np)      ! pressure in mb
-    integer, intent(in) :: np                   ! number of pressure layer
-    character(*), intent(in) :: pf              ! path to parmfile for msisinit
-    real,   intent(out) :: temp(np,nlat,nday)   ! temperature
-    real,   intent(out) :: n_o(np,nlat,nday)    ! number density of o
-    real,   intent(out) :: n_o2(np,nlat,nday)   ! number density of o2
-    real,   intent(out) :: n_n2(np,nlat,nday)   ! number density of n2
-    real                :: alt(np,nlat,nday)    ! altitude in km
-    real(kind=rp)       :: d(9),t,ap(7),ut,xlong,f107,f107a,zkm
-    integer             :: k,il,ip
-    real(4)             :: switch_legacy(1:25)
+    integer, intent(in) :: iday                   ! calender day
+    real(kind=esmf_kind_r8), intent(in) :: xlat   ! latitude (degrees)
+    real(kind=esmf_kind_r8), intent(in) :: pr(np) ! pressure (hPa)
+    integer, intent(in) :: np                     ! number of pressure layers
+    character(*), intent(in) :: pf                ! path to parmfile for msisinit
+    real(kind=esmf_kind_r8), intent(out) :: temp(np) ! temperature (K)
+    real(kind=esmf_kind_r8), intent(out) :: n_o(np)  ! number density of o
+    real(kind=esmf_kind_r8), intent(out) :: n_o2(np) ! number density of o2
+    real(kind=esmf_kind_r8), intent(out) :: n_n2(np) ! number density of n2
+! Local variables
+    real(kind=rp), parameter :: alt=100, ut=0, f107=150, f107a=150, ap(7)=9, xlong=0
+    real(kind=rp) :: t, d(10), zkm
+    integer       :: k,il,ip
+    real(4)       :: switch_legacy(1:25)
 
 ! set swich 7,8,10,14 to zero to avoid diurnal changes in output tempe
 ! #7 is for diurnal, #8 for semidiurnal, #10 is for all ut/longitudinal
@@ -33,36 +33,25 @@
     switch_legacy(14) = 0.
 
     call msisinit(parmfile=pf, switch_legacy=switch_legacy)
-! set 10.7cm flux, Ap to an average value
-    f107=150.
-    f107a=150.
-    ap=9.
-! set longitude, ut, it should not make difference to output
-    ut=0.
-    xlong=0.
-! default z0
-    alt = 100.
-! calculate temperature, species, for each lat,pres level,day
-    do k=1,nday
-      do il=1,nlat
-        do ip=1,np
-          call ghp8(iday(k),ut,alt(ip,il,k),xlat(il),xlong,f107a,f107,  &
-                    ap,pr(ip),zkm,d,t)
-          temp(ip,il,k)=t
-          n_n2(ip,il,k)=d(2)
-          n_o2(ip,il,k)=d(3)
-          n_o( ip,il,k)=d(4)
-        enddo
-      enddo
+! calculate temperature, species, for each pres level
+    do ip=1,np
+      call ghp8(real(iday, rp), ut, alt, real(xlat, rp),   &
+                xlong , f107a, f107, ap, real(pr(ip), rp), &
+                zkm, d, t)
+      temp(ip)=real(t,    esmf_kind_r8)
+      n_n2(ip)=real(d(2), esmf_kind_r8)
+      n_o2(ip)=real(d(3), esmf_kind_r8)
+      n_o( ip)=real(d(4), esmf_kind_r8)
     enddo
 
   end subroutine gettemp
 
   subroutine ghp8(day,utsec,z0,glat,glon,f107a,f107,ap,pres,alt,dn,tn)
 
-    use msis_constants, only : kB, NA, g0, rp
-    use msis_calc, only      : msiscalc
-    use msis_utils, only     : alt2gph
+    use msis_constants, only: kB, NA, g0, rp
+    use msis_calc,      only: msiscalc
+    use msis_utils,     only: alt2gph
+    use esmf,           only: esmf_kind_r8
 
     implicit none
 
