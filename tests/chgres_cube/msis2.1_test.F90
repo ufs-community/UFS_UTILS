df2e3b33d4f028ad841e8c080db23656172bb379
George Gayno <George.Gayno@noaa.gov>
2023-03-13 20:25:10 +0000

    The file with the check values also includes the input values. Remove old input value file.
    Add comments to unit test.
    
    Fixes #759.

--- tests/chgres_cube/msis2.1_test.F90
+++ tests/chgres_cube/msis2.1_test.F90
@@ -1,6 +1,20 @@
+program msistest
+
+! Test the MSIS library. This test is based on the test that
+! is part of the library. It was modified slightly so it could
+! be run under Github actions.
+!
+! It reads a text file (that is also part of the library) of 200 test
+! cases. The file contains variables input to routine gtd8d and
+! the check values of the variables output from that routine.
+! If the computed values differ from the check values by a small
+! threshold, the test fails.
+!
+! @author George.Gayno@noaa.gov
+
 !#######################################################################
-! MSIS® (NRL-SOF-014-1) SOFTWARE
-! NRLMSIS® empirical atmospheric model software. Use is governed by the
+! MSIS (NRL-SOF-014-1) SOFTWARE
+! NRLMSIS empirical atmospheric model software. Use is governed by the
 ! Open Source Academic Research License Agreement contained in the file
 ! nrlmsis2.1_license.txt, which is part of this software package. BY
 ! USING OR MODIFYING THIS SOFTWARE, YOU ARE AGREEING TO THE TERMS AND
@@ -15,85 +29,108 @@
 !==================================================================================================
 ! MSISTEST: Test program for NRLMSIS 2.1
 !==================================================================================================
-program msistest
 
   use msis_init, only          : msisinit
 
   implicit none
 
-  integer, parameter          :: nrec = 200
+  integer                     :: i, iyd, isec
+  integer                     :: mass ! Not use by v2.
 
-  integer                     :: iyd, mass, iyd_check, sec_check
   real(4)                     :: sec, alt, glat, glong, stl, f107a, f107, ap(7), apd
-  real(4)                     :: alt_check, glat_check, glong_check, &
-                                 stl_check, f107a_check, f107_check, ap_check
-  real(4)                     :: d(10),t(2)
-  real(4)                     :: d_check(10),t_check, epsilon
-  
-  integer                     :: i
-  character(128)              :: dummy
+  real(4)                     :: d(10), t(2), d_check(10), t_check
 
   !Initialize model
   call msisinit(parmpath='./data/',parmfile='msis21.parm')
 
-  !Open input and output files, loop through records, and call model
-  open(77,file='./data/msis2.1_test_in.txt',status='old')
-  open(78,file='/scratch1/NCEPDEV/da/George.Gayno/ufs_utils.git/UFS_UTILS/sorc/chgres_cube.fd/msis2.1.fd/msis2.1_test_ref_dp.txt',status='old')
-  read(77,*) dummy
-  read(78,*)
+! Open file that contains the variables input to routine
+! gtd8d and the check values for the variables output from
+! that routine.
+
+  open(78,file='./data/msis2.1_test_ref_dp.txt',status='old')
+  read(78,*)  ! Ignore first line of file.
+
+! Loop through all 200 records.
+
   do i = 1,200
-    read(77,*) iyd,sec,alt,glat,glong,stl,f107a,f107,apd
-    ap(1) = apd
-    call gtd8d(iyd,sec,alt,glat,glong,stl,f107a,f107,ap,mass,d,t)
-    print*,'after gdt8d ',i,d,t
+
     read(78,'(2i7,3f7.1,f7.2,3f7.1,10e13.4,1e13.4,f8.2)')  &
-      iyd_check,sec_check,alt_check,glat_check,glong_check,stl_check, &
-      f107a_check,f107_check,ap_check,d_check(1:10),t_check
-      print*,'after read ',i,iyd_check,sec_check,alt_check,glat_check,glong_check, &
-               stl_check,f107a_check,f107_check,ap_check,d_check,t_check
+         iyd,isec,alt,glat,glong,stl,f107a,f107,apd,d_check,t_check
+
+    ap(1) = apd
+    sec = float(isec)
+
+! Input variables are:
+!
+!   iyd   - year and day
+!   sec   - seconds
+!   alt   - altitude in km
+!   glat  - latitude in deg
+!   glong - longitude in deg
+!   stl   - local solar time
+!   f107a - 81 day average of solar activity index
+!   f107  - daily solar activity indiex
+!   ap    - daily geomagnetic activity index
+ 
+    call gtd8d(iyd,sec,alt,glat,glong,stl,f107a,f107,ap,mass,d,t)
 
 ! Check He number density
      call checkit(d(1), d_check(1), 2)
+
 ! Check O number density.
      call checkit(d(2), d_check(2), 4)
+
 ! Check N2 number density.
      call checkit(d(3), d_check(3), 6)
+
 ! Check O2 number density.
      call checkit(d(4), d_check(4), 8)
+
 ! Check Ar number density.
      call checkit(d(5), d_check(5), 10)
+
 ! Check Total mass density.
      call checkit(d(6), d_check(6), 12)
+
 ! Check H number density.
      call checkit(d(7), d_check(7), 14)
+
 ! Check N number density.
      call checkit(d(8), d_check(8), 16)
+
 ! Check Anomalous oxygen number density.
      call checkit(d(9), d_check(9), 18)
+
 ! Check NO number density.
      call checkit(d(10), d_check(10), 20)
+
 ! Check temperature at altitude.
      if ( abs(t(2)-t_check) > 0.01) stop 28
 
   enddo
 
-  close(77)
   close(78)
 
+  print*,"OK"
   print*,"SUCCESS!"
 
 end program msistest
+
+! Routine to check the calculated values against the
+! check values. A percentage threshold is used. Some
+! fields contain missing values (flag value 9.99e-38). 
 
 subroutine checkit(calc_val, check_val, istat)
 
  implicit none
 
  integer, intent(in) :: istat
+
  real(4), intent(in) :: calc_val, check_val
 
- real :: epsilon
+ real                :: epsilon
 
- if (check_val > 1) then
+ if (check_val > 1) then  ! Check value is not missing.
    epsilon =  abs(calc_val-check_val) / check_val
    if (epsilon > 0.001) stop istat
  else ! Check value is missing. Is computed value also missing?
