79917b1af539c89779cc9be4d0e347970de5b9b0
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2023-07-10 20:53:13 +0000

    Updates to unit test to successfully run the old test.

--- tests/global_cycle/ftst_land_increments.F90
+++ tests/global_cycle/ftst_land_increments.F90
@@ -9,25 +9,24 @@ program ftst_land_increments
 
  implicit none
 
- character(len=3) :: update_type
-
  integer :: my_rank, ierr, lsm, isot, ivegsrc
  integer :: lensfc, lsoil, l
 
  real, parameter :: EPSILON=0.001
 
+ real(kind=4), allocatable :: zsoil(:)
  real, allocatable :: rsoiltype(:)
  integer, allocatable :: mask(:)
  real, allocatable :: stc_bck(:,:)
  real, allocatable :: smc_anl(:,:)
  real, allocatable :: slc_anl(:,:)
  real, allocatable :: stc_anl(:,:)
+ integer, allocatable :: stc_updated(:), slc_updated(:)
 
  call mpi_init(ierr)
 
  call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr)
 
- update_type='stc' ! Update soil temperature.
  lsm=1             ! For noah land surface model.
  isot = 1          ! STATSGO soil type.
  ivegsrc = 1       ! IGBP vegetation type.
@@ -35,12 +34,20 @@ program ftst_land_increments
  
  lensfc= 3  ! Number of test points.
  
+ allocate(zsoil(lsoil))
+ zsoil(1) = -0.1
+ zsoil(2) = -0.4
+ zsoil(3) = -1.0
+ zsoil(4) = -2.0
+
  allocate(rsoiltype(lensfc))      ! Soil type.
  allocate(mask(lensfc))           ! Land mask
  allocate(stc_bck(lensfc,lsoil))  ! Background soil temperature (K).
  allocate(smc_anl(lensfc,lsoil))  ! Analyzed total soil moisture.
  allocate(slc_anl(lensfc,lsoil))  ! Analyzed liquid soil moisture.
  allocate(stc_anl(lensfc,lsoil))  ! Analyzed soil temperature (K).
+ allocate(stc_updated(lensfc))
+ allocate(slc_updated(lensfc))
 
 ! Point 1 is above freezing before the adjustment
 ! and above freezing after the adjustment. Therefore,
@@ -80,8 +87,9 @@ program ftst_land_increments
  slc_anl(3,:) = .25
  stc_anl(3,:) =  271.0
 
- call apply_land_da_adjustments_stc(lsm, isot, ivegsrc,lensfc, &
-           lsoil, rsoiltype, mask, stc_bck, stc_anl, smc_anl, slc_anl)
+ call apply_land_da_adjustments_soil(lsm, isot, ivegsrc,lensfc, &
+           lsoil, rsoiltype, mask, stc_bck, stc_anl, smc_anl, slc_anl, &
+           stc_updated, slc_updated,zsoil)
 
  do l = 1, lsoil
    if (abs(smc_anl(1,l) - 0.25) > EPSILON) stop 2
