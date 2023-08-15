fc1759322117a13cf0f130c4b141080eee57114b
George.Gayno <george.gayno@noaa.gov>
2023-08-15 15:31:49 +0000

    Minor updates to the wcoss2 driver grid script.
    Fixes #821.

--- driver_scripts/driver_grid.wcoss2.sh
+++ driver_scripts/driver_grid.wcoss2.sh
@@ -4,7 +4,7 @@
 #PBS -e log
 #PBS -q debug
 #PBS -A GFS-DEV
-#PBS -l walltime=00:15:00
+#PBS -l walltime=00:30:00
 #PBS -N make_grid
 #PBS -l select=1:ncpus=24:mem=500GB
 
@@ -70,7 +70,7 @@ module list
 # Set grid specs here.
 #-----------------------------------------------------------------------
 
-export gtype=regional_esg      # 'uniform', 'stretch', 'nest', 
+export gtype=uniform      # 'uniform', 'stretch', 'nest', 
                                # 'regional_gfdl', 'regional_esg'
 
 export make_gsl_orog=false     # When 'true' will output 'oro' files for
@@ -82,7 +82,7 @@ export vegsoilt_frac='.false.'  # When true, outputs percent of each
                                # outputs the dominant category. A
                                # Fortran logical, so include the dots.
 
-export veg_type_src="modis.igbp.0.05" #  Vegetation type data.
+export veg_type_src="viirs.v2.igbp.30s" #  Vegetation type data.
                                 # For viirs-based vegetation type data, set to:
                                 # 1) "viirs.v2.igbp.30s" for global 30s data
                                 # For the modis-based data, set to:
@@ -92,15 +92,15 @@ export veg_type_src="modis.igbp.0.05" #  Vegetation type data.
                                 # 4) "modis.igbp.nh.30s" for N Hemis 30s data
                                 # 5) "modis.igbp.30s" for global 30s data
 
-export soil_type_src="statsgo.0.05"  # Soil type data
+export soil_type_src="bnu.v2.30s"  # Soil type data
+                                # For Beijing Normal Univ. data, set to:
+                                # 1) "bnu.v2.30s" for global 30s data.
                                 # For STATSGO soil type data, set to:
                                 # 1) "statsgo.0.05" for global 0.05-deg data
                                 # 2) "statsgo.0.03" for global 0.03-deg data
                                 # 3) "statsgo.conus.30s" for CONUS 30s data
                                 # 4) "statsgo.nh.30s" for NH 30s data
                                 # 5) "statsgo.30s" for global 30s data
-                                # For Beijing Normal Univ. data, set to:
-                                # 1) "bnu.v2.30s" for global 30s data.
 
 if [ $gtype = uniform ]; then
   export res=96
