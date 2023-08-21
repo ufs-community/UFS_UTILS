4bc4e85b0b04592fdd55693d791711d7e09026ce
George Gayno <George.Gayno@noaa.gov>
2023-08-21 08:55:48 -0500

    Adjust requested resources for Orion grid_gen regression tests.
    Fixes #821.

--- reg_tests/grid_gen/driver.orion.sh
+++ reg_tests/grid_gen/driver.orion.sh
@@ -70,7 +70,7 @@ TEST1=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:15:00 -A $PROJECT_
 #-----------------------------------------------------------------------------
 
 LOG_FILE2=${LOG_FILE}02
-TEST2=$(sbatch --parsable --ntasks-per-node=24 --nodes=1 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.viirs.bnu \
+TEST2=$(sbatch --parsable --ntasks-per-node=15 --nodes=2 -t 0:15:00 -A $PROJECT_CODE -q $QUEUE -J c96.viirs.bnu \
       -o $LOG_FILE2 -e $LOG_FILE2 ./c96.viirs.bnu.sh)
 
 #-----------------------------------------------------------------------------
