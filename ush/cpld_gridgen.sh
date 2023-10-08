a9309cb5931c5d40925e2f70309fe065ae4bd220
denise.worthen <Denise.Worthen@noaa.gov>
2023-10-08 08:19:59 -0400

    set lat type to cap for ncremap
    * generate destination rectilinear grids with ncremap
    using lat type "cap"

--- ush/cpld_gridgen.sh
+++ ush/cpld_gridgen.sh
@@ -64,9 +64,7 @@ if [ $RESNAME = 100 ]; then
     export EDITSFILE=topo_edits_011818.nc
     if [ $DO_POSTWGTS == .true. ]; then
 	#pre-generate SCRIP files for dst rectilinear grids using NCO
-	# TODO: is the stagger really correct? The first pt is at 0.0E?
-	# should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
+	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr#lat_typ=cap
     fi
 fi
 
@@ -77,10 +75,8 @@ if [ $RESNAME = 050 ]; then
     export EDITSFILE='none'
     if [ $DO_POSTWGTS == .true. ]; then
 	#pre-generate SCRIP files for dst rectilinear grids using NCO
-	# TODO: is the stagger really correct? The first pt is at 0.0E?
-	# should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
-	ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr
+	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr#lat_typ=cap
+	ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr#lat_typ=cap
     fi
 fi
 
@@ -91,11 +87,9 @@ if [ $RESNAME = 025 ]; then
     export EDITSFILE=All_edits.nc
     if [ $DO_POSTWGTS == .true. ]; then
 	#pre-generate SCRIP files for dst rectilinear grids using NCO
-	# TODO: is the stagger really correct? The first pt is at 0.0E?
-	# should lat_type be cap? #lon_typ=grn_ctr#lat_typ=cap
-	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr
-	ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr
-	ncremap -g ${OUTDIR_PATH}/rect.0p25_SCRIP.nc -G latlon=721,1440#lon_typ=grn_ctr
+	ncremap -g ${OUTDIR_PATH}/rect.1p0_SCRIP.nc -G latlon=181,360#lon_typ=grn_ctr#lat_typ=cap
+	ncremap -g ${OUTDIR_PATH}/rect.0p5_SCRIP.nc -G latlon=361,720#lon_typ=grn_ctr#lat_typ=cap
+	ncremap -g ${OUTDIR_PATH}/rect.0p25_SCRIP.nc -G latlon=721,1440#lon_typ=grn_ctr#lat_typ=cap
     fi
 fi
 
