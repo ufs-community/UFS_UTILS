c8b32532a7b44468f40e2862cd4ded31a0b2055e
George Gayno <George.Gayno@noaa.gov>
2023-10-17 19:28:10 +0000

    Fix typo.
    Fixes #860.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon
+    'sphinx.ext.napoleon'
 ]
 
 #bibtex_bibfiles = ['references.bib']
