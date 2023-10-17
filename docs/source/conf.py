f515e4e1e9e249110e72e1f52c3eb27f3de8c034
George Gayno <George.Gayno@noaa.gov>
2023-10-17 17:52:30 +0000

    Comment out unused file.
    Fixes #860.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ extensions = [
     'sphinxcontrib.bibtex'
 ]
 
-bibtex_bibfiles = ['references.bib']
+#bibtex_bibfiles = ['references.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
