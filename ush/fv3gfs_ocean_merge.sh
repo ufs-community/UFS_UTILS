6e18c0f3364aa6c520d6c00fd9669fb86f26eeac
sanatcumar <sanath.kumar@noaa.gov>
2023-11-15 19:59:30 +0000

    Fixed issues with nck commands with the new libaries from last PR

--- ush/fv3gfs_ocean_merge.sh
+++ ush/fv3gfs_ocean_merge.sh
@@ -33,8 +33,9 @@ EOF
     echo '"'${TEMP_DIR}/ocean_merged/C${res}.mx${ocn}/C${res}.mx${ocn}.tile${tnum}.nc'"' >> INPS
 
     cat INPS
-    time ${exec_dir}/orog < INPS
 
+    time ${exec_dir}/orog < INPS
+   ncks -4 -O ${TEMP_DIR}/ocean_merged/C${res}.mx${ocn}/C${res}.mx${ocn}.tile${tnum}.nc  ${TEMP_DIR}/ocean_merged/C${res}.mx${ocn}/C${res}.mx${ocn}.tile${tnum}.nc		
     ncks -A -v lake_frac,lake_depth ${TEMP_DIR}/ocean_merged/C${res}.mx${ocn}/C${res}.mx${ocn}.tile${tnum}.nc out.oro.nc
     #cp out.oro.nc $out_dir/oro_C${res}.mx${ocn}.tile${tnum}.nc
     cp out.oro.nc $orog_dir/oro.C${res}.tile${tnum}.nc
