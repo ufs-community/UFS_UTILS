76dd01a7827cb0436518bce24c771e7f9443ceb4
sanatcumar <sanath.kumar@noaa.gov>
2023-11-16 20:40:44 +0000

    Made filenames consistent

--- ush/fv3gfs_ocean_merge.sh
+++ ush/fv3gfs_ocean_merge.sh
@@ -39,5 +39,5 @@ EOF
     ncks -A -v lake_frac,lake_depth ${TEMP_DIR}/ocean_merged/C${res}.mx${ocn}/C${res}.mx${ocn}.tile${tnum}.nc out.oro.nc
     #cp out.oro.nc $out_dir/oro_C${res}.mx${ocn}.tile${tnum}.nc
     cp out.oro.nc $orog_dir/oro.C${res}.tile${tnum}.nc
-    cp C${res}_grid.tile${tnum}.nc $out_dir/C${res}_grid.tile${tnum}.nc
+    #cp C${res}_grid.tile${tnum}.nc $out_dir/C${res}_grid.tile${tnum}.nc
     done
