7c74c2cd1f7d9c81d8252758d4be81d6c485874a
George Gayno <George.Gayno@noaa.gov>
2023-12-21 20:29:33 +0000

    Update gdas_init utility to only use the 'mx' version of the 'orog' and 'sfc' files.
    Fixes #867.

--- util/gdas_init/set_fixed_files.sh
+++ util/gdas_init/set_fixed_files.sh
@@ -4,10 +4,21 @@
 # Set directory names and file names for orog data.
 #---------------------------------------------------------------------------
 
-if [ -z "${OCNRES}" ]; then  # for uncoupled runs.
-  ORO_DIR="${CTAR}"
-  ORO_NAME="${CTAR}_oro_data"
-else                         # for coupled runs.
-  ORO_DIR="${CTAR}"
-  ORO_NAME="${CTAR}.mx${OCNRES}_oro_data"
+if [ ${CTAR} == 'C48' ] ; then
+  OCNRES='500'
+elif [ ${CTAR} == 'C96' ]; then
+  OCNRES='500'
+elif [ ${CTAR} == 'C192' ]; then
+  OCNRES='050'
+elif [ ${CTAR} == 'C384' ]; then
+  OCNRES='025'
+elif [ ${CTAR} == 'C768' ]; then
+  OCNRES='025'
+elif [ ${CTAR} == 'C1152' ]; then
+  OCNRES='025'
+else
+  OCNRES='025'
 fi
+
+ORO_DIR="${CTAR}"
+ORO_NAME="${CTAR}.mx${OCNRES}_oro_data"
