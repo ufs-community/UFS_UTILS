580f5ba80d78666321ba3cc08485f43cace8e655
henrywinterbottom-wxdev <henry.winterbottom.wxdev@gmail.com>
2024-02-27 10:38:07 -0700

    Update for Rocky 8.

--- modulefiles/build.hera.intel.lua
+++ modulefiles/build.hera.intel.lua
@@ -5,7 +5,7 @@ Load environment to compile UFS_UTILS on Hera using Intel
 hpss_ver=os.getenv("hpss_ver") or ""
 load(pathJoin("hpss", hpss_ver))
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev-rocky8/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
