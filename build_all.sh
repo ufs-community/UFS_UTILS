22c6787db197f467306d1c78db775dc0a64609f3
Denise Worthen <denise.worthen@noaa.gov>
2024-05-14 14:20:07 -0400

    revert temporary build changes

--- build_all.sh
+++ build_all.sh
@@ -19,8 +19,8 @@ source "${DIR_ROOT}/sorc/machine-setup.sh"
 
 # User Options
 target=${target:-"NULL"}
-#compiler=${compiler:-"intel"}
-compiler="gnu"
+compiler=${compiler:-"intel"}
+#compiler="gnu"
 
 if [[ "$target" == "linux.*" || "$target" == "macosx.*" ]]; then
   unset -f module
@@ -44,8 +44,8 @@ if [[ ! -d "${DIR_ROOT}/ccpp-physics/physics" ]]; then
 fi
 
 # Collect BUILD Options
-#CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Release}"
-CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Debug"
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-Release}"
+#CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Debug"
 
 # Install options; destination for built executables, libraries, CMake Package config
 CMAKE_FLAGS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX:-${DIR_ROOT}} -DCMAKE_INSTALL_BINDIR=${INSTALL_BINDIR:-exec}"
@@ -54,8 +54,8 @@ CMAKE_FLAGS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX:-${DIR_ROOT}} -DCMAKE_INS
 # The unit test data download is part of the build system. Not all machines can
 # access the EMC ftp site, so turn off the build (-DBUILD_TESTING=OFF) of the units tests accordingly.
 # Those with access to the EMC ftp site are: Orion and Hera.
-#CMAKE_FLAGS+=" -DBUILD_TESTING=${BUILD_TESTING:-OFF}"
-CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
+CMAKE_FLAGS+=" -DBUILD_TESTING=${BUILD_TESTING:-OFF}"
+#CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
 
 # Allow users of this script to provide CMake options e.g. -DGFS=ON|OFF to build GFS specific utilities only
 CMAKE_OPTS=${CMAKE_OPTS:-}
