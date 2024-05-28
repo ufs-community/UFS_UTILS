f8a640bb0d1469b5842643399b7ff46715bb34ba
Wei Huang <Wei.Huang@noaa.gov>
2024-05-28 14:59:21 +0000

    to compile on GCP and Azure

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -38,17 +38,43 @@ elif [[ -d /scratch1 ]] ; then
     fi
     target=hera
     module purge
-elif [[ -d /gpfs && -d /ncrc ]] ; then
+elif [[ -d /lustre && -d /ncrc ]] ; then
     # We are on GAEA.
     if ( ! eval module help > /dev/null 2>&1 ) ; then
-      # We cannot simply load the module command.  The GAEA
-      # /etc/profile modifies a number of module-related variables
-      # before loading the module command.  Without those variables,
-      # the module command fails.  Hence we actually have to source
-      # /etc/profile here.
-      source /etc/profile
+        # We cannot simply load the module command.  The GAEA
+        # /etc/profile modifies a number of module-related variables
+        # before loading the module command.  Without those variables,
+        # the module command fails.  Hence we actually have to source
+        # /etc/profile here.
+        source /etc/profile
+        __ms_source_etc_profile=yes
+    else
+        __ms_source_etc_profile=no
     fi
-    module reset
+    module purge > /dev/null 2>&1
+    module purge
+# clean up after purge
+    unset _LMFILES_
+    unset _LMFILES_000
+    unset _LMFILES_001
+    unset LOADEDMODULES
+    module load modules
+    if [[ -d /opt/cray/ari/modulefiles ]] ; then
+        module use -a /opt/cray/ari/modulefiles
+    fi
+    if [[ -d /opt/cray/pe/ari/modulefiles ]] ; then
+        module use -a /opt/cray/pe/ari/modulefiles
+    fi
+    if [[ -d /opt/cray/pe/craype/default/modulefiles ]] ; then
+        module use -a /opt/cray/pe/craype/default/modulefiles
+    fi
+    if [[ -s /etc/opt/cray/pe/admin-pe/site-config ]] ; then
+        source /etc/opt/cray/pe/admin-pe/site-config
+    fi
+    if [[ "$__ms_source_etc_profile" == yes ]] ; then
+      source /etc/profile
+      unset __ms_source_etc_profile
+    fi
     target=gaea
 elif [[ "$(hostname)" =~ "Orion" ]]; then
     target="orion"
@@ -67,16 +93,14 @@ elif [[ -d /data/prod ]] ; then
     fi
     target=s4
     module purge
-elif [[ "$(dnsdomainname)" =~ "pw" ]]; then
-    if [[ "${PW_CSP}" == "aws" ]]; then # TODO: Add other CSPs here.
+else
+    if [[ "${PW_CSP}" == "aws" || "${PW_CSP}" == "azure" || "${PW_CSP}" == "google" ]]; then
 	target=noaacloud
         module purge
     else
+        echo WARNING: UNKNOWN PLATFORM 1>&2
         echo WARNING: UNSUPPORTED CSP PLATFORM 1>&2; exit 99
     fi
-else
-
-    echo WARNING: UNKNOWN PLATFORM 1>&2
 fi
 
 unset __ms_shell
