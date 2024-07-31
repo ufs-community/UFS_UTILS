be2a680ddb49cbf8496ba002aa5c830d44bd5ccb
George Gayno <ggayno@hercules-login-2.hpc.msstate.edu>
2024-07-31 13:55:23 -0500

    Combine routine minmxj and mnmxja into a single routine called minmax. Write unit test for this new routine.
    Fixes #970.

--- sorc/orog_mask_tools.fd/orog.fd/orog_utils.F90
+++ sorc/orog_mask_tools.fd/orog.fd/orog_utils.F90
@@ -12,8 +12,62 @@
  private
 
  public :: latlon2xyz
+ public :: minmax
 
  contains
+
+!> Print out the maximum and minimum values of
+!! an array and optionally pass back the i/j
+!! location of the maximum.
+!!
+!! @param[in] im The 'i' dimension of the array.
+!! @param[in] jm The 'j' dimension of the array.
+!! @param[in] a The array to check.
+!! @param[in] title Name of the data to be checked.
+!! @param[out] imax The 'i' location of the maximum.
+!! @param[out] jmax The 'j' location of the maximum.
+!!
+!! @author Jordan Alpert NOAA/EMC
+ subroutine minmax(im,jm,a,title,imax,jmax)
+
+ implicit none
+
+ character(len=8), intent(in)   :: title
+ 
+ integer, intent(in)            :: im, jm
+ integer, intent(out), optional :: imax, jmax
+
+ real,    intent(in)            :: a(im,jm)
+
+ integer                        :: i, j
+
+ real                           :: rmin,rmax
+
+ rmin=huge(a)
+ rmax=-rmin
+
+ if (present(imax) .and. present(jmax)) then
+   imax=0
+   jmax=0
+ endif
+
+ do j=1,jm
+   do i=1,im
+     if(a(i,j) >= rmax) then
+       rmax=a(i,j)
+       if (present(imax) .and. present(jmax)) then
+         imax = i
+         jmax = j
+       endif
+     endif
+     if(a(i,j) <= rmin)rmin=a(i,j)
+   enddo
+ enddo
+
+ write(6,150) title,rmin,rmax
+150 format(' - ',a8,' MIN=',e13.4,2x,'MAX=',e13.4)
+
+ end subroutine minmax
 
 !> Convert from latitude and longitude to x,y,z coordinates.
 !!
@@ -23,6 +77,7 @@
 !! @param[out] x 'x' Coordinate of the converted points.
 !! @param[out] y 'y' Coordinate of the converted points.
 !! @param[out] z 'z' Coordinate of the converted points.
+!!
 !! @author GFDL programmer
  subroutine latlon2xyz(siz,lon, lat, x, y, z)
 
