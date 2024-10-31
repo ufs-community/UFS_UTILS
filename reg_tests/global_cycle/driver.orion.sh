7efdbedd90b960e13781c1840ab88aea82d57b63
George Gayno <George.Gayno@noaa.gov>
2024-10-31 15:19:16 -0500

    Update Orion regresson test driver scripts to default to intel LLVM.
    Fixes #879.

--- reg_tests/global_cycle/driver.orion.sh
+++ reg_tests/global_cycle/driver.orion.sh
@@ -22,7 +22,7 @@ set -x
 
 source ../../sorc/machine-setup.sh > /dev/null 2>&1
 module use ../../modulefiles
-module load build.$target.intel
+module load build.$target.intelllvm
 module list
 
 export WORK_DIR="${WORK_DIR:-/work/noaa/stmp/$LOGNAME}"
