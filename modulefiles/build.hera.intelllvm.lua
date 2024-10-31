2324720c51b659b988b8ce8f7a5f79cfc5f1d6ce
George Gayno <George.Gayno@noaa.gov>
2024-10-31 18:58:40 +0000

    Update the intelLLVM version 2024 on Hera.
    Fixes #879.

--- modulefiles/build.hera.intelllvm.lua
+++ modulefiles/build.hera.intelllvm.lua
@@ -14,7 +14,7 @@ stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 load("gnu")
-load("intel/2023.2.0")
+load("intel/2024.2.1")
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
