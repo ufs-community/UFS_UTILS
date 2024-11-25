6936faa92a7893f13ad3cbc242dafd7c2fd07961
George Gayno <George.Gayno@noaa.gov>
2024-11-25 18:18:35 +0000

    Add more test points to ftst_get_index.F90.
    Fixes #1000

--- tests/orog/ftst_get_index.F90
+++ tests/orog/ftst_get_index.F90
@@ -21,8 +21,13 @@
 
  print*,"Starting test of get_index."
 
+! The get_index routine contains three 'if' blocks. The test
+! points are designed to check each block.
+
 ! Point 1 - At equator. Western edge of grid cell at
-! Greenwich.
+! Greenwich. This tests the third 'if' block of the routine.
+
+ print*,"Test point 1."
 
  lato(1) = 0.0; lono(1) = 0.0
  lato(2) = 1.0; lono(2) = 0.5
@@ -40,10 +45,12 @@
    if (ilist(i) /= i) stop 8
  enddo
 
-! Point 2 - At equator. Grid cell centered at
-! Greenwich.
+! Point 2 - At equator. Grid cell crosses Greenwich.
+! This tests the second 'if' block of the routine.
 
- lato(1) = 0.0; lono(1) = -0.5
+ print*,"Test point 2."
+
+ lato(1) = 0.0; lono(1) = 359.5
  lato(2) = 1.0; lono(2) = 0.0
  lato(3) = 0.0; lono(3) = 0.5
  lato(4) = -1.0; lono(4) = 0.0
@@ -56,13 +63,19 @@
  if (jen /= 10925) stop 14
  if (numx /= 121)  stop 16
  ii = 1
- do i = -59, 61, 1
+ do i = 43141, 43200  ! East of greenwich
+   if (ilist(ii) /= i) stop 18
+   ii = ii + 1
+ enddo
+ do i = 1, 61  ! West of greenwich
    if (ilist(ii) /= i) stop 18
    ii = ii + 1
  enddo
 
 ! Point 3 - At equator. Grid cell centered at
-! the dateline.
+! the dateline. This tests the third 'if' block.
+
+ print*,"Test point 3."
 
  lato(1) = -1.0; lono(1) = 179.0
  lato(2) = 1.0; lono(2) = 179.0
@@ -80,6 +93,27 @@
  do i = 21481, 21721
    if (ilist(ii) /= i) stop 28
    ii = ii + 1
+ enddo
+
+! Point 4 - At North Pole. Grid cell centered at
+! Greenwich. This tests the first 'if' block.
+
+ print*,"Test point 4."
+
+ lato(1) = 89.0; lono(1) = 359.5
+ lato(2) = 90.0; lono(2) = 359.5
+ lato(3) = 90.0; lono(3) = 0.5
+ lato(4) = 89.0; lono(4) = 0.5
+
+ ilist = -999
+
+ call get_index(imn,jmn,npts,lono,lato,delxn,jst,jen,ilist,numx)
+
+ if (jst /= 21476) stop 32
+ if (jen /= 21600) stop 34
+ if (numx /= 43200)  stop 36
+ do i = 1, numx
+   if (ilist(i) /= i) stop 38
  enddo
 
  print*,"OK"
