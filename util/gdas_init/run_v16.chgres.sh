28e0e0d69898631d9c33b436434228dc8927097b
George Gayno <ggayno@hercules-login-4.hpc.msstate.edu>
2025-01-14 12:46:17 -0600

    Remove temporary logic from ./util/gdas_init/run_v16.chgres.sh.
    Fixes #1000.

--- util/gdas_init/run_v16.chgres.sh
+++ util/gdas_init/run_v16.chgres.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 #---------------------------------------------------------------------------
 # Run chgres using v16 netcdf history data as input.  These history
@@ -13,7 +13,7 @@ set -x
 MEMBER=$1
 
 FIX_FV3=$UFS_DIR/fix
-FIX_ORO=${FIX_FV3}/orog.lowres
+FIX_ORO=${FIX_FV3}/orog
 FIX_AM=${FIX_FV3}/am
 
 WORKDIR=${WORKDIR:-$OUTDIR/work.${MEMBER}}
