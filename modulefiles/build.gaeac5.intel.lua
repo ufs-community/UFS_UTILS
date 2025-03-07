c87310700b435f36d17b3343a871857eac77ce08
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2025-03-07 16:18:28 -0500

    Update hsi path on Gaea clusters (#1031)
    Fixes #1030.

--- modulefiles/build.gaeac5.intel.lua
+++ modulefiles/build.gaeac5.intel.lua
@@ -2,7 +2,7 @@ help([[
 Load environment to compile UFS_UTILS on Gaea using Intel
 ]])
 
-prepend_path("MODULEPATH", "/sw/rdtn/modulefiles")
+prepend_path("MODULEPATH", "/usw/hpss/modulefiles")
 load("hsi")
 
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
