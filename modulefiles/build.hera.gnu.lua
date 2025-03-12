eb2d4d802cf381c6b3971ff94a87ebabed132ea6
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2025-03-12 13:28:25 -0400

    Use spack-stack-1.6.0 environment, in accordance with the current UFS-WM stack (#1028)
    The main reason for this change is so that G2 v3.5.1 can be used, which can handle
    'large' GRIB2 files. This is needed so chgres_cube can be updated to read 
    RRFS GRIB2 files.

--- modulefiles/build.hera.gnu.lua
+++ modulefiles/build.hera.gnu.lua
@@ -1,14 +1,16 @@
 help([[
-Load environment to compile UFS_UTILS on Hera using Gnu
+Load environment to compile UFS_UTILS on Hera using Gnu 13.3
 ]])
 
 hpss_ver=os.getenv("hpss_ver") or ""
 load(pathJoin("hpss", hpss_ver))
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8-ompi416/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/stmp1/role.epic/installs/gnu/modulefiles")
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/stmp1/role.epic/installs/openmpi/modulefiles")
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/stmp1/role.epic/spack-stack/spack-stack-1.6.0_gnu13/envs/fms-2024.01/install/modulefiles/Core")
 
-stack_gcc_ver=os.getenv("stack_gcc_ver") or "9.2"
-load(pathJoin("stack-gcc", gnu_ver))
+stack_gnu_ver=os.getenv("stack_gnu_ver") or "13.3.0"
+load(pathJoin("stack-gcc", stack_gnu_ver))
 
 stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.6"
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
@@ -19,7 +21,7 @@ load(pathJoin("cmake", cmake_ver))
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 load(pathJoin("bacio", bacio_ver))
 
-g2_ver=os.getenv("g2_ver") or "3.4.5"
+g2_ver=os.getenv("g2_ver") or "3.5.1"
 load(pathJoin("g2", g2_ver))
 
 ip_ver=os.getenv("ip_ver") or "4.3.0"
@@ -41,13 +43,22 @@ load(pathJoin("w3emc", w3emc_ver))
 sigio_ver=os.getenv("sigio_ver") or "2.3.2"
 load(pathJoin("sigio", sigio_ver))
 
-nccmp_ver=os.getenv("nccmp_ver") or "1.9.1"
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
 
-esmf_ver=os.getenv("esmf_ver") or "8.5.0"
+esmf_ver=os.getenv("esmf_ver") or "8.6.0"
 load(pathJoin("esmf", esmf_ver))
 
-nco_ver=os.getenv("nco_ver") or "5.1.6"
+nco_ver=os.getenv("nco_ver") or "5.0.6"
 load(pathJoin("nco", nco_ver))
+
+
+prepend_path("CPPFLAGS", " -I/apps/slurm_hera/23.11.3/include/slurm"," ")
+prepend_path("LD_LIBRARY_PATH", "/apps/slurm_hera/23.11.3/lib")
+setenv("LD_PRELOAD", "/scratch2/NCEPDEV/stmp1/role.epic/installs/gnu/13.3.0/lib64/libstdc++.so.6")
+
+setenv("CC", "mpicc")
+setenv("CXX", "mpic++")
+setenv("FC", "mpif90")
 
 whatis("Description: UFS_UTILS build environment")
