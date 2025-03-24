aeabd3a27a4995ade6fda392e068302170b9963d
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2025-03-24 15:49:18 +0000

    George's updates to the reg_test on WCOSS.

--- reg_tests/regrid_sfc/driver.sh
+++ reg_tests/regrid_sfc/driver.sh
@@ -32,6 +32,10 @@ if [[ "$compiler" == "intelllvm" ]]; then
   fi
 fi
 module load build.$target.$compiler
+if [[ "$target" == "wcoss2" ]];then
+  module load netcdf
+  module load nccmp
+fi
 set +x
 module list
 set -x
@@ -71,6 +75,12 @@ elif [[ "$target" == "hercules" ]];then
   export HOMEreg=/work/noaa/nems/role-nems/ufs_utils.hercules/reg_tests/regrid_sfc
   export APRUN_REGRID=srun
   PARTITION=hercules
+elif [[ "$target" == "wcoss2" ]];then
+  WORK_DIR="${WORK_DIR:-/lfs/h2/emc/stmp/$LOGNAME}"
+  PROJECT_CODE="${PROJECT_CODE:-GFS-DEV}"
+  QUEUE="${QUEUE:-dev}"
+  export HOMEreg=/lfs/h2/emc/nems/noscrub/emc.nems/UFS_UTILS/reg_tests/regrid_sfc
+  export APRUN_REGRID="mpiexec -n 6 -ppn 6 --cpu-bind core"
 fi
 
 DATA_DIR="${WORK_DIR}/reg-tests/regrid_sfc"
@@ -79,16 +89,35 @@ export NWPROD=$PWD/../..
 LOG_FILE=consistency.log01
 rm -f $LOG_FILE
 export DATA="${DATA_DIR}/test1"
-TEST1=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:05:00 -A $PROJECT_CODE -q $QUEUE -J gauss2fv3incr \
+if [[ "$target" == "wcoss2" ]];then
+  TEST1=$(qsub -V -o $LOG_FILE -e $LOG_FILE -q $QUEUE -A $PROJECT_CODE -l walltime=00:05:00 \
+        -N gauss2fv3incr -l select=1:ncpus=6:ompthreads=1:mem=10GB ./gauss2fv3incr.sh)
+else
+  TEST1=$(sbatch --parsable --ntasks-per-node=6 --nodes=1 -t 0:05:00 -A $PROJECT_CODE -q $QUEUE -J gauss2fv3incr \
       --partition=$PARTITION -o $LOG_FILE -e $LOG_FILE ./gauss2fv3incr.sh)
+fi
 
 LOG_FILE=consistency.log
 rm -f $LOG_FILE summary.log
+
+if [[ "$target" == "wcoss2" ]];then
+
+this_dir=$PWD
+qsub -V -o ${LOG_FILE} -e ${LOG_FILE} -q $QUEUE -A $PROJECT_CODE -l walltime=00:01:00 \
+        -N summary -l select=1:ncpus=1:mem=100MB -W depend=afterok:$TEST1 << EOF
+#!/bin/bash
+cd $this_dir
+grep -a '<<<' ${LOG_FILE}?? | grep -v echo > ./summary.log
+EOF
+
+else
+
 sbatch --partition=$PARTITION --nodes=1  -t 0:01:00 -A $PROJECT_CODE -J summary -o $LOG_FILE -e $LOG_FILE \
-       --open-mode=append -q $QUEUE -d\
-       afterok:$TEST1 << EOF
+       --open-mode=append -q $QUEUE -d afterok:$TEST1 << EOF
 #!/bin/bash
 grep -a '<<<' ${LOG_FILE}* > ./summary.log
 EOF
+
+fi
 
 exit
