87dbf67e08ca5d79efabd4561a33ac21295b256e
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2025-03-31 22:13:05 +0000

    Fix corner / center bug.

--- sorc/regrid_sfc.fd/grids_IO.F90
+++ sorc/regrid_sfc.fd/grids_IO.F90
@@ -74,6 +74,8 @@
      call error_handler("unknown grid_setup%descriptor in setup_grid", 1)
  end select
 
+ call error_handler("CSD - through create_grid_gauss", 1)
+
 !--------------------------
 ! Calculate and add the mask
 
@@ -133,8 +135,8 @@
 
 !> read variables from fv3 netcdf restart file into ESMF Fields
 !! @param[in] localpet          local pet
-!! @param[in] i_dim             latitudinal dimension
-!! @param[in] j_dim             longitudinal dimension
+!! @param[in] i_dim             longitudinal dimension
+!! @param[in] j_dim             latitudinal dimension
 !! @param[in] fname_read        file name to read in
 !! @param[in] dir_read          directory of file name to read
 !! @param[in] grid_setup        grid details
@@ -231,8 +233,8 @@
 
 !> read lat and lon from SCRIP file, for use in Gaussian grid
 !! @param[in] localpet          local pet
-!! @param[in] i_dim             latitudinal dimension
-!! @param[in] j_dim             longitudinal dimension
+!! @param[in] i_dim             longitudinal dimension
+!! @param[in] j_dim             latitudinal dimension
 !! @param[in] fname_read        file name to read in
 !! @param[in] dir_read          directory of file name to read
 !! @param[in] grid_setup        grid details
@@ -264,9 +266,12 @@
  real(esmf_kind_r8)              :: vec1(i_dim*j_dim), vec4(4,i_dim*j_dim)
  real(esmf_kind_r8), allocatable :: array_lat(:,:,:), array_lon(:,:,:), array2d(:,:)
  real(esmf_kind_r8)              :: array_tmp(i_dim,j_dim)
+ type(esmf_staggerloc)           :: staggerloc(2)
  character(len=30)               :: vname
  character(len=15), dimension(2) :: lonvar_list
  character(len=15), dimension(2) :: latvar_list
+
+ integer, dimension(2) :: print_count
 
  ! center coord names
  lonvar_list(1) = 'grid_center_lon'
@@ -277,23 +282,35 @@
  latvar_list(2) = 'grid_corner_lat'
 
  ! Create the fields
- do v=1,2
+ staggerloc = (/ ESMF_STAGGERLOC_CENTER, ESMF_STAGGERLOC_CORNER /)
+ do v=1,2 ! CSD
+        call ESMF_GridGetFieldBounds(gauss_grid, localDe=localpet, &
+                staggerloc=staggerloc(v), totalCount=print_count, rc=ierr)
+
+        call ESMF_GridGetCoord(gauss_grid, staggerLoc=ESMF_STAGGERLOC_CORNER,    &
+          coordDim=2,                           &
+          array=arrayCoordY, rc=rc)
+
+        write(6,*) 'CSD - get from grid ', v, localpet,  ':', print_count
+ enddo
+ v=2
         vname="gauss_grid_" // trim(lonvar_list(v))
         lon_fields(v) = ESMF_FieldCreate(gauss_grid, &
                                    typekind=ESMF_TYPEKIND_R8, &
-                                   staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                   staggerloc=staggerloc(v), &
                                    name=vname, rc=ierr)
         if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
         call error_handler("IN FieldCreate", ierr)
 
+
         vname="gauss_grid_" // trim(latvar_list(v))
         lat_fields(v) = ESMF_FieldCreate(gauss_grid, &
                                    typekind=ESMF_TYPEKIND_R8, &
-                                   staggerloc=ESMF_STAGGERLOC_CENTER, &
+                                   staggerloc=staggerloc(v), &
                                    name=vname, rc=ierr)
         if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
         call error_handler("IN FieldCreate", ierr)
- enddo
+ !enddo
 
  if (localpet==0) then
      allocate(array_lat(2, i_dim, j_dim))
@@ -317,6 +334,7 @@
 
      ! reading the center points
 
+     if (1 == 0 ) then !CSD
      print *, 'Reading ', trim(lonvar_list(1))
      ierr=nf90_inq_varid(ncid, trim(lonvar_list(1)), id_var)
      call netcdf_err(ierr, 'reading variable id' )
@@ -333,7 +351,6 @@
      ierr=nf90_get_var(ncid, id_var, vec1)
      call netcdf_err(ierr, 'reading variable' )
 
-
      ! increment files are S->N, need to flip input lats from N->S
      if ( grid_setup%descriptor == 'gau_inc') then
         do j =1,j_dim
@@ -343,6 +360,8 @@
      else
         array_lat(1,:,:) = reshape(vec1,(/i_dim,j_dim/))
      endif
+
+     endif !CSD
 
      ! read in the corners
 
@@ -379,22 +398,24 @@
   endif
 
   ! scatter the arrays into the fields
-  do v =1,2
+  !do v =1 ! CSD
+  v=2
+
 
       ! scatter longitudes
-      if (localpet==0) print *, 'scattering lon', lonvar_list(v)
+      if (localpet==0) print *, 'scattering ', lonvar_list(v)
       array2d=array_lon(v,:,:)
       call ESMF_FieldScatter(lon_fields(v), array2d, rootpet=0,  rc=ierr)
       if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
          call error_handler("IN FieldScatter", ierr)
 
       ! scatter latitude
-      if (localpet==0) print *, 'scattering lat', latvar_list(v)
+      if (localpet==0) print *, 'scattering ', latvar_list(v)
       array2d=array_lat(v,:,:)
       call ESMF_FieldScatter(lat_fields(v), array2d, rootpet=0,  rc=ierr)
       if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
          call error_handler("IN FieldScatter", ierr)
- enddo
+ !enddo
 
  ! clean up
  deallocate(array_lat)
@@ -405,8 +426,8 @@
 
 !> write variables from ESMF Fields into netcdf restart-like file
 !! @param[in] localpet          local pet
-!! @param[in] i_dim             latitudinal dimension
-!! @param[in] j_dim             longitudinal dimension
+!! @param[in] i_dim             longitudinal dimension
+!! @param[in] j_dim             latitudinal dimension
 !! @param[in] fname_out         file name to write to
 !! @param[in] dir_out           directory of file name to write to
 !! @param[in] n_vars            number of variables to read in 
@@ -615,7 +636,8 @@
                          grid_setup, gauss_grid, lon_fields, lat_fields)
 
  staggerloc = (/ ESMF_STAGGERLOC_CENTER, ESMF_STAGGERLOC_CORNER /)
- do v = 1,2
+ !do v = 1 ! CSD
+ v=2
      ! add coordinates to the grid
      call ESMF_GridAddCoord(gauss_grid, &
                             staggerloc=staggerloc(v), rc=ierr)
@@ -641,7 +663,7 @@
      if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
             call error_handler("IN FieldGet longitude", ierr)
 
-     ! set coord pointe to field pointer
+     ! set coord pointer to field pointer
      lon_ptr_coord = lon_ptr_field
 
      nullify(lat_ptr_coord)
@@ -664,17 +686,18 @@
 
      ! set lat coord to field values
      lat_ptr_coord =  lat_ptr_field
- enddo
+ !enddo
 
  ! clean up
- do v = 1,2
+ !do v = 1 ! CSD
+  v=2
      call ESMF_FieldDestroy(lat_fields(v),rc=ierr)
      if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
         call error_handler("DESTROYING FIELD", ierr)
      call ESMF_FieldDestroy(lon_fields(v),rc=ierr)
      if(ESMF_logFoundError(rcToCheck=ierr,msg=ESMF_LOGERR_PASSTHRU,line=__LINE__,file=__FILE__)) &
         call error_handler("DESTROYING FIELD", ierr)
- enddo
+ !enddo
 
  end subroutine create_grid_gauss
 
