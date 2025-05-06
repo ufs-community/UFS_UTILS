155bab8f48a43d0da3f53ab767d906ac6dfe3bf0
ChuankaiWang-NOAA <93553802+ChuankaiWang-NOAA@users.noreply.github.com>
2025-05-06 13:51:44 -0400

    Fix writes of large grids (#1044)
    Fixed the 'regional_esg' and 'orog_gsl' programs so that the
    NetCDF file writes do not fail for very large grids. This was done
    by specifying a NetCDF4 file in nf90_create.
    
    Fixes #1043.

--- sorc/orog_mask_tools.fd/orog_gsl.fd/module_gsl_oro_data_lg_scale.f90
+++ sorc/orog_mask_tools.fd/orog_gsl.fd/module_gsl_oro_data_lg_scale.f90
@@ -808,7 +808,7 @@ else   ! stand-alone regional tile
 end if
 
 ! Open netCDF file for output
-err = nf90_create(oro_data_output_file_name, NF90_CLOBBER, ncid_out)
+err = nf90_create(oro_data_output_file_name, IOR(NF90_NETCDF4,NF90_CLASSIC_MODEL), ncid_out)
 call netcdf_err(err, 'creating: '//oro_data_output_file_name)
 
 err = nf90_redef(ncid_out)
