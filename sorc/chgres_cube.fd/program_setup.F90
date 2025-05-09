4e29d641070ea095fd16426ec7675dd05649f47c
George.Gayno <george.gayno@noaa.gov>
2025-05-09 17:18:45 +0000

    Read program namelist on one task.
    Fixes #1055.

--- sorc/chgres_cube.fd/program_setup.F90
+++ sorc/chgres_cube.fd/program_setup.F90
@@ -153,12 +153,15 @@
 !! ./fort.41).
 !! @author George Gayno NCEP/EMC
  subroutine read_setup_namelist(filename)
+
+ use mpi_f08
+
  implicit none
 
  character(len=*), intent(in), optional :: filename
  character(len=250), allocatable :: filename_to_use
 
- integer                     :: is, ie, ierr
+ integer                     :: i, is, ie, ierr, myrank
 
 
  namelist /config/ varmap_file, &
@@ -197,7 +200,6 @@
                    nsoill_out, &
                    thomp_mp_climo_file
 
- print*,"- READ SETUP NAMELIST"
 
  if (present(filename)) then
    filename_to_use = filename
@@ -205,12 +207,72 @@
    filename_to_use = "./fort.41"
  endif
 
- open(41, file=trim(filename_to_use), iostat=ierr)
- if (ierr /= 0) call error_handler("OPENING SETUP NAMELIST.", ierr)
- read(41, nml=config, iostat=ierr)
- if (ierr /= 0) call error_handler("READING SETUP NAMELIST.", ierr)
- close (41)
- 
+ call mpi_comm_rank(mpi_comm_world, myrank, ierr)
+
+ if (myrank == 0) then
+   print*,"- READ SETUP NAMELIST"
+   open(41, file=trim(filename_to_use), iostat=ierr)
+   if (ierr /= 0) call error_handler("OPENING SETUP NAMELIST.", ierr)
+   read(41, nml=config, iostat=ierr)
+   if (ierr /= 0) call error_handler("READING SETUP NAMELIST.", ierr)
+   close (41)
+ endif
+
+ call mpi_barrier(MPI_COMM_WORLD,ierr)
+ call mpi_bcast(varmap_file,len(varmap_file),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(mosaic_file_target_grid,len(mosaic_file_target_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(fix_dir_target_grid,len(fix_dir_target_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(orog_dir_target_grid,len(orog_dir_target_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ do i = 1, 6
+   call mpi_bcast(orog_files_target_grid(i),len(orog_files_target_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ enddo
+ call mpi_bcast(mosaic_file_input_grid,len(mosaic_file_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(orog_dir_input_grid,len(orog_dir_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ do i = 1, 6
+   call mpi_bcast(orog_files_input_grid(i),len(orog_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ enddo
+ call mpi_bcast(nst_files_input_grid,len(nst_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ do i = 1, 6
+   call mpi_bcast(sfc_files_input_grid(i),len(sfc_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+   call mpi_bcast(atm_files_input_grid(i),len(atm_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+   call mpi_bcast(atm_tracer_files_input_grid(i),len(atm_tracer_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ enddo
+ do i = 1, 7
+   call mpi_bcast(atm_core_files_input_grid(i),len(atm_core_files_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ enddo
+ call mpi_bcast(grib2_file_input_grid,len(grib2_file_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(geogrid_file_input_grid,len(geogrid_file_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(data_dir_input_grid,len(data_dir_input_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(vcoord_file_target_grid,len(vcoord_file_target_grid),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+
+ call mpi_bcast(cycle_year,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(cycle_mon,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(cycle_day,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(cycle_hour,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(convert_atm,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(convert_nst,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(convert_sfc,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(wam_cold_start,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(vgtyp_from_climo,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(sotyp_from_climo,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(vgfrc_from_climo,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(minmax_vgfrc_from_climo,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(lai_from_climo,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(tg3_from_soil,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(regional,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(input_type,len(input_type),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(external_model,len(external_model),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(wam_parm_file,len(wam_parm_file),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(atm_weight_file,len(atm_weight_file),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ do i = 1, max_tracers
+   call mpi_bcast(tracers(i),len(tracers),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+   call mpi_bcast(tracers_input(i),len(tracers_input),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+ enddo
+ call mpi_bcast(halo_bndy,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(halo_blend,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(nsoill_out,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
+ call mpi_bcast(thomp_mp_climo_file,len(thomp_mp_climo_file),MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+
  call to_lower(input_type)
  
  orog_dir_target_grid = trim(orog_dir_target_grid) // '/'
