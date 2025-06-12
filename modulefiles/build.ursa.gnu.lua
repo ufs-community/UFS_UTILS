867b42ca75474804bf56cc2573353fc948dcf140
George.Gayno <george.gayno@noaa.gov>
2025-06-12 20:13:31 +0000

    Remove hera build modules. Add ursa build module using gnu.
    Fixes #1064.

---
+++ modulefiles/build.ursa.gnu.lua
@@ -0,0 +1,60 @@
+help([[
+Load environment to compile UFS_UTILS on Ursa using Gnu
+]])
+
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.1/envs/ue-gcc-12.4.0/install/modulefiles/Core")
+
+stack_gcc_ver=os.getenv("stack_gcc_ver") or "12.4.0"
+load(pathJoin("stack-gcc", stack_gcc_ver))
+
+stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.6"
+load(pathJoin("stack-openmpi", stack_openmpi_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.30.2"
+load(pathJoin("cmake", cmake_ver))
+
+bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+load(pathJoin("bacio", bacio_ver))
+
+g2_ver=os.getenv("g2_ver") or "3.5.1"
+load(pathJoin("g2", g2_ver))
+
+ip_ver=os.getenv("ip_ver") or "5.1.0"
+load(pathJoin("ip", ip_ver))
+
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
+load(pathJoin("nemsio", nemsio_ver))
+
+--sp_ver=os.getenv("sp_ver") or "2.5.0"
+--load(pathJoin("sp", sp_ver))
+
+w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
+load(pathJoin("w3emc", w3emc_ver))
+
+-- Uncomment when CHGRES_ALL is ON
+--sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
+--load(pathJoin("sfcio", sfcio_ver))
+
+sigio_ver=os.getenv("sigio_ver") or "2.3.3"
+load(pathJoin("sigio", sigio_ver))
+
+netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
+load(pathJoin("netcdf-c", netcdf_c_ver))
+
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
+load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
+
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
+load(pathJoin("nccmp", nccmp_ver))
+
+esmf_ver=os.getenv("esmf_ver") or "8.6.1"
+load(pathJoin("esmf", esmf_ver))
+
+nco_ver=os.getenv("nco_ver") or "5.2.4"
+load(pathJoin("nco", nco_ver))
+
+setenv("MPI_CC", "mpicc")
+setenv("MPI_CCX", "mpic++")
+setenv("MPI_FC", "mpif90")
+
+whatis("Description: UFS_UTILS build environment")
