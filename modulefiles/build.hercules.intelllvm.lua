bfb726a447bd58d9066310debb927914bbb60efe
David Huber <david.huber@noaa.gov>
2025-06-20 13:55:27 -0500

    Fix hercules stack module name

--- modulefiles/build.hercules.intelllvm.lua
+++ modulefiles/build.hercules.intelllvm.lua
@@ -5,7 +5,7 @@ Load environment to compile UFS_UTILS on Hercules using IntelLLVM
 prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
-load(pathJoin("stack_oneapi", stack_oneapi_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
