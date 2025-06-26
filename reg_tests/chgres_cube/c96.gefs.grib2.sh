5e32ea1c69535556341c02aa1dbd5326938a56eb
George.Gayno <george.gayno@noaa.gov>
2025-06-26 20:19:58 +0000

    Update chgres_cube regression tests to store the output from the executable to the main log file.
    Fixes #1041.

--- reg_tests/chgres_cube/c96.gefs.grib2.sh
+++ reg_tests/chgres_cube/c96.gefs.grib2.sh
@@ -32,6 +32,9 @@ export OMP_NUM_THREADS_CH=${OMP_NUM_THREADS:-1}
 
 NCCMP=${NCCMP:-$(which nccmp)}
 
+export PGMOUT=out
+export PGMERR=err
+
 #-----------------------------------------------------------------------------
 # Invoke chgres program.
 #-----------------------------------------------------------------------------
@@ -41,6 +44,12 @@ echo "Starting at: " `date`
 ${HOMEufs}/ush/chgres_cube.sh
 
 iret=$?
+
+cd $DATA
+
+cat $PGMOUT
+cat $PGMERR
+
 if [ $iret -ne 0 ]; then
   set +x
   echo "<<< C96 GEFS GRIB2 TEST FAILED. <<<"
@@ -52,8 +61,6 @@ echo "Ending at: " `date`
 #-----------------------------------------------------------------------------
 # Compare output from chgres to baseline set of data.
 #-----------------------------------------------------------------------------
-
-cd $DATA
 
 test_failed=0
 for files in *.nc
