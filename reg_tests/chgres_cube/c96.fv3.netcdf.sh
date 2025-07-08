edec2e6cd79fb08cc49072af89da60f7c4d712a2
GeorgeGayno-NOAA <52789452+GeorgeGayno-NOAA@users.noreply.github.com>
2025-07-08 13:19:07 -0400

    Remove REDOUT and REDERR variables from 'ush' scripts (#1072)
    Fixes #1041.

--- reg_tests/chgres_cube/c96.fv3.netcdf.sh
+++ reg_tests/chgres_cube/c96.fv3.netcdf.sh
@@ -28,6 +28,9 @@ export OMP_NUM_THREADS_CH=${OMP_NUM_THREADS:-1}
 
 NCCMP=${NCCMP:-$(which nccmp)}
 
+export PGMOUT=out
+export PGMERR=err
+
 #-----------------------------------------------------------------------------
 # Invoke chgres program.
 #-----------------------------------------------------------------------------
@@ -37,6 +40,12 @@ echo "Starting at: " `date`
 ${HOMEufs}/ush/chgres_cube.sh
 
 iret=$?
+
+cd $DATA
+
+cat $PGMOUT
+cat $PGMERR
+
 if [ $iret -ne 0 ]; then
   set +x
   echo "<<< C96 FV3 GAUSSIAN NETCDF TEST FAILED. <<<"
@@ -48,8 +57,6 @@ echo "Ending at: " `date`
 #-----------------------------------------------------------------------------
 # Compare output from chgres to baseline set of data.
 #-----------------------------------------------------------------------------
-
-cd $DATA
 
 test_failed=0
 for files in *.nc
