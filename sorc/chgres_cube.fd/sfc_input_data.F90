63a212158a40eabefce4921649c6854c479b1a15
yuanxue2870 <yuan.xue@noaa.gov>
2025-07-15 14:34:03 +0000

    add mask check for restart vs. orog

--- sorc/chgres_cube.fd/sfc_input_data.F90
+++ sorc/chgres_cube.fd/sfc_input_data.F90
@@ -1101,17 +1101,18 @@ module sfc_input_data
 
  integer                         :: error, rc
  integer                         :: id_dim, idim_input, jdim_input
- integer                         :: ncid, tile, id_var, idum(2)
+ integer                         :: ncid, tile, id_var, id_var1, idum(2)
 
- real(esmf_kind_r8), allocatable :: data_one_tile(:,:)
+ real(esmf_kind_r8), allocatable :: data_one_tile(:,:), land_frac_one_tile(:,:), vtype_one_tile(:,:)
  real(esmf_kind_r8), allocatable :: data_one_tile_3d(:,:,:)
 
  type(esmf_vm)                   :: vm
 
-!---------------------------------------------------------------------------
+!------------------------------------------------------------------------------------------
 ! Get i/j dimensions and number of soil layers from first surface file.
 ! Do dimensions match those from the orography file?
-!---------------------------------------------------------------------------
+! Do land/sea masks (i.e., ocean res. and orog version) match that from the orography file?
+!------------------------------------------------------------------------------------------
 
  print*,"- CALL VMGetGlobal"
  call ESMF_VMGetGlobal(vm, rc=rc)
@@ -1151,9 +1152,13 @@ module sfc_input_data
 
  if (localpet == 0) then
    allocate(data_one_tile(idim_input,jdim_input))
+   allocate(land_frac_one_tile(idim_input,jdim_input))
+   allocate(vtype_one_tile(idim_input,jdim_input))
    allocate(data_one_tile_3d(idim_input,jdim_input,lsoil_input))
  else
    allocate(data_one_tile(0,0))
+   allocate(land_frac_one_tile(0,0))
+   allocate(vtype_one_tile(0,0))
    allocate(data_one_tile_3d(0,0,0))
  endif
 
@@ -1166,10 +1171,25 @@ module sfc_input_data
      call netcdf_err(error, 'OPENING OROGRAPHY FILE' )
      error=nf90_inq_varid(ncid, 'orog_raw', id_var)
      call netcdf_err(error, 'READING OROG RECORD ID' )
+     error=nf90_inq_varid(ncid, 'land_frac', id_var1)
+     call netcdf_err(error, 'READING OROG LAND_FRAC ID' )
      error=nf90_get_var(ncid, id_var, data_one_tile)
      call netcdf_err(error, 'READING OROG RECORD' )
+     error=nf90_get_var(ncid, id_var1, land_frac_one_tile)
+     call netcdf_err(error, 'READING OROG LAND_FRAC' )
      print*,'terrain check ',tile, maxval(data_one_tile)
      error=nf90_close(ncid)
+
+     ! Check if land_frac > 0, vtype > 0
+     ! If this condition not satisfies, restart and orog files are inconsistent
+     call read_fv3_grid_data_netcdf('vtype', tile, idim_input, jdim_input, &
+                                   lsoil_input, sfcdata=vtype_one_tile)
+     if (any((land_frac_one_tile > 0.) .and. (vtype_one_tile <= 0))) then
+       call error_handler("INCONSISTENT LAND/SEA MASK BETWEEN RESTART AND OROG FILES.", 10)
+     else
+       print*,'mask check ',tile,': consistent!'
+     endif
+
    endif
 
    print*,"- CALL FieldScatter FOR INPUT TERRAIN."
