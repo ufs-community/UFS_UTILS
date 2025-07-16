3214857de0377c57cba656532ba6475b7fc70d51
David Huber <david.huber@noaa.gov>
2025-07-16 13:53:52 +0000

    Update nccmp module on WCOSS2 to align with the netcdf-D suite

--- reg_tests/chgres_cube/driver.wcoss2.sh
+++ reg_tests/chgres_cube/driver.wcoss2.sh
@@ -30,8 +30,7 @@ source ../../sorc/machine-setup.sh > /dev/null 2>&1
 module use ../../modulefiles
 module load build.$target.$compiler
 # load nccmp
-module load netcdf
-module load nccmp
+module load nccmp-D/1.9.0.1
 module list
 
 export OUTDIR="${WORK_DIR:-/lfs/h2/emc/stmp/$LOGNAME}"
--- reg_tests/rt.sh
+++ reg_tests/rt.sh
@@ -15,14 +15,14 @@ wait_for_fin() {
 
 ulimit -s unlimited
 
-export MAILTO=
+export MAILTO=david.huber@noaa.gov
 
 # Directory to download UFS_UTILS to and run the consistency tests
-export WORK_DIR=
+export WORK_DIR=/lfs/h2/emc/stmp/david.huber/ufs_utils_rt
 
-export PROJECT_CODE=
-export QUEUE=
-TIMEOUT_LIMIT=3600
+export PROJECT_CODE=GFS-DEV
+export QUEUE=dev
+TIMEOUT_LIMIT=7200
 
 mkdir -p ${WORK_DIR}
 cd ${WORK_DIR}
