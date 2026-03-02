154fef838ef92eabfe1ce1ea9afb2b3afbe97ca0
denise.worthen <Denise.Worthen@noaa.gov>
2026-03-02 19:22:28 +0000

    remove ifdef carve-out of code

--- sorc/cpld_gridgen.fd/gen_fixgrid.F90
+++ sorc/cpld_gridgen.fd/gen_fixgrid.F90
@@ -488,7 +488,7 @@ program gen_fixgrid
 
      nvalid = size(catm)
   end if ! if (maintask)
-#ifdef test
+
   !---------------------------------------------------------------------
   ! set up for parallel work
   !---------------------------------------------------------------------
@@ -641,5 +641,4 @@ program gen_fixgrid
      deallocate(grid%Cu%lat, grid%Cu%lon)
      deallocate(grid%Bu%lat, grid%Bu%lon)
   endif ! if (maintask)
-#endif
 end program gen_fixgrid
