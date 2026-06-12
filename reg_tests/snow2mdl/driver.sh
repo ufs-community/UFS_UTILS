4bea902e1e9f2692daf7041655ff799f25ece5e3
Brian Curtis <Brian.Curtis@noaa.gov>
2026-06-12 13:30:11 -0400

    adjusting weird memory issue with c6 slurm

--- reg_tests/snow2mdl/driver.sh
+++ reg_tests/snow2mdl/driver.sh
@@ -129,8 +129,8 @@ case ${MACHINE_ID,,} in
         submit_test 02 1 1 5G 0:03:00 u1-compute false false snow.global snow2mdl.global.sh "${jobkeep}"
         ;;
     gaeac6)
-        jobkeep=$(submit_test 01 1 1 5G 0:03:00 batch c6 false snow.hemi snow2mdl.hemi.sh false)
-        submit_test 02 1 1 5G 0:03:00 batch c6 false snow.global snow2mdl.global.sh "${jobkeep}"
+        jobkeep=$(submit_test 01 1 1 0 0:03:00 batch c6 false snow.hemi snow2mdl.hemi.sh false)
+        submit_test 02 1 1 0 0:03:00 batch c6 false snow.global snow2mdl.global.sh "${jobkeep}"
         ;;
     wcoss2)
         jobkeep=$(submit_test 01 1 1 5G 0:03:00 dev false false snow.hemi snow2mdl.hemi.sh false)
