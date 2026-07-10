645c3a1e85fbd89e92a6ab48c06784f394a8e105
Brian Curtis <Brian.Curtis@noaa.gov>
2026-07-10 14:57:29 -0400

    intel-classic troubles, removing support for now

--- modulefiles/build.gaeac6.intelllvm.lua
+++ modulefiles/build.gaeac6.intelllvm.lua
@@ -1,5 +1,5 @@
 help([[
-Load environment to compile UFS_UTILS on Gaea C6 using Intel
+Load environment to compile UFS_UTILS on Gaea C6 using IntelLLVM
 ]])
 
 prepend_path("MODULEPATH", "/usw/hpss/modulefiles")
